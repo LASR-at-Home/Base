d27c37556f7ddae1f9c644df3c7210efb6e51a14
fireblonde <nicollehchevska@gmail.com>
2022-10-25 12:06:11 +0100

    feat: add basic st for meeting and greating

--- subtasks/meet_and_greet/src/meet_and_greet/meet_and_greet_sm.py
+++ subtasks/meet_and_greet/src/meet_and_greet/meet_and_greet_sm.py
@@ -6,7 +6,7 @@ import sys
 from smach import Concurrence
 from smach_ros import IntrospectionServer
 from create_dataset import CreateDatasetState, TalkCreateDatasetState
-from create_dataset.train_model import train_model
+from train_dataset_model import TrainModelState
 
 
 class MeetAndGreetSM(smach.StateMachine):
@@ -14,7 +14,7 @@ class MeetAndGreetSM(smach.StateMachine):
         smach.StateMachine.__init__(self, outcomes=['finished_meet_and_greet', 'wonder_around'])
 
         with self:
-            cc = Concurrence(outcomes=['finished_talking', 'not_finished_talking', 'abort'],
+            cc = Concurrence(outcomes=['finished_dataset_collection', 'not_finished_dataset_collection', 'abort'],
                              default_outcome='not_finished_talking',
                              outcome_map=[{
                                  'finished_talking':
@@ -23,11 +23,18 @@ class MeetAndGreetSM(smach.StateMachine):
                                          'CreateDataset': 'finished_dataset'}
                              }])
             with cc:
-                smach.Concurrence.add('TALK_CREATE_DATASET', TalkCreateDatasetState())
-                smach.Concurrence.add('CREATE_DATASET', CreateDatasetState())
+                smach.Concurrence.add('TALK_CREATE_DATASET', TalkCreateDatasetState(), remapping={'current_person' : 'current_person'})
+                smach.Concurrence.add('CREATE_DATASET', CreateDatasetState(), remapping={'dataset_path': 'dataset_path'})
 
 
-            smach.StateMachine.add('CREATE_DATASET_AND_TALK', cc)
+            smach.StateMachine.add('CREATE_DATASET_AND_TALK', cc,
+                                   transitions={
+                                       'finished_dataset_collection' : 'TRAIN_MODEL',
+                                       'not_finished_dataset_collection' : 'CREATE_DATASET_AND_TALK'
+                                   })
+
+            smach.StateMachine.add('TRAIN_MODEL', TrainModelState(), transitions={'finished_training' : 'FETCH_ATTRIBUTES', 'training_failed' : 'wonder'},
+                                                                     remapping={'current_person' : 'current_guest', 'dataset_path' : 'dataset_path'})
 
 if __name__ == "__main__":
     rospy.init_node("collect_and_scan_sm", sys.argv, anonymous=True)
