7c30daedaa5dc685717bcd0c9ff8ccc26adcb780
Jared Swift <j.w.swift@outlook.com>
2023-07-09 12:59:34 +0100

    Always go to the closest table.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/go_to_table.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/go_to_table.py
@@ -2,6 +2,7 @@
 import smach
 import rospy
 from geometry_msgs.msg import Pose, Point, Quaternion
+import numpy as np
 
 class GoToTable(smach.State):
     def __init__(self, base_controller):
@@ -9,8 +10,10 @@ class GoToTable(smach.State):
         self.base_controller = base_controller
     def execute(self, userdata):
         tables = rospy.get_param("/tables")
+        robot_x, robot_y = self.base_controller.get_pose()
         unvisited = [(label, table) for label, table in tables.items() if table["status"] == "unvisited"]
-        label, next_table = unvisited[0]
+        closest_table = min(unvisited, key=lambda table: np.linalg.norm([table[1]["location"]["position"]["x"] - robot_x, table[1]["location"]["position"]["y"] - robot_y]))
+        label, next_table = closest_table
         position, orientation = next_table["location"]["position"], next_table["location"]["orientation"]
         self.base_controller.sync_to_pose(Pose(position=Point(**position), orientation=Quaternion(**orientation)))
         rospy.set_param(f"/tables/{label}/status", "currently_visiting")
