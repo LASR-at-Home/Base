a572237f8fc219debdb7ab90d67a04b2b5e6f85f
Jared Swift <j.w.swift@outlook.com>
2023-07-09 12:59:54 +0100

    Better phase 1.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/check_table.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/check_table.py
@@ -10,6 +10,7 @@ from lasr_object_detection_yolo.srv import YoloDetection
 from lasr_voice.voice import Voice
 from pcl_segmentation.srv import SegmentCuboid
 from common_math import pcl_msg_to_cv2
+import numpy as np
 OBJECTS = ["cup", "mug"]
 
 class CheckTable(smach.State):
@@ -22,57 +23,57 @@ class CheckTable(smach.State):
         self.voice_controller = voice_controller
         self.mask_from_cuboid = rospy.ServiceProxy("/pcl_segmentation_server/segment_cuboid", SegmentCuboid)
         self.bridge = CvBridge()
+        self.detections_objects = []
+        self.detections_people = []
+        
 
     def perform_detection(self, pcl_msg, min_xyz, max_xyz, filter):
         mask = self.mask_from_cuboid(pcl_msg, Point(*min_xyz), Point(*max_xyz)).mask
         cv_im = pcl_msg_to_cv2(pcl_msg)
         cv_mask = self.bridge.imgmsg_to_cv2_np(mask)
-
         img_msg = self.bridge.cv2_to_imgmsg(cv2.bitwise_and(cv_im, cv_im, mask=cv_mask))
         detections = self.detect(img_msg, "coco", 0.5, 0.3)
         detections = [det for det in detections.detected_objects if det.name in filter]
-        return detections
+        return detections, cv2.bitwise_and(cv_im, cv_im, mask=cv_mask)
+
+    def check(self, pcl_msg):
+        detections_objects_, objects_mask = self.perform_detection(pcl_msg, self.min_xyz_objects, self.max_xyz_objects, OBJECTS)
+        detections_people_, people_mask = self.perform_detection(pcl_msg, self.min_xyz_people, self.max_xyz_people, ["person"])
+        self.detections_objects.extend(detections_objects_)
+        self.detections_people.extend(detections_people_)
+        #stack = np.hstack((objects_mask, people_mask))
+        #cv2.imshow("Masks", stack)
+        #cv2.waitKey(1)
 
     def execute(self, userdata):
         current_table = rospy.get_param("current_table")
-        min_xyz_objects, max_xyz_objects = rospy.get_param(f"/tables/{current_table}/objects_cuboid/min"), rospy.get_param(f"/tables/{current_table}/objects_cuboid/max")
-        min_xyz_people, max_xyz_people = rospy.get_param(f"/tables/{current_table}/persons_cuboid/min"), rospy.get_param(f"/tables/{current_table}/persons_cuboid/max")
+        rospy.loginfo(current_table)
+        self.min_xyz_objects, self.max_xyz_objects = rospy.get_param(f"/tables/{current_table}/objects_cuboid/min"), rospy.get_param(f"/tables/{current_table}/objects_cuboid/max")
+        self.min_xyz_people, self.max_xyz_people = rospy.get_param(f"/tables/{current_table}/persons_cuboid/min"), rospy.get_param(f"/tables/{current_table}/persons_cuboid/max")
+        self.detections_objects = []
+        self.detections_people = []
+        self.pcl_sub = rospy.Subscriber("/xtion/depth_registered/points", PointCloud2, self.check)
 
-        pm_goal = PlayMotionGoal(motion_name="check_table", skip_planning=True)
-        self.play_motion_client.send_goal_and_wait(pm_goal)
-        pcl_msg = rospy.wait_for_message("/xtion/depth_registered/points", PointCloud2)
-        detections_objects = self.perform_detection(pcl_msg, min_xyz_objects, max_xyz_objects, OBJECTS)
-        detections_people = self.perform_detection(pcl_msg, min_xyz_people, max_xyz_people, ["person"])
+        motions = ["check_table", "check_table_low", "look_left", "look_down_left", "look_right",  "look_down_right", "back_to_default"]
 
-        pm_goal = PlayMotionGoal(motion_name="look_down_left", skip_planning=True)
-        self.play_motion_client.send_goal_and_wait(pm_goal)
-        pcl_msg = rospy.wait_for_message("/xtion/depth_registered/points", PointCloud2)
-        detections_objects_ = self.perform_detection(pcl_msg, min_xyz_objects, max_xyz_objects, OBJECTS)
-        detections_people_ = self.perform_detection(pcl_msg, min_xyz_people, max_xyz_people, ["person"])
-        detections_objects.extend(detections_objects_)
-        detections_people.extend(detections_people_)
+        for motion in motions:
+            pm_goal = PlayMotionGoal(motion_name=motion, skip_planning=True)
+            self.play_motion_client.send_goal_and_wait(pm_goal)
+            rospy.sleep(1)
 
-        pm_goal = PlayMotionGoal(motion_name="look_down_right", skip_planning=True)
-        self.play_motion_client.send_goal_and_wait(pm_goal)
-        pcl_msg = rospy.wait_for_message("/xtion/depth_registered/points", PointCloud2)
-        detections_objects_ = self.perform_detection(pcl_msg, min_xyz_objects, max_xyz_objects, OBJECTS)
-        detections_people_ = self.perform_detection(pcl_msg, min_xyz_people, max_xyz_people, ["person"])
-        detections_objects.extend(detections_objects_)
-        detections_people.extend(detections_people_)
+        self.pcl_sub.unregister()
 
-        pm_goal = PlayMotionGoal(motion_name="back_to_default", skip_planning=True)
-        self.play_motion_client.send_goal_and_wait(pm_goal)
-
-        if len(detections_objects) > 0 and len(detections_people) == 0:
+        if len(self.detections_objects) > 0 and len(self.detections_people) == 0:
             status = "needs cleaning"
-        elif len(detections_objects) > 0 and len(detections_people) > 0:
+        elif len(self.detections_objects) > 0 and len(self.detections_people) > 0:
             status = "served"
-        elif len(detections_objects) == 0 and len(detections_people) > 0:
+        elif len(self.detections_objects) == 0 and len(self.detections_people) > 0:
             status = "needs serving"
-        elif len(detections_objects) == 0 and len(detections_people) == 0:
+        elif len(self.detections_objects) == 0 and len(self.detections_people) == 0:
             status = "ready"
 
         rospy.set_param(f"/tables/{current_table}/status/", status)
         self.voice_controller.sync_tts("The status of this table is")
         self.voice_controller.sync_tts(str(status))
+        cv2.destroyAllWindows()
         return 'finished' if len([(label, table) for label, table in rospy.get_param("/tables").items() if table["status"] == "unvisited"]) == 0 else 'not_finished'
