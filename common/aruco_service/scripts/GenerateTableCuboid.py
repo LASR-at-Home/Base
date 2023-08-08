d006431993244896b35022656fdc0600689753c6
Jared Swift <100244619+jws-1@users.noreply.github.com>
2023-08-08 16:24:19 +0100

    feat: publish corners. (#28)

--- common/aruco_service/scripts/GenerateTableCuboid.py
+++ common/aruco_service/scripts/GenerateTableCuboid.py
@@ -5,6 +5,7 @@ import rospy
 import datetime
 from aruco_service.srv import TableNumber, TableNumberResponse
 from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
 import rosparam
 
 # Units are in meters
@@ -14,9 +15,29 @@ TABLE_SHORT_SIDE = 0.6
 PADDING = 0.5
 FILENAME = "/home/peter/robocup_ws/src/aruco_service/test_check_table_sim.yaml"
 
+def create_marker_msg(point, idx):
+    marker_msg = Marker()
+    marker_msg.header.frame_id = "map"
+    marker_msg.header.stamp = rospy.Time.now()
+    marker_msg.id = idx
+    marker_msg.type = Marker.SPHERE
+    marker_msg.action = Marker.ADD
+    marker_msg.pose.position.x = point[0]
+    marker_msg.pose.position.y = point[1]
+    marker_msg.color.r = 1.0
+    marker_msg.color.g = 0.0
+    marker_msg.color.b = 0.0
+    marker_msg.scale.x = 0.1
+    marker_msg.scale.y = 0.1
+    marker_msg.scale.z = 0.1
+    marker_msg.color.a = 1.0
+    return marker_msg
+
 def generate_table_cuboid(number):
     table = number.table
     seconds = rospy.get_time()
+    objects_marker_pub = rospy.Publisher("/table/objects_cuboid", Marker, queue_size=10)
+    persons_marker_pub = rospy.Publisher("/table/persons_cuboid", Marker, queue_size=10)
 
     if latest_pose is not None and (seconds - latest_pose.header.stamp.secs < 1.0):
 
@@ -24,13 +45,20 @@ def generate_table_cuboid(number):
         corner_2 = [latest_pose.pose.position.x - 0.05 + TABLE_LONG_SIDE, latest_pose.pose.position.y - 0.05]
         corner_3 = [latest_pose.pose.position.x - 0.05 + TABLE_LONG_SIDE, latest_pose.pose.position.y - 0.05 + TABLE_SHORT_SIDE]
         corner_4 = [latest_pose.pose.position.x - 0.05, latest_pose.pose.position.y - 0.05 + TABLE_SHORT_SIDE]
-
+        objects_marker_pub.publish(create_marker_msg(corner_1, 0))
+        objects_marker_pub.publish(create_marker_msg(corner_2, 1))
+        objects_marker_pub.publish(create_marker_msg(corner_3, 2))
+        objects_marker_pub.publish(create_marker_msg(corner_4, 3))
         rospy.set_param("/tables/table" + str(table) + "/objects_cuboid", [corner_1, corner_2, corner_3, corner_4])
 
         padded_corner_1 = [corner_1[0] - PADDING, corner_1[1] - PADDING]
         padded_corner_2 = [corner_2[0] + PADDING, corner_2[1] - PADDING]
         padded_corner_3 = [corner_3[0] + PADDING, corner_3[1] + PADDING]
         padded_corner_4 = [corner_4[0] - PADDING, corner_4[1] + PADDING]
+        persons_marker_pub.publish(create_marker_msg(padded_corner_1, 0))
+        persons_marker_pub.publish(create_marker_msg(padded_corner_2, 1))
+        persons_marker_pub.publish(create_marker_msg(padded_corner_3, 2))
+        persons_marker_pub.publish(create_marker_msg(padded_corner_4, 3))
         rospy.set_param("/tables/table" + str(table) + "/persons_cuboid", [padded_corner_1, padded_corner_2, padded_corner_3, padded_corner_4])
 
         now = str(datetime.datetime.now())
