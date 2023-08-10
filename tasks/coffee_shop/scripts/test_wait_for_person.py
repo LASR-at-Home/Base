8f9a37463846e4448204a23d6d627c97cd557678
Jared Swift <j.w.swift@outlook.com>
2023-08-10 22:32:36 +0100

    chore: fixup test.

--- tasks/coffee_shop/scripts/test_wait_for_person.py
+++ tasks/coffee_shop/scripts/test_wait_for_person.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import smach
-from coffee_shop.phases.phase_3.states import WaitForPerson, GreetPerson, GoToWaitLocation
+from coffee_shop.phases.phase_3.states import LookForPerson, GreetPerson, GoToPerson
 import rospy
 from lasr_voice import Voice
 from tiago_controllers import BaseController
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     rospy.init_node("test_wait_for_person")
     sm = smach.StateMachine(outcomes=['end'])
     with sm:
-        sm.add('WAIT_FOR_PERSON', WaitForPerson(), transitions={'done' : 'GREET_PERSON'})
+        sm.add('LOOK_FOR_PERSON', LookForPerson(), transitions={'found' : 'GO_TO_PERSON', 'not found' : 'LOOK_FOR_PERSON'})
+        sm.add('GO_TO_PERSON', GoToPerson(BaseController()), transitions={'done' : 'GREET_PERSON'})
         sm.add('GREET_PERSON', GreetPerson(Voice()), transitions={'done' : 'end'})
     sm.execute()
