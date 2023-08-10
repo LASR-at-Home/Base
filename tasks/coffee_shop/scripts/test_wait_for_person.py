c29ac9003898442b1e29a08c247099cef86deb21
Jared Swift <j.w.swift@outlook.com>
2023-08-10 22:02:53 +0100

    refactor: don't go to wait location.

--- tasks/coffee_shop/scripts/test_wait_for_person.py
+++ tasks/coffee_shop/scripts/test_wait_for_person.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     rospy.init_node("test_wait_for_person")
     sm = smach.StateMachine(outcomes=['end'])
     with sm:
-        sm.add('GO_TO_WAIT_LOCATION', GoToWaitLocation(BaseController()), transitions={'done': 'WAIT_FOR_PERSON'})
         sm.add('WAIT_FOR_PERSON', WaitForPerson(), transitions={'done' : 'GREET_PERSON'})
         sm.add('GREET_PERSON', GreetPerson(Voice()), transitions={'done' : 'end'})
     sm.execute()
