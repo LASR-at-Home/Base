6a8475710e08e3956dc505c115dd3c0b67c8f41f
fireblonde <nicollehchevska@gmail.com>
2023-08-15 12:23:23 +0100

    LIFT01: connect going to the pose

--- common/navigation/choosing_wait_position/src/choosing_wait_position/final_lift_key_point/predict_pos.py
+++ common/navigation/choosing_wait_position/src/choosing_wait_position/final_lift_key_point/predict_pos.py
@@ -5,6 +5,7 @@ import torch
 from PIL import Image
 
 from torchvision.transforms import functional as F
+from narrow_space_navigation.waypoints import *
 
 import glob
 from common_tools import get_model
@@ -18,16 +19,18 @@ os.chdir(os.path.abspath(os.path.join(FWD, 'models')))
 def make_prediction(image_path):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    import os
-    print(os.getcwd())
     model = get_model(1, weights_path='keypointsrcnn_weights.pth')
-    # common / navigation / choosing_wait_position / src / choosing_wait_position / final_lift_key_point / models / keypointsrcnn_weights.pth
 
     origin_list = glob.glob(image_path)
+
+    # origin_list = [image_path]
     print(origin_list)
 
     for i, img_name in enumerate(origin_list):
         original_img = Image.open(img_name)
+
+        plt.imshow(original_img, cmap='gray')
+        plt.show()
 
         img = F.to_tensor(original_img)
         img = torch.unsqueeze(img,dim=0)
@@ -36,6 +39,7 @@ def make_prediction(image_path):
         with torch.no_grad():
             predictions = model(img.to(device))[0]
             predict_boxes = predictions["boxes"].to("cpu").numpy()
+            print("predict boxes: ", predict_boxes)
             predict_classes = predictions["labels"].to("cpu").numpy()
             predict_keypoints = predictions["keypoints"].to("cpu").numpy()
             bbox = predict_boxes[0]
@@ -54,27 +58,51 @@ def visualise_predictions(image, bbox, keypoint):
     target_pos = (int(keypoint[0][0]), int(keypoint[0][1]))
     window_name = "prediction"
     color = (255,0,0)
-    thickness = 2
+    thickness = 1
 
     image = cv2.rectangle(image, start_point,end_point,color,thickness)
-    image = cv2.circle(image, target_pos, 5, (255,0,0), 10)
+    image = cv2.circle(image, target_pos, 1, (255,0,0), 1)
 
 
     plt.figure(figsize=(40,40))
     plt.imshow(image)
     plt.show()
 
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose, Point, Quaternion
+from tiago_controllers.controllers.controllers import Controllers
 
 #EXAMPLE: 
-
+import rospy
+rospy.init_node('predict_pos')
 print(os.getcwd())
-#We create an image object using the image path: 
-image_path = '../834.jpg'
+#We create an image object using the image path:
+w = Waypoint()
+warped, analysis, M = w.get_lift_information()
+plt.imshow(warped, cmap='gray')
+plt.show()
+image = Image.fromarray(warped)
+image.save("test.jpg")
+
+image_path = "test.jpg"
+# image_path = '../834.jpg'
+
+print(image_path)
+
 # image_sample = Image.open(image_path)
 # base/src/Base/common/navigation/choosing_wait_position/src/choosing_wait_position/final_lift_key_point/data/834.jpg
 
 #Then we predict the bounding box and the keypoint using the make predictions class
 bbox, keypoint = make_prediction(image_path)
+print(f"keypoint: {keypoint}")
+global_points = w.local_to_global_points(M=M, points=keypoint)
+p = Pose()
+p.position.x =  global_points[0][0]
+p.position.y =  global_points[0][1]
+p.orientation.w = 1
+c = Controllers()
+c.base_controller.sync_to_pose(p)
+
+
 
 
 #We create an image object using the image path:
