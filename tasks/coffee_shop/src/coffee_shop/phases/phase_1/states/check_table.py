6aac14f8da539670817e14bf683deb7be29f371a
Jared Swift <j.w.swift@outlook.com>
2023-08-19 00:22:41 +0100

    fix: use correct var.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/check_table.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_1/states/check_table.py
@@ -160,6 +160,6 @@ class CheckTable(smach.State):
         people_count = len(self.detections_people)
         people_text = "person" if people_count == 1 else "people"
         status_text = f"The status of this table is {status}."
-        count_text = f"There {'is' if person_count == 1 else 'are'} {people_count} {people_text}."
+        count_text = f"There {'is' if people_count == 1 else 'are'} {people_count} {people_text}."
         self.voice_controller.sync_tts(f"{status_text} {count_text}")
         return 'finished' if len([(label, table) for label, table in rospy.get_param("/tables").items() if table["status"] == "unvisited"]) == 0 else 'not_finished'
