07df48aaae6ab47474159378b18629b21a9deb57
Ptisni <peter.tisnikar@gmail.com>
2023-08-18 13:53:41 +0100

    Add service for navigation points

---
+++ common/aruco_service/scripts/save_navigation_points.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import yaml
+import rospy
+from aruco_service.srv import TableNumber, TableNumberResponse
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import rosparam
+import rospkg
+
+path = rospkg.get_ros_package_path()
+FILENAME = path + "/test_check_table_sim.yaml"
+
+def save_points(number):
+    table = number.table
+
+    position = latest_pose.pose.position
+    orientation = latest_pose.pose.orientation
+
+    if table >= 0:
+        rospy.set_param("/tables/table" + str(table) + "/location/position", position)
+        rospy.set_param("/tables/table" + str(table) + "/location/orientation", orientation)
+        rospy.loginfo("Navigation point for table %d saved to parameter server", table)
+
+    elif table == -1:
+        rospy.set_param("/counter/location/position", position)
+        rospy.set_param("counter/location/orientation", orientation)
+        rospy.loginfo("Navigation point for the counter saved to parameter server")
+
+    elif table == -2:
+        rospy.set_param("/wait/location/position", position)
+        rospy.set_param("wait/location/orientation", orientation)
+        rospy.loginfo("Navigation point for the waiting area saved to parameter server")
+
+    else:
+        rospy.loginfo("Invalid table number.")
+        return TableNumberResponse(False)
+    
+# Dump rosparams to file
+    data = {
+        'tables': rosparam.get_param('/tables'),
+        'current_table': rosparam.get_param('/current_table'),
+        'counter': rosparam.get_param('/counter'),
+        'wait': rosparam.get_param('/wait')
+    }
+
+    with open(FILENAME, 'w') as file:
+        yaml.dump(data, file)
+
+    return TableNumberResponse(True)
+
+def get_latest_pose(msg):
+    global latest_pose
+    latest_pose = msg
+
+if __name__ == "__main__":
+
+    rospy.init_node("save_navigation_points")
+    sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, get_latest_pose)
+    s = rospy.Service("save_navigation_points", TableNumber, save_points)
+
+    els = rosparam.load_file(FILENAME)
+    for param, ns in els:
+        rosparam.upload_params(ns, param)
+    
+    rospy.loginfo("Navigation Points Saver Ready")
+    
+    while not rospy.is_shutdown():
+        rospy.sleep(1.0)
