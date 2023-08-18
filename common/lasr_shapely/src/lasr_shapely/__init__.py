eaf9104973bb95bda511b7b4d575d7c4fd60af90
Paul Makles <me@insrt.uk>
2023-08-18 14:54:16 +0100

    fix(shapely): finish implementation of class

--- common/lasr_shapely/src/lasr_shapely/__init__.py
+++ common/lasr_shapely/src/lasr_shapely/__init__.py
@@ -1,6 +1,6 @@
 import rospy
 
-from lasr_shapely.srv import PointInPolygon2D
+from lasr_shapely.srv import PointInPolygon2D, PointsInPolygon2D
 
 class LasrShapely:
     '''
@@ -9,15 +9,16 @@ class LasrShapely:
 
     def __init__(self):
         self.point_proxy = rospy.ServiceProxy('/lasr_shapely/point_in_polygon_2d', PointInPolygon2D)
+        self.points_proxy = rospy.ServiceProxy('/lasr_shapely/points_in_polygon_2d', PointsInPolygon2D)
     
     def is_point_in_polygon_2d(self, polygon_2d_array, x, y):
         return self.point_proxy(polygon=[item for sublist in polygon_2d_array for item in sublist], x=x, y=y)
     
     def are_points_in_polygon_2d(self, polygon_2d_array, points_2d_array):
-        return self.point_proxy(polygon=[item for sublist in polygon_2d_array for item in sublist],
+        return self.points_proxy(polygon=[item for sublist in polygon_2d_array for item in sublist],
                                 points=[item for sublist in points_2d_array for item in sublist])
 
 if __name__=='__main__':
-    print('grdfgdf')
-    rospy.init_node('gfdgdfgd')
+    rospy.init_node('testgkfdp', anonymous=True)
     print(LasrShapely().is_point_in_polygon_2d([[0, 0], [5,0], [10, 10], [0, 5]], 6, 6))
+    print(LasrShapely().are_points_in_polygon_2d([[0, 0], [5,0], [10, 10], [0, 5]], [[1,2], [11, 11]]))
