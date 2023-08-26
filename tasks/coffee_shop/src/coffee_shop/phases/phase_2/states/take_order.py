6589f28cf6beda2e5a6a18a130d987f7fea19992
Jared Swift <j.w.swift@outlook.com>
2023-08-26 16:29:15 +0100

    feat: initial take order implementation.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/take_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/take_order.py
@@ -2,6 +2,7 @@
 import smach
 import rospy
 import random
+import json
 
 MOCK_ORDER = ["cup", "cup"]
 class TakeOrder(smach.State):
@@ -14,30 +15,48 @@ class TakeOrder(smach.State):
 
     def listen(self):
         resp = self.speech()
-        if not resp:
+        if not resp.success:
             self.voice_controller.sync_tts("Sorry, I didn't get that")
             return self.listen()
+        return json.loads(resp.json_response)
 
     def get_order(self):
         resp = self.listen()
-        item = [entity["item"]["value"] for entity in resp["entities"] if entity["entity"] == "item"][0]
-        self.voice_controller.sync_tts(f"You asked for {item}, is that correct?")
+        if resp["intent"]["name"] != "make_order":
+            self.voice_controller.sync_tts("Sorry, I didn't get that")
+            return self.get_order()
+        items = resp["entities"].get("item", [])
+        items = [item["value"] for item in items]
+        if not items:
+            self.voice_controller.sync_tts("Sorry, I didn't get that")
+            return self.get_order()
+        self.voice_controller.sync_tts(f"You asked for {items}, is that correct?")
         if not self.affirm():
             self.voice_controller.sync_tts("Okay, could you repeat please?")
             return self.get_order()
-        return item
+        return items
 
     def affirm(self):
         resp = self.listen()
-        return bool([entity["choice"]["value"] for entity in resp["entities"] if entity["entity"] == "choice"][0])
+        if resp["intent"]["name"] != "affirm":
+            self.voice_controller.sync_tts("Sorry, I didn't get that")
+            return self.affirm()
+        choices = resp["entities"].get("choice", None)
+        if choices is None:
+            self.voice_controller.sync_tts("Sorry, I didn't get that")
+            return self.affirm()
+        choice = choices[0]["value"]
+        return bool(int(choice))
 
     def execute(self, userdata):
         self.voice_controller.sync_tts("Can I please take your order?")
         order = []
         while True:
-            order.append(self.get_order())
+            order.extend(self.get_order())
             self.voice_controller.sync_tts("Can I get you anything else?")
             if not self.affirm():
                 break
+            self.voice_controller.sync_tts("What else can I get for you?")
+        self.voice_controller.sync_tts(f"Your order is {order}")
         rospy.set_param(f"/tables/{rospy.get_param('/current_table')}/order", order)
         return 'done'
