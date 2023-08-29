eca8fbd35f7bea81582c52e9692b3582291f2487
Jared Swift <j.w.swift@outlook.com>
2023-08-29 10:42:40 +0100

    revert: head manager sim fix.

--- tasks/coffee_shop/scripts/test_make_order.py
+++ tasks/coffee_shop/scripts/test_make_order.py
@@ -9,17 +9,7 @@ from lasr_object_detection_yolo.srv import YoloDetection
 from coffee_shop.srv import TfTransform, TfTransformRequest
 from lasr_speech.srv import Speech
 from tiago_controllers import BaseController
-import rosservice
 from lasr_shapely import LasrShapely
-
-HAS_HEAD_MANAGER = "pal_startup_control/stop" in rosservice.get_service_list()
-
-try:
-    from pal_startup_msgs.srv import StartupStart, StartupStop
-    HAS_HEAD_MANAGER &= True
-except ImportError:
-    HAS_HEAD_MANAGER = False
-    pass
 
 if __name__ == "__main__":
     rospy.init_node("test_check_table")
@@ -36,18 +26,9 @@ if __name__ == "__main__":
     rospy.wait_for_service("/lasr_speech/transcribe_and_parse")
     speech = rospy.ServiceProxy("/lasr_speech/transcribe_and_parse", Speech)
 
-    if HAS_HEAD_MANAGER:
-        rospy.wait_for_service("/pal_startup_control/start")
-        start_head_manager = rospy.ServiceProxy("/pal_startup_control/start", StartupStart)
-        rospy.wait_for_service("/pal_startup_control/stop")
-        stop_head_manager = rospy.ServiceProxy("/pal_startup_control/stop", StartupStop)
-    else:
-        start_head_manager = lambda *args: None
-        stop_head_manager = lambda *args: None
-
     with sm:
         sm.add('MAKE_ORDER', MakeOrder(voice_controller), transitions={'done' : 'CHECK_ORDER'})
-        sm.add('CHECK_ORDER', CheckOrder(yolo, tf, play_motion_client, shapely, start_head_manager, stop_head_manager), transitions={'correct': 'LOAD_ORDER', 'incorrect': 'INVALIDATE_ORDER'})
+        sm.add('CHECK_ORDER', CheckOrder(yolo, tf, play_motion_client, shapely), transitions={'correct': 'LOAD_ORDER', 'incorrect': 'INVALIDATE_ORDER'})
         sm.add('INVALIDATE_ORDER', InvalidateOrder(voice_controller), transitions={'done': 'CHECK_ORDER'})
         sm.add('LOAD_ORDER', LoadOrder(base_controller, voice_controller, play_motion_client, speech), transitions={'done':'end'})
 
