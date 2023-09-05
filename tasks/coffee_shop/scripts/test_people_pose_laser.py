bf07c1b7a6692854c049999130c79cc425437b4e
Ptisni <peter.tisnikar@gmail.com>
2023-09-05 12:54:11 +0100

    Version 1 of P3 Code

--- tasks/coffee_shop/scripts/test_people_pose_laser.py
+++ tasks/coffee_shop/scripts/test_people_pose_laser.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import rospy
-from common_math import pcl_msg_to_cv2, seg_to_centroid
-from sensor_msgs.msg import LaserScan
-from std_msgs.msg import String, Header
-from geometry_msgs.msg import PointStamped, Point
+from sensor_msgs.msg import LaserScan, CameraInfo, Image
+from geometry_msgs.msg import PointStamped
 from lasr_object_detection_yolo.srv import YoloDetection
-from coffee_shop.srv import TfTransform, TfTransformRequest
+from lasr_shapely.srv import PointsInPolygon2D
 from visualization_msgs.msg import Marker
-from cv_bridge3 import CvBridge
 import numpy as np
 import laser_geometry.laser_geometry as lg
 import sensor_msgs.point_cloud2 as pc2
+from image_geometry import PinholeCameraModel
+import tf2_ros
+import tf2_geometry_msgs
+import rosparam
 
 def create_point_marker(x, y, z, idx):
     marker_msg = Marker()
-    marker_msg.header.frame_id = "map"
+    marker_msg.header.frame_id = "xtion_rgb_optical_frame"
     marker_msg.header.stamp = rospy.Time.now()
     marker_msg.id = idx
     marker_msg.type = Marker.SPHERE
@@ -32,50 +33,93 @@ def create_point_marker(x, y, z, idx):
     marker_msg.color.b = 0.0
     return marker_msg
 
-def get_pcl_from_laser(self, msg):
-        pcl_msg = lg.LaserProjection().projectLaser(msg)
-        pcl_points = [p for p in pc2.read_points(pcl_msg, field_names=("x, y, z"), skip_nans=True)]
-        
-        padded_points = []
-        for point in pcl_points:
-            for z in np.linspace(0., 2., 10):
-                padded_points.append((point[0], point[1], z))
+def get_transform(from_frame, to_frame):
+    tf_buffer = tf2_ros.Buffer()
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+    try:
+        t = tf_buffer.lookup_transform(to_frame, from_frame, rospy.Time(0), rospy.Duration(5))
+        return t
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        raise
 
-        h = Header()
-        h.stamp = rospy.Time.now()
-        h.frame_id = "base_link"
-        padded_pcl = pc2.create_cloud_xyz32(h, padded_points)
-        return padded_pcl
+def apply_transform(input_xyz, transform, target="xtion_rgb_optical_frame"):
+    ps = PointStamped()
+    ps.point.x = input_xyz[0]
+    ps.point.y = input_xyz[1]
+    ps.point.z = input_xyz[2]
+    ps.header.frame_id = target
+    ps.header.stamp = rospy.Time.now()
 
-rospy.init_node("test_people_pose_laser")
+    tr_point = tf2_geometry_msgs.do_transform_point(ps, transform)
+    return (tr_point.point.x, tr_point.point.y, tr_point.point.z)
 
-people_pose_pub = rospy.Publisher("/people_poses_laser", Marker, queue_size=100)
+def get_points_and_pixels_from_laser(msg):
+    """ Converts a LaserScan message to a collection of points in camera frame, with their corresponding pixel values in a flat array. The method pads out the points to add vertical "pillars" to the point cloud.
 
-rospy.wait_for_service("/yolov8/detect", rospy.Duration(15.0))
-yolo = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
-tf = rospy.ServiceProxy("/tf_transform", TfTransform)
-bridge = CvBridge()
+    Args:
+        msg (LaserScan): ROS Laser Scan message from /scan topic.
+    Returns:
+        List: A list of tuples containing all the filtered and padded points in camera frame.
+        List: A list of pixel values corresponding to the points in the first list.
+    """
+    pcl_msg = lg.LaserProjection().projectLaser(msg)
+    pcl_points = [p for p in pc2.read_points(pcl_msg, field_names=("x, y, z"), skip_nans=True)]
+    tr = get_transform("base_laser_link", "xtion_rgb_optical_frame")
+    
+    padded_converted_points = []
+    pixels = []
+    for point in pcl_points:
+        for z in np.linspace(0., 2., 5):
+            p = apply_transform((point[0], point[1], z), tr)
+            u,v = camera.project3dToPixel(p)
+            if u >= 0 and u < 640 and v >= 0 and v < 480:
+                padded_converted_points.append(p)
+                pixels.append(u)
+                pixels.append(v)
 
-idx = 0
+    return padded_converted_points, pixels
 
-while not rospy.is_shutdown():
-    lsr_scan = rospy.wait_for_message("/scan", LaserScan)
-    pcl_msg = get_pcl_from_laser(lsr_scan)
-    cv_im = pcl_msg_to_cv2(pcl_msg)
-    img_msg = bridge.cv2_to_imgmsg(cv_im)
-    detections = yolo(img_msg, "yolov8n-seg.pt", 0.3, 0.3)
-    for detection in detections.detected_objects:
-        if detection.name == "person":
-            centroid_xyz = seg_to_centroid(pcl_msg, np.array(detection.xyseg))
-            centroid = PointStamped()
-            centroid.point = Point(*centroid_xyz)
-            centroid.header = pcl_msg.header
-            tf_req = TfTransformRequest()
-            tf_req.target_frame = String("map")
-            tf_req.point = centroid
-            centroid_tf = tf(tf_req).target_point.point
-            people_pose_pub.publish(create_point_marker(centroid_tf.x, centroid_tf.y, centroid_tf.z, idx))
-            idx += 1
-            prev_xyz = centroid_xyz
+if __name__ == "__main__":
 
-    rospy.sleep(rospy.Duration(1.0))
+    rospy.init_node("test_people_pose_laser")
+
+    people_pose_pub = rospy.Publisher("/people_poses_laser", Marker, queue_size=100)
+
+    rospy.wait_for_service("/yolov8/detect", rospy.Duration(15.0))
+    yolo = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
+    shapely = rospy.ServiceProxy('/lasr_shapely/points_in_polygon_2d', PointsInPolygon2D)
+    camera = PinholeCameraModel()
+    camera.fromCameraInfo(rospy.wait_for_message("/xtion/rgb/camera_info", CameraInfo))
+    
+    try:
+        corners = rospy.get_param("/wait/cuboid")
+    except KeyError:
+        rospy.logwarn("No cuboid found in parameter server.")
+        corners = None
+
+    while not rospy.is_shutdown():
+        lsr_scan = rospy.wait_for_message("/scan", LaserScan)
+        points, pixels = get_points_and_pixels_from_laser(lsr_scan)
+        img_msg = rospy.wait_for_message("/xtion/rgb/image_raw", Image)
+        detections = yolo(img_msg, "yolov8n-seg.pt", 0.3, 0.3)
+        i = 0
+        for detection in detections.detected_objects:
+            if detection.name == "person":
+                decision = shapely(detection.xyseg, pixels)
+                #save index for every true decision
+                idx = [idx for idx, el in enumerate(decision.inside) if el]
+                filtered_points = [points[i] for i in idx]
+                if corners is not None:
+                    waiting_area = shapely(corners, filtered_points) if corners else None
+                    idx = [idx for idx, el in enumerate(waiting_area.inside) if el]
+                    points_inside_area = [filtered_points[i] for i in idx]
+                    if len(points_inside_area):
+                        point = points_inside_area[0]
+                        marker_msg = create_point_marker(point[0], point[1], point[2], i)
+                        people_pose_pub.publish(marker_msg)
+                else:
+                    for point in filtered_points:
+                        marker_msg = create_point_marker(point[0], point[1], point[2], i)
+                        people_pose_pub.publish(marker_msg)
+                
+        rospy.sleep(rospy.Duration(.1))
