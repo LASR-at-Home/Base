4f937cdecc7b2e3165e13b94fd69138829df5337
Jared Swift <j.w.swift@outlook.com>
2023-09-05 20:25:01 +0100

    feat: be clearer about our intentions.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/take_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/take_order.py
@@ -70,7 +70,7 @@ class TakeOrder(smach.State):
 
     def execute(self, userdata):
         self.context.stop_head_manager("head_manager")
-        self.context.voice_controller.sync_tts("Can I please take your order, please answer after the beep?")
+        self.context.voice_controller.sync_tts("I'm TIAGo, I'll be server today. Please state your order after the beep - this indicates that I am listening.")
         order = []
         while True:
             order.extend(self.get_order())
