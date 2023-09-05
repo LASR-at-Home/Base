ba31704e7ffb4ce35e92571dc3c3a9764788cd47
Zoe <ezoe013@gmail.com>
2023-09-05 13:14:39 +0100

    not working square

--- common/navigation/choosing_wait_position/src/choosing_wait_position/final_lift_key_point/generate_wait_box.py
+++ common/navigation/choosing_wait_position/src/choosing_wait_position/final_lift_key_point/generate_wait_box.py
@@ -1,18 +1,43 @@
 import numpy as np 
 
-def generate_lift_wait_box(corner_zero):
+
+WAITING_AREA_LONG_SIDE = 1.2
+WAITING_AREA_SHORT_SIDE = 0.6
+
+
+def generate_lift_wait_box(corner_zero,area):
     x,y = corner_zero
+    #This is the corner that if you were facing the lift, would be the left corner of the lift. 
+
+
     wait_box = np.zeros((4,2))
     print(wait_box)
+    see_wait_area_pos = [0,0]
 
-    wait_box[0] = [x,y]
-    wait_box[1] = [x-3.1,y]
-    wait_box[2] = [x-3.1,y+2.5] 
-    wait_box[3] = [x,y+2.5]
+    if (area=='606_lift'):
+        wait_box[0] = [x,y]
+        wait_box[1] = [x-3.5,y]
+        wait_box[2] = [x-3.5,y+2.7] 
+        wait_box[3] = [x,y+2.5]
 
-    return wait_box
+        see_wait_area_pos = [x,y+2.5]
+    elif(area=='6th floor lift'):
+        wait_box[0] = [x,y]
+        wait_box[1] = [x-3.5,y]
+        wait_box[2] = [x-3.5,y+2.7] 
+        wait_box[3] = [x,y+2.5]
+
+
+   
+    return wait_box, see_wait_area_pos
 
 
 
-wait_box = generate_lift_wait_box([3.9606051445,-0.823928892612]) 
+
+
+corner = [4.10421085358,-0.823522567749]
+
+
+wait_box,see_wait_area_pos = generate_lift_wait_box(corner,'606_lift')
 print(wait_box)
+print(see_wait_area_pos)
