3e518f6640808eac6ed9fcbc87614635807be4b4
Zoe <ezoe013@gmail.com>
2023-09-07 15:51:48 +0100

    cleaning up go to lift code

--- tasks/lift/src/lift/phases/phase2/states/go_to_lift.py
+++ tasks/lift/src/lift/phases/phase2/states/go_to_lift.py
@@ -24,39 +24,30 @@ class GoToLift(smach.State):
 
         width_o, height_o = image_o.size 
         image_origin = [width_o/2,height_o/2]
-
-    
-
         width_r , height_r = image_r.size
 
         x_offset, y_offset = (width_r - width_o)/2 , (height_r-height_o)/2
-      #  print("image origin: ", image_origin)
-       # print("keypoint in rotate", keypoint)
         x, y, bin = keypoint[0]
         ox, oy = image_origin
 
         tx = x - ox 
         ty = y - oy 
-        #print("new tx,ty ", (tx,ty))
-
 
         x_rotate = tx*math.cos(math.radians(degrees)) - ty*math.sin(math.radians(degrees))
         y_rotate = ty*math.cos(math.radians(degrees)) + tx*math.sin(math.radians(degrees))
 
-      #  print('x_rotate,y_rotate', (x_rotate,y_rotate))
+        #x = x_rotate + 480
+        #y = y_rotate + 640
 
-        x = x_rotate + 480
-        y = y_rotate + 640
+        x = x_rotate + ox
+        y = y_rotate + oy
         
         keypoint_rotated = [[x+x_offset,y+y_offset,1]]
         return keypoint_rotated
 
     def flip_keypoint(self, keypoint,image):
         width, height = image.size
-        width = 630
-        height = 480
-            #to flip keypoint back: (x,y) -> (x,length-y-1)
-        #print("keypoint in flip: ", keypoint)
+        #to flip keypoint back: (x,y) -> (x,length-y-1)
         keypoint = keypoint[0]
         keypoint_flipped = [[keypoint[0],(height-keypoint[1]-1)],1]
         return keypoint_flipped
@@ -70,9 +61,7 @@ class GoToLift(smach.State):
 
     def execute(self, userdata):
         # position robot in front of the lift waiting area
-
         # self.controllers.base_controller.sync_to_pose(get_pose_from_param('/wait/pose'))
-
         # do zoe's stuff
         w = Waypoint()
         print("getting lift information  {} ".format(rospy.get_param("/is_simulation")))
@@ -87,31 +76,26 @@ class GoToLift(smach.State):
         if PLOT_SAVE:
             image.save(DEBUG_PATH + "/zoe_predict_pos_test_before_rotate" + str(TEST) + ".jpg")
         
+        #Rotate the image to make sure the door is the bottom of the image when we pass to NN: 
         degrees = 90
-        #Rotate the image to make sure it's facing the door properly: 
         image_rotated = image.rotate(degrees)
         image_rotated = ImageOps.mirror(image_rotated)
 
         if PLOT_SAVE:
             image_rotated.save(DEBUG_PATH + "/zoe_predict_pos_test_after_rotate" + str(TEST) + ".jpg")
 
-
         #predicting with rotated image: 
-        
         image_path = DEBUG_PATH + "/zoe_predict_pos_test_after_rotate" + str(TEST) + ".jpg"
-        #print("image_path: ", image_path)
         bbox, keypoint = make_prediction(image_path)
         if keypoint is None and bbox is None:
             keypoint = analysis[3]
 
+        #rotating keypoint to match image: 
         keypoint = self.mirror_keypoint(keypoint,image_rotated)
         keypoint = self.rotate_keypoint(keypoint,(360-degrees),image_rotated,image)
-    #        print(f"keypoints predicted after rotate: {keypoint}")
 
         
-        #print("keypoint flipped: ", keypoint_flipped)
         visualise_predictions(warped, bbox, keypoint)
-     #   print(type(keypoint))
         
 
         global_points = w.local_to_global_points(M=M, points=keypoint, is_lift=True)
