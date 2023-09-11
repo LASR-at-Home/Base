433a689ecbd1f53548df758e783586398522b5ce
Jared Swift <j.w.swift@outlook.com>
2023-09-11 17:56:57 +0100

    fix: make order test.

--- tasks/coffee_shop/scripts/test_make_order.py
+++ tasks/coffee_shop/scripts/test_make_order.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import smach
 import rospy
-from coffee_shop.phases.phase_2.states import MakeOrder, CheckOrder, InvalidateOrder, LoadOrder, WaitForOrder
+from coffee_shop.phases.phase_2.states import MakeOrder, CheckOrder, LoadOrder, WaitForOrder
 from coffee_shop.context import Context
 import sys
 
 if __name__ == "__main__":
-    rospy.init_node("test_check_table")
+    rospy.init_node("test_make_order")
     sm = smach.StateMachine(outcomes=['end'])
     context = Context(sys.argv[1])
     context.current_table = "table0"
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     with sm:
         sm.add('MAKE_ORDER', MakeOrder(context), transitions={'done' : 'WAIT_FOR_ORDER'})
-        smach.StateMachine.add('WAIT_FOR_ORDER', WaitForOrder(context), transitions={'done': 'CHECK_ORDER'})
+        sm.add('WAIT_FOR_ORDER', WaitForOrder(context), transitions={'done': 'CHECK_ORDER'})
         sm.add('CHECK_ORDER', CheckOrder(context), transitions={'correct': 'LOAD_ORDER', 'incorrect': 'WAIT_FOR_ORDER'})
         sm.add('LOAD_ORDER', LoadOrder(context), transitions={'done':'end'})
 
