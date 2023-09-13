889f4b4a45bbb3e81d828f76e406dcd3f69fdc1a
Jared Swift <j.w.swift@outlook.com>
2023-09-13 11:40:59 +0100

    feat: wait and more 'I am finished'

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/load_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/load_order.py
@@ -3,6 +3,7 @@ import smach
 import numpy as np
 from play_motion_msgs.msg import PlayMotionGoal
 import json
+import rospy
 
 class LoadOrder(smach.State):
 
@@ -11,18 +12,12 @@ class LoadOrder(smach.State):
         self.context = context
 
     def execute(self, userdata):
+        self.context.voice_controller.sync_tts("That's the right order, I'll turn around so that you can load it")
         self.context.base_controller.rotate(np.pi)
         pm_goal = PlayMotionGoal(motion_name="load_unload", skip_planning=True)
         self.context.play_motion_client.send_goal_and_wait(pm_goal)
-        self.context.voice_controller.sync_tts("Please load the order and say `finished` when you are ready for me to deliver it.")
-        resp = self.context.speech(True)
-        while True:
-            if not resp.success:
-                continue
-            resp = json.loads(resp.json_response)
-            if "finished" in resp["text"].lower():
-                break
-            resp = self.context.speech(False)
+        self.context.voice_controller.sync_tts("I'll give you some time to load the order...")
+        rospy.sleep(rospy.Duration(10.0))
         pm_goal = PlayMotionGoal(motion_name="back_to_default", skip_planning=True)
         self.context.play_motion_client.send_goal_and_wait(pm_goal)
         self.context.start_head_manager("head_manager", '')
