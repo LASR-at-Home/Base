4d9e2e47cad8fe13a23059cbcb407f9abb1e6185
jws-1 <j.w.swift@outlook.com>
2023-09-14 16:43:55 +0100

    feat: change wake word and difflib the output.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/wait_for_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/wait_for_order.py
@@ -3,6 +3,7 @@ import smach
 import json
 import rospy
 from play_motion_msgs.msg import PlayMotionGoal
+import difflib
 
 
 class WaitForOrder(smach.State):
@@ -16,9 +17,14 @@ class WaitForOrder(smach.State):
         self.context.play_motion_client.send_goal_and_wait(pm_goal)
         resp = self.context.speech(True)
         resp = json.loads(resp.json_response)
+        ready = False
         while True:
             rospy.loginfo(resp)
-            if "finish" in resp["text"].lower():
+            for word in resp["text"].lower().split(" "):
+                if difflib.get_close_matches(word, ["check", "order"]):
+                    ready = True
+                    break
+            if ready:
                 break
             resp = self.context.speech(False)
             resp = json.loads(resp.json_response)
