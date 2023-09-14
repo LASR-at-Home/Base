6efc4c4f13fcacd0a68fe045e9d4ba61c0c618f2
Jared Swift <j.w.swift@outlook.com>
2023-09-14 20:00:03 +0100

    feat: simplify things...

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/wait_for_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/wait_for_order.py
@@ -4,7 +4,7 @@ import json
 import rospy
 from play_motion_msgs.msg import PlayMotionGoal
 import difflib
-
+from lasr_speech_recognition_common.msg import Transcription
 
 class WaitForOrder(smach.State):
 
@@ -12,20 +12,27 @@ class WaitForOrder(smach.State):
         smach.StateMachine.__init__(self, outcomes=['done'])
         self.context = context
 
+    def listen(self):
+        resp = self.context.speech(True)
+        if not resp.success:
+            self.context.voice_controller.sync_tts(self.context.get_random_retry_utterance())
+            return self.listen()
+        resp = json.loads(resp.json_response)
+        rospy.loginfo(resp)
+        return resp
+
+    def affirm(self):
+        resp = self.listen()
+        if resp["intent"]["name"] not in ["affirm", "deny"]:
+            self.context.voice_controller.sync_tts(self.context.get_random_retry_utterance())
+            return self.affirm()
+        return resp["intent"]["name"] == "affirm"
+
     def execute(self, userdata):
         pm_goal = PlayMotionGoal(motion_name="back_to_default", skip_planning=True)
         self.context.play_motion_client.send_goal_and_wait(pm_goal)
-        resp = self.context.speech(True)
-        resp = json.loads(resp.json_response)
-        ready = False
         while True:
-            rospy.loginfo(resp)
-            for word in resp["text"].lower().split(" "):
-                if difflib.get_close_matches(word, ["check", "order"]):
-                    ready = True
-                    break
-            if ready:
-                break
-            resp = self.context.speech(False)
-            resp = json.loads(resp.json_response)
-        return 'done'
+            rospy.sleep(rospy.Duration(5.0))
+            self.context.voice_controller.sync_tts("Is the order ready to be checked? Please answer with yes or no.")
+            if self.affirm():
+                return 'done'
