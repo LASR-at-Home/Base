20d0412e68abfc1f07dac35b2da1c2dc2159a430
jws-1 <j.w.swift@outlook.com>
2023-09-15 20:24:50 +0100

    Revert "feat: distinguish loading and unloading positions"
    This reverts commit 7576f7dd5747d036d14a39bd8bbdf1cb4a4dabc8.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/load_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/load_order.py
@@ -12,11 +12,7 @@ class LoadOrder(smach.State):
         self.context = context
 
     def execute(self, userdata):
-        location = rospy.get_param("/counter/load_location")
-        position = location["position"]
-        orientation = location["orientation"]
-        self.context.voice_controller.sync_tts("That's the right order, I'll position myself so that it can be loaded")
-        self.context.base_controller.sync_to_pose(Pose(position=Point(**position), orientation=Quaternion(**orientation)))
+        self.context.voice_controller.sync_tts("That's the right order, I'll turn around so that you can load it")
         self.context.base_controller.rotate(np.pi)
         pm_goal = PlayMotionGoal(motion_name="load_unload", skip_planning=True)
         self.context.play_motion_client.send_goal_and_wait(pm_goal)
