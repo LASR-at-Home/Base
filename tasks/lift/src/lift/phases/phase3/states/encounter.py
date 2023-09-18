c81a078357ab79bda7309de10a1e4268f1a08f06
fireblonde <nicollehchevska@gmail.com>
2023-09-18 11:17:20 +0100

    MK: finished y

--- tasks/lift/src/lift/phases/phase3/states/encounter.py
+++ tasks/lift/src/lift/phases/phase3/states/encounter.py
@@ -1,65 +1,41 @@
 #!/usr/bin/env python3
-
-import rospy
+import os
 import smach
-from lasr_object_detection_yolo.detect_objects_v8 import detect_objects
-from tiago_controllers.controllers.look_at import LookAt
-
+from pal_startup_msgs.srv import StartupStart, StartupStop
+import rosservice
+import rospy
+from geometry_msgs.msg import Point, PointStamped
+from std_msgs.msg import String
+import numpy as np
+import ros_numpy as rnp
+from object_interest_tracking.srv import  Tdr
+from geometry_msgs.msg import Pose
 
 HORIZONTAL = 0.8
 VERTICAL = 0.3
 class Encounter(smach.State):
-    def __init__(self, controllers, voice, cmd):
-        smach.State.__init__(self, outcomes=['success', 'failed'])
-        self.voice = voice
-        self.controllers = controllers
-        self.cmd_vel = cmd
-        self.controllers.head_controller.sync_reach_to(0, 0)  # start by centering the head
-        self.search_points = [(-1 * HORIZONTAL, VERTICAL),
-                              (0, VERTICAL),
-                              (HORIZONTAL, VERTICAL),
-                              (HORIZONTAL, 0),
-                              (0, 0),
-                              (-1 * HORIZONTAL, 0),
-                              (-1 * HORIZONTAL, -1 * VERTICAL),
-                              (0, -1 * VERTICAL),
-                              (HORIZONTAL, -1 * VERTICAL),
-                              (0, 0)]
+    def __init__(self, default):
+        smach.State.__init__(self, outcomes=['success'])
+        
+        self.default = default
 
-    def search_for_person(self):
-        for point in self.search_points:
-            self.controllers.head_controller.sync_reach_to(point[0], point[1])
-            people = detect_objects(["person"])
-            if people:
-                return people[0], point[0]
-        return None
-
+        # stop head manager
+        if "/pal_startup_control/stop" in rosservice.get_service_list():
+            self.stop_head_manager = rospy.ServiceProxy("/pal_startup_control/stop", StartupStop)
+            self.start_head_manager = rospy.ServiceProxy("/pal_startup_control/start", StartupStart)
 
     def execute(self, userdata):
+        # result = self.controllers.base_controller.sync_to_pose(get_pose_from_param('/lift/pose'))
+        # get the point from zoe
 
+        self.stop_head_manager()
 
+        self.voice.speak("Hi")
+        rospy.wait_for_service('/v2')
+        rospy.ServiceProxy('/v2', Tdr)()
+        self.default.voice.speak("Hi mate, nice to meet you, you Look great today. I have just arrived at the second floor.")
 
-
-
-
-
-
-
-
-
-        turns = 4
-        for i in range(turns):
-            try:
-                closest_person, head_rot = self.search_for_person()
-                if closest_person:
-                    look_at = LookAt(self.controllers.head_controller, self.controllers.base_controller, self.cmd_vel, "person")
-                    look_at.look_at(closest_person.xywh, head_rot)
-                    return 'success'
-            except TypeError:
-                self.cmd_vel.rotate(60, 360/turns, True)
-
-        self.voice.speak("I can't see anyone!")
-        return 'failed'
+        return 'success'
 
 
 if __name__ == '__main__':
