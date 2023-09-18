f5dd7e84b54ba74e8041e1485f34ada6712c21f5
jws-1 <j.w.swift@outlook.com>
2023-09-18 13:51:47 +0100

    Revert "MAYBE REVERT: move base recovery"
    This reverts commit c093eed3b5b9b200255e0f6698113ffaca89ba1d.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_1/phase_1.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_1/phase_1.py
@@ -10,5 +10,5 @@ class Phase1(smach.StateMachine):
             smach.StateMachine.add('START_PHASE_1', Start(context), transitions={'done' : 'GO_TO_TABLE'})
            # smach.StateMachine.add('GO_TO_TABLE', GoToPreTable(context), transitions={'done' : 'GO_TO_TABLE'})
             #smach.StateMachine.add('PRE_CHECK_TABLE', PreCheckTable(context), transitions={'done' : 'GO_TO_TABLE'})
-            smach.StateMachine.add('GO_TO_TABLE', GoToTable(context), transitions={'done' : 'CHECK_TABLE', 'not done' : 'GO_TO_TABLE'})
+            smach.StateMachine.add('GO_TO_TABLE', GoToTable(context), transitions={'done' : 'CHECK_TABLE'})
             smach.StateMachine.add('CHECK_TABLE', CheckTable(context), transitions={'not_finished' : 'GO_TO_TABLE', 'finished' : 'done'})
