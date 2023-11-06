d3de21e9a93484b16fbc791b64d521986aa0bca5
Jared Swift <j.w.swift@outlook.com>
2023-11-06 10:34:44 +0000

    fix: wait for person in area.

--- skills/src/lasr_skills/wait_for_person_in_area.py
+++ skills/src/lasr_skills/wait_for_person_in_area.py
@@ -20,16 +20,16 @@ class WaitForPersonInArea(smach.StateMachine):
     class CheckForPerson(smach.State):
 
         def __init__(self):
-            smach.State.__init__(self, outcomes=['done', 'not_done'], input_keys=['people_detections'])
+            smach.State.__init__(self, outcomes=['done', 'not_done'], input_keys=['people_detections_3d'])
 
         def execute(self, userdata):
-            if len(userdata.people_detections):
+            if len(userdata.people_detections_3d):
                 return 'done'
             else:
-                return 'not done'
+                return 'not_done'
             
     def __init__(self):
-        smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['area_polygon'], output_keys=['people_detections'])
+        smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['area_polygon'], output_keys=['people_detections_3d'])
 
 
         with self:
@@ -41,16 +41,26 @@ if __name__ == "__main__":
     rospy.init_node("test_wait_for_person")
     sm = smach.StateMachine(outcomes=['end'])
 
+    class MockA(smach.State):
+
+        def __init__(self):
+            smach.State.__init__(self, outcomes=['1'], output_keys=['area_polygon'])
+
+        def execute(self, userdata):
+            userdata.area_polygon = [[1.9, 2.8], [3.17, 2.95], [1.69, 4.01], [2.88, 4.26]]
+            return '1'
+
     class MockC(smach.State):
 
         def __init__(self):
-            smach.State.__init__(self, outcomes=['1'], input_keys=['people_detections'])
+            smach.State.__init__(self, outcomes=['1'], input_keys=['people_detections_3d'])
 
         def execute(self, userdata):
-            print(userdata.filtered_people_detections)
+            print(userdata.people_detections_3d)
             return '1'
 
     with sm:
+        sm.add('A', MockA(), transitions={'1' : 'WAIT_FOR_PERSON'})
         sm.add('WAIT_FOR_PERSON', WaitForPersonInArea(), transitions={'succeeded' : 'C', 'failed' : 'end'})
         sm.add('C', MockC(), transitions={'1' : 'end'})
 
