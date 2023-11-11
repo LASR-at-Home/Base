6214e409f71d87e8028695a1f6ab897a4733c088
Paul Makles <me@insrt.uk>
2023-11-11 13:16:31 +0000

    refactor(bodypix): clean up the test code

--- common/vision/lasr_vision_bodypix/scripts/test-face.py
+++ common/vision/lasr_vision_bodypix/scripts/test-face.py
@@ -16,37 +16,20 @@ while cap.isOpened():
     success, frame = cap.read()
 
     if success:
-        # im = Image.fromarray(frame, 'RGB')
-        # img_array = np.array(im)
-
-        # expand dimensions to match the 4D Tensor shape
-        # img_array = np.expand_dims(img_array, axis=0)
-        # img_array = tf.keras.preprocessing.image.img_to_array(img_array)
-
+        # run prediction on camera frame
         result = bodypix_model.predict_single(frame)
+
+        # extract mask with minimum confidence
         mask = result.get_mask(threshold=0.75)
+
+        # generate mask of only face
         mask2 = result.get_part_mask(mask=mask, part_names=['left_face', 'right_face']).squeeze()
 
-        # np.save("yay", mask2)
-        # mask2 = (1 - mask2.squeeze())
-        # mask2 = mask2.squeeze()
-
-        # mask_image = Image.fromarray(mask2.astype(    .uint8))
-        # mask2 = mask2.numpy()
-        # print(mask2.shape)
-        #cv2.imshow("gghd", mask_image)
-
-        # height, width, _ = frame.shape
-        # object_image = np.zeros((height, width, 4), dtype=np.uint8)
-        # object_image[:,:,0:3] = frame
-        # object_image[:, :, 3] = mask2
-        
-        # mask2_resized = cv2.resize(mask2.squeeze(), (frame.shape[1], frame.shape[0]))
-        # cv2.bitwise_and(frame, mask2)
-
-        # print(frame.shape, mask2.shape)
+        # mask out non-face areas
         frame[mask2 == 0] = 0
-        cv2.imshow("ogjgfijhogf", frame)
+
+        # preview
+        cv2.imshow("Preview", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
--- common/vision/lasr_vision_bodypix/scripts/test.py
+++ common/vision/lasr_vision_bodypix/scripts/test.py
@@ -12,6 +12,7 @@ cap = cv2.VideoCapture(0)
 
 # load model
 bodypix_model = load_model(
+    # uncomment to use MobileNet instead:
     # download_model(BodyPixModelPaths.MOBILENET_FLOAT_50_STRIDE_16)
     download_model(BodyPixModelPaths.RESNET50_FLOAT_STRIDE_16)
 )
@@ -20,67 +21,27 @@ while cap.isOpened():
     success, frame = cap.read()
 
     if success:
-        # im = Image.fromarray(frame, 'RGB')
-        # img_array = np.array(im)
-
-        # expand dimensions to match the 4D Tensor shape
-        # img_array = np.expand_dims(img_array, axis=0)
-        # img_array = tf.keras.preprocessing.image.img_to_array(img_array)
-
+        # run prediction on camera frame
         result = bodypix_model.predict_single(frame)
-        mask = result.get_mask(threshold=0.75)
-        mask2 = result.get_part_mask(mask=mask, part_names=['torso_front', 'torso_back']).squeeze()
-        
-        aaa = result.get_colored_part_mask(mask).astype(np.uint8)
-        from tf_bodypix.draw import draw_poses  # utility function using OpenCV
 
+        # extract mask with minimum confidence
+        mask = result.get_mask(threshold=0.75)
+        
+        # generate coloured mask
+        coloured_mask = result.get_colored_part_mask(mask).astype(np.uint8)
+        
+        # draw poses on top
+        from tf_bodypix.draw import draw_poses
         poses = result.get_poses()
-        aaa = draw_poses(
-            aaa.copy(),  # create a copy to ensure we are not modifing the source image
+        coloured_mask = draw_poses(
+            coloured_mask.copy(),
             poses,
             keypoints_color=(255, 100, 100),
             skeleton_color=(100, 100, 255),
         )
 
-        # cv2_img = cv2.cvtColor(aaa, cv2.COLOR_RGB2BGR)
-        # aaa_im = np.array(Image.fromarray(aaa, 'RGB'))
-
-        # np.save("yay", mask2)
-        # mask2 = (1 - mask2.squeeze())
-        # mask2 = mask2.squeeze()
-
-        # mask_image = Image.fromarray(mask2.astype(    .uint8))
-        # mask2 = mask2.numpy()
-        # print(mask2.shape)
-        #cv2.imshow("gghd", mask_image)
-
-        # height, width, _ = frame.shape
-        # object_image = np.zeros((height, width, 4), dtype=np.uint8)
-        # object_image[:,:,0:3] = frame
-        # object_image[:, :, 3] = mask2
-        
-        # mask2_resized = cv2.resize(mask2.squeeze(), (frame.shape[1], frame.shape[0]))
-        # cv2.bitwise_and(frame, mask2)
-
-        # print(frame.shape, mask2.shape)
-        frame[mask2 == 0] = 0
-
-        # HISTORGRAM!!
-        # red, green, blue = frame[:,:,0].flatten(), frame[:,:,1].flatten(), frame[:,:,2].flatten()
-        # plt.figure(figsize=(12, 12))
-        # plt.hist(red,   bins=256, density=False, color='red', alpha=0.5)
-        # plt.hist(green, bins=256, density=False, color='green', alpha=0.4)
-        # plt.hist(blue,  bins=256, density=False, color='blue', alpha=0.3)
-
-        # plt.xticks(ticks=np.linspace(0, 1, 17), labels=range(0, 257, 16))
-
-        # plt.title("hgfhgdf")
-        # plt.ylabel("count")
-        # plt.xlabel("intensity")
-
-        # plt.show()
-
-        cv2.imshow("ogjgfijhogf", aaa)
+        # preview
+        cv2.imshow("Preview", coloured_mask)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
