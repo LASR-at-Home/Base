16e25028f2b6445878451f2fcf443192b1fdff6c
Jared Swift <j.w.swift@outlook.com>
2023-11-13 14:27:46 +0000

    WIP: look for place to seat people.

--- tasks/receptionist/src/receptionist/sm.py
+++ tasks/receptionist/src/receptionist/sm.py
@@ -5,6 +5,7 @@ from receptionist import AskForDrink
 from receptionist import End
 from receptionist import Default
 from receptionist import GoToPerson
+from receptionist import GoToSeatingArea
 from receptionist import LookForSeats
 from tasks import GoToWaitForPerson
 from lasr_skills import WaitForPersonInArea
@@ -18,12 +19,10 @@ class Receptionist(smach.StateMachine):
         self.userdata.area_polygon = [[1.94, 0.15], [2.98, 0.28], [3.08, -0.68], [2.06, -0.84]]
         with self:
             smach.StateMachine.add('START', Start(self.default), transitions={'succeeded' : 'GO_TO_WAIT_FOR_PERSON'})
-            smach.StateMachine.add('LOOK_FOR_SEATS', LookForSeats(), transitions={'succeeded' : 'GO_TO_WAIT_FOR_PERSON', 'failed' : 'failed'})
             smach.StateMachine.add("GO_TO_WAIT_FOR_PERSON",GoToWaitForPerson(self.default), transitions={'succeeded': 'WAIT_FOR_PERSON'})
             smach.StateMachine.add('WAIT_FOR_PERSON', WaitForPersonInArea() ,transitions={'succeeded' : 'GO_TO_PERSON', 'failed' : 'failed'})
             smach.StateMachine.add('GO_TO_PERSON', GoToPerson(self.default),transitions={'succeeded':'ASK_FOR_DRINK'})
-            smach.StateMachine.add('ASK_FOR_DRINK', AskForDrink(self.default),transitions={'failed':'ASK_FOR_DRINK','waitforguest2':'GO_TO_WAIT_FOR_PERSON','succeeded':'END'})
-            #Next part: smach.StateMachine.add('TAKE_TO_SEAT, TakeToSeat(self.default),transistions={'failed':'TAKE_TO_SEAT, 'succeeded':'succeeded'})
-            smach.StateMachine.add('END',End(self.default),transitions={'succeeded':'succeeded'})
-
-
+            smach.StateMachine.add('ASK_FOR_DRINK', AskForDrink(self.default),transitions={'failed':'ASK_FOR_DRINK','succeeded':'GO_TO_SEATING_AREA'})
+            smach.StateMachine.add('GO_TO_SEATING_AREA', GoToSeatingArea(self.default), transitions={'succeeded' : 'LOOK_FOR_SEATS'})
+            smach.StateMachine.add('LOOK_FOR_SEATS', LookForSeats(self.default), transitions={'succeeded' : 'END'})
+            smach.StateMachine.add('END', End(self.default),transitions={'succeeded':'succeeded'})
--- tasks/receptionist/src/receptionist/states/look_for_seats.py
+++ tasks/receptionist/src/receptionist/states/look_for_seats.py
@@ -1,4 +1,53 @@
+#!/usr/bin/env python3
+
 import smach
+from play_motion_msgs.msg import PlayMotionGoal
+
+from lasr_skills import DetectObjects3D
 
 class LookForSeats(smach.StateMachine):
-    pass
+
+
+    class Look(smach.State):
+
+        def __init__(self, default):
+            smach.State.__init__(self, outcomes=['done', 'not_done'], input_keys=['look_motion'])
+            self.default = default
+            self.motions = ['look_left', 'look_right', 'look_center']
+
+        def execute(self, userdata):
+            if not self.motions:
+                return 'done'
+            pm_goal = PlayMotionGoal(motion_name=self.motions.pop(0), skip_planning=True)
+            self.default.pm.send_goal_and_wait(pm_goal)
+            return 'not_done'
+
+    class ProcessDetections(smach.State):
+
+        def __init__(self):
+            smach.State.__init__(self, outcomes=['succeeded'], input_keys=['detections_3d', 'bulk_detections_3d'], output_keys=['bulk_detections_3d'])
+        
+        def execute(self, userdata):
+            userdata.bulk_detections_3d.extend(userdata.detections_3d)
+            print(userdata.bulk_detections_3d)
+            return 'succeeded'
+
+    def __init__(self, default):
+        smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed'])
+        self.default = default
+        self.userdata.filter = ["person", "chair"]
+        self.userdata.bulk_detections_3d = []
+        self.userdata.depth_topic = "xtion/depth_registered/points"
+
+        with self:
+            smach.StateMachine.add('LOOK', self.Look(self.default), transitions={'not_done' : 'DETECT_OBJECTS_3D', 'done' : 'succeeded'})
+            smach.StateMachine.add('DETECT_OBJECTS_3D', DetectObjects3D(), transitions={'succeeded' : 'PROCESS_DETECTIONS', 'failed' : 'failed'})
+            smach.StateMachine.add('PROCESS_DETECTIONS', self.ProcessDetections(), transitions={'succeeded' : 'LOOK'})
+
+if __name__ == "__main__":
+    import rospy
+    from receptionist import Default
+    rospy.init_node("test_look_for_seats")
+    default = Default()
+    sm = LookForSeats(default)
+    sm.execute()
