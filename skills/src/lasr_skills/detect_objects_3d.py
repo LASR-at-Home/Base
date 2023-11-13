52851fd2389befbbefab279086a9a0de27f35fa5
Jared Swift <j.w.swift@outlook.com>
2023-11-13 11:26:05 +0000

    refactor: let the generic perception skills acquire their own sensor input (image/pcl).

--- skills/src/lasr_skills/detect_objects_3d.py
+++ skills/src/lasr_skills/detect_objects_3d.py
@@ -2,19 +2,22 @@
 import rospy
 import smach
 
-from lasr_vision_msgs.srv import YoloDetection
 from common_math import pcl_msg_to_cv2, seg_to_centroid
 from cv_bridge3 import CvBridge
 from tf_module.srv import TfTransform, TfTransformRequest
 from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped, Point
+from sensor_msgs.msg import PointCloud2
 import numpy as np
+
+from lasr_vision_msgs.srv import YoloDetection
+
 
 
 class DetectObjects3D(smach.State):
 
     def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['pcl_msg', 'filter'], output_keys=['detections_3d'])
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['depth_topic', 'filter'], output_keys=['detections_3d'])
         self.yolo = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
         self.tf = rospy.ServiceProxy("/tf_transform", TfTransform)
         self.bridge = CvBridge()
@@ -31,12 +34,13 @@ class DetectObjects3D(smach.State):
         return np.array([response.target_point.point.x, response.target_point.point.y, response.target_point.point.z])
 
     def execute(self, userdata):
+        pcl_msg = rospy.wait_for_message(userdata.depth_topic, PointCloud2)
         try:
-            cv_im = pcl_msg_to_cv2(userdata.pcl_msg)
+            cv_im = pcl_msg_to_cv2(pcl_msg)
             img_msg = self.bridge.cv2_to_imgmsg(cv_im)
             result = self.yolo(img_msg, "yolov8n-seg.pt", 0.5, 0.3)
             result.detected_objects = [det for det in result.detected_objects if det.name in userdata.filter]
-            result = [(detection, self.estimate_pose(userdata.pcl_msg, detection)) for detection in result.detected_objects]
+            result = [(detection, self.estimate_pose(pcl_msg, detection)) for detection in result.detected_objects]
             userdata.detections_3d = result
             return 'succeeded'
         except rospy.ServiceException as e:
