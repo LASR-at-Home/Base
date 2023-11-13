2935e0df958ab3aa2921142e3503870403fd2b83
Jared Swift <j.w.swift@outlook.com>
2023-11-13 14:27:30 +0000

    feat: always publish markers for debugging, when doing 3D point estimation.

--- skills/src/lasr_skills/detect_objects_3d.py
+++ skills/src/lasr_skills/detect_objects_3d.py
@@ -8,6 +8,8 @@ from tf_module.srv import TfTransform, TfTransformRequest
 from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped, Point
 from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import PointStamped
+from visualization_msgs.msg import Marker
 import numpy as np
 
 from lasr_vision_msgs.srv import YoloDetection
@@ -21,6 +23,9 @@ class DetectObjects3D(smach.State):
         self.yolo = rospy.ServiceProxy('/yolov8/detect', YoloDetection)
         self.tf = rospy.ServiceProxy("/tf_transform", TfTransform)
         self.bridge = CvBridge()
+        self.n_published_markers = 0
+        self.marker_point_pub = rospy.Publisher("/detect_objects_3d/points", Marker)
+        self.marker_text_pub = rospy.Publisher("/detect_objects_3d/labels", Marker)
 
     def estimate_pose(self, pcl_msg, detection):
         centroid_xyz = seg_to_centroid(pcl_msg, np.array(detection.xyseg))
@@ -41,6 +46,47 @@ class DetectObjects3D(smach.State):
             result = self.yolo(img_msg, "yolov8n-seg.pt", 0.5, 0.3)
             result.detected_objects = [det for det in result.detected_objects if det.name in userdata.filter]
             result = [(detection, self.estimate_pose(pcl_msg, detection)) for detection in result.detected_objects]
+            for detection, point in result:
+                marker_point_msg = Marker()
+                marker_point_msg.header.frame_id = "map"
+                marker_point_msg.header.stamp = rospy.Time.now()
+                marker_point_msg.id = self.n_published_markers
+                marker_point_msg.type = Marker.SPHERE
+                marker_point_msg.action = Marker.ADD
+                marker_point_msg.pose.position.x = point[0]
+                marker_point_msg.pose.position.y = point[1]
+                marker_point_msg.pose.position.z = point[2]
+                marker_point_msg.color.r = 1.0
+                marker_point_msg.color.g = 0.0
+                marker_point_msg.color.b = 0.0
+                marker_point_msg.scale.x = 0.1
+                marker_point_msg.scale.y = 0.1
+                marker_point_msg.scale.z = 0.1
+                marker_point_msg.color.a = 1.0
+                self.marker_point_pub.publish(marker_point_msg)
+
+                marker_text_msg = Marker()
+                marker_text_msg.header.frame_id = "map"
+                marker_text_msg.header.stamp = rospy.Time.now()
+                marker_text_msg.id = self.n_published_markers
+                marker_text_msg.type = Marker.TEXT_VIEW_FACING
+                marker_text_msg.action = Marker.ADD
+                marker_text_msg.pose.position.x = point[0]
+                marker_text_msg.pose.position.y = point[1]
+                marker_text_msg.pose.position.z = point[2] + 0.15
+                marker_text_msg.text = detection.name
+                marker_text_msg.color.r = 1.0
+                marker_text_msg.color.g = 0.0
+                marker_text_msg.color.b = 0.0
+                marker_text_msg.scale.x = 0.1
+                marker_text_msg.scale.y = 0.1
+                marker_text_msg.scale.z = 0.1
+                marker_text_msg.color.a = 1.0
+                self.marker_text_pub.publish(marker_text_msg)
+
+
+                self.n_published_markers += 1
+
             userdata.detections_3d = result
             return 'succeeded'
         except rospy.ServiceException as e:
