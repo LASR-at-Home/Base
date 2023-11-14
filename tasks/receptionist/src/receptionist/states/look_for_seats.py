733eb1d4b624fb84bae54be2a530eec425a78c7e
Jared Swift <j.w.swift@outlook.com>
2023-11-14 11:45:44 +0000

    feat: look for chair and seat guest.

--- tasks/receptionist/src/receptionist/states/look_for_seats.py
+++ tasks/receptionist/src/receptionist/states/look_for_seats.py
@@ -2,9 +2,11 @@
 
 import smach
 import rospy
+import numpy as np
 from play_motion_msgs.msg import PlayMotionGoal
-
-from lasr_skills import DetectObjects3D
+from geometry_msgs.msg import Point
+from shapely.geometry import Polygon
+from lasr_skills import DetectObjects3D, LookToPoint
 
 class LookForSeats(smach.StateMachine):
 
@@ -14,7 +16,7 @@ class LookForSeats(smach.StateMachine):
         def __init__(self, default):
             smach.State.__init__(self, outcomes=['done', 'not_done'], input_keys=['look_motion'])
             self.default = default
-            self.motions = ['look_left', 'look_right', 'look_center']
+            self.motions = ['look_down_center']
 
         def execute(self, userdata):
             if not self.motions:
@@ -27,24 +29,56 @@ class LookForSeats(smach.StateMachine):
     class ProcessDetections(smach.State):
 
         def __init__(self):
-            smach.State.__init__(self, outcomes=['succeeded'], input_keys=['detections_3d', 'bulk_detections_3d'], output_keys=['bulk_detections_3d'])
+            smach.State.__init__(self, outcomes=['succeeded'], input_keys=['detections_3d', 'bulk_people_detections_3d', 'bulk_seat_detections_3d'], output_keys=['bulk_people_detections_3d', 'bulk_seat_detections3d'])
         
         def execute(self, userdata):
-            userdata.bulk_detections_3d.extend(userdata.detections_3d)
-            print(userdata.bulk_detections_3d)
+            for det in userdata.detections_3d:
+                if det[0].name == "person":
+                    userdata.bulk_people_detections_3d.append(det)
+                else:
+                    userdata.bulk_seat_detections_3d.append(det)
             return 'succeeded'
+
+    class CheckSeat(smach.State):
+        def __init__(self, default):
+            smach.State.__init__(self, outcomes=['not_done', 'succeeded', 'failed'],input_keys=['detections_3d', 'bulk_people_detections_3d', 'bulk_seat_detections_3d'], output_keys=['free_seat', 'bulk_people_detections_3d', 'bulk_seat_detections3d', 'point'])
+            self.default = default
+
+        def is_person_sitting_in_chair(self, person_contours, chair_contours):
+            return Polygon(person_contours).intersects(Polygon(chair_contours))
+
+        def execute(self, userdata):
+            if not userdata.bulk_seat_detections_3d:
+                return 'failed'
+            det1, p1 = userdata.bulk_seat_detections_3d.pop(0)
+            if not userdata.bulk_people_detections_3d:
+                userdata.free_seat = [det1, p1]
+                userdata.point = Point(*p1)
+                print(f"Chair at {p1} is free!")
+                return 'succeeded'
+            for (det2, p2) in userdata.bulk_people_detections_3d:
+                if not self.is_person_sitting_in_chair(np.array(det2.xyseg).reshape(-1, 2), np.array(det1.xyseg).reshape(-1, 2)):
+                    userdata.free_seat = [det1, p1]
+                    userdata.point = Point(*p1)
+                    print(f"Chair at {p1} is free!")
+                    self.default.voice.speak("Please sit down on this chair")
+                    return 'succeeded'
+            return 'not_done'
 
     def __init__(self, default):
         smach.StateMachine.__init__(self, outcomes=['succeeded', 'failed'])
         self.default = default
         self.userdata.filter = ["person", "chair"]
-        self.userdata.bulk_detections_3d = []
+        self.userdata.bulk_people_detections_3d = []
+        self.userdata.bulk_seat_detections_3d = []
         self.userdata.depth_topic = "xtion/depth_registered/points"
 
         with self:
-            smach.StateMachine.add('LOOK', self.Look(self.default), transitions={'not_done' : 'DETECT_OBJECTS_3D', 'done' : 'succeeded'})
+            smach.StateMachine.add('LOOK', self.Look(self.default), transitions={'not_done' : 'DETECT_OBJECTS_3D', 'done' : 'CHECK_SEAT'})
             smach.StateMachine.add('DETECT_OBJECTS_3D', DetectObjects3D(), transitions={'succeeded' : 'PROCESS_DETECTIONS', 'failed' : 'failed'})
             smach.StateMachine.add('PROCESS_DETECTIONS', self.ProcessDetections(), transitions={'succeeded' : 'LOOK'})
+            smach.StateMachine.add('CHECK_SEAT', self.CheckSeat(self.default), transitions={'succeeded' : 'FINALISE_SEAT', 'failed' : 'failed', 'not_done': 'CHECK_SEAT'})
+            smach.StateMachine.add('FINALISE_SEAT', LookToPoint(), transitions={'succeeded' : 'succeeded'})
 
 if __name__ == "__main__":
     from receptionist import Default
