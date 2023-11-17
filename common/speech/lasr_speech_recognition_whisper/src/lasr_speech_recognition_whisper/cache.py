4e6d03f7198943e1fb45053a07c52a5d3000f344
Jared Swift <j.w.swift@outlook.com>
2023-11-17 14:18:07 +0000

    feat: add option to specify device to use for whisper.

--- common/speech/lasr_speech_recognition_whisper/src/lasr_speech_recognition_whisper/cache.py
+++ common/speech/lasr_speech_recognition_whisper/src/lasr_speech_recognition_whisper/cache.py
@@ -4,7 +4,7 @@ import rospy
 # Keep all loaded models in memory
 MODEL_CACHE = {}
 
-def load_model(name: str):
+def load_model(name: str, device: str = 'cpu'):
     '''
     Load a given Whisper model
     '''
@@ -12,6 +12,6 @@ def load_model(name: str):
 
     if name not in MODEL_CACHE:
         rospy.loginfo(f'Load model {name}')
-        MODEL_CACHE[name] = whisper.load_model(name)
+        MODEL_CACHE[name] = whisper.load_model(name, device=device)
     
     return MODEL_CACHE[name]
