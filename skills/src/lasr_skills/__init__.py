a5349afb4f2d32c35e73e1a14ba23e6016f49b7d
fireblonde <nicollehchevska@gmail.com>
2023-12-04 18:15:44 +0000

    fix: the rosparam calls

--- skills/src/lasr_skills/__init__.py
+++ skills/src/lasr_skills/__init__.py
@@ -7,3 +7,5 @@ from .detect_people_in_area_3d import DetectPeopleInArea3D
 from .wait_for_person import WaitForPerson
 from .wait_for_person_in_area import WaitForPersonInArea
 from .look_to_point import LookToPoint
+from .go_to_location import GoToLocation
+from .go_to_semantic_location import GoToSemanticLocation
--- skills/src/lasr_skills/go_to_location.py
+++ skills/src/lasr_skills/go_to_location.py
@@ -3,6 +3,7 @@
 import rospy
 import smach
 from tiago_controllers.controllers import Controllers
+from geometry_msgs.msg import Point, Quaternion, Pose
 
 class GoToLocation(smach.State):
 
@@ -19,3 +20,12 @@ class GoToLocation(smach.State):
         except rospy.ERROR as e:
             rospy.logwarn(f"Unable to go to location. {userdata.location} -> ({str(e)})")
             return 'failed'
+
+if __name__ == '__main__':
+    rospy.init_node('go_to_location')
+    sm = smach.StateMachine(outcomes=['succeeded', 'failed'])
+    loc = rospy.get_param('/living_room/table/location')
+    sm.userdata.location = Pose(position=Point(**loc['position']), orientation=Quaternion(**loc['orientation']))
+    with sm:
+        smach.StateMachine.add('GoToLocation', GoToLocation(), transitions={'succeeded': 'succeeded', 'failed': 'failed'})
+    sm.execute()
