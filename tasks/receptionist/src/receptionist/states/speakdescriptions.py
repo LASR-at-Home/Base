a4bfa70827cf3e2b46a2a4e5aac206ef216a9767
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-02-05 12:20:05 +0000

    Speak normally.

--- tasks/receptionist/src/receptionist/states/speakdescriptions.py
+++ tasks/receptionist/src/receptionist/states/speakdescriptions.py
@@ -8,15 +8,15 @@ class SpeakDescriptions(smach.State):
         self.default = default
 
     def execute(self, userdata):
-        # for person in userdata['people']:
-        #     self.default.voice.speak('I see a person')
+        for person in userdata['people']:
+            self.default.voice.speak('I see a person')
             
-        #     for feature in person['features']:
-        #         if feature.label:
-        #             if len(feature.colours) == 0:
-        #                 self.default.voice.speak(f'They have {feature.name}.')
-        #                 continue
+            for feature in person['features']:
+                if feature.label:
+                    if len(feature.colours) == 0:
+                        self.default.voice.speak(f'They have {feature.name}.')
+                        continue
                     
-        #             self.default.voice.speak(f'They have {feature.name} and it has the colour {feature.colours[0]}')
+                    self.default.voice.speak(f'They have {feature.name} and it has the colour {feature.colours[0]}')
 
         return 'succeeded'
