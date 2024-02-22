1678eab0eea32e299ac7d0cc36dac7cebbc7b573
m-barker <mattbarker322@gmail.com>
2024-02-22 09:38:48 +0000

    chore: create faiss package

--- common/vision/lasr_vision_clip/setup.py
+++ common/vision/lasr_vision_clip/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=["qualification"], package_dir={"": "src"}
+    packages=["lasr_vision_clip"], package_dir={"": "src"}
 )
 
 setup(**setup_args)
