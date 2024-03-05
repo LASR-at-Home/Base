64a2246a2e4d5dca1a95321e7fd4c1f30b0d945b
Jared Swift <j.w.swift@outlook.com>
2024-03-05 11:43:52 +0000

    WIP: FindPerson skill.

---
+++ skills/src/lasr_skills/find_person.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import smach
+from smach_ros import SimpleActionState
+
+from geometry_msgs.msg import Pose
+
+from typing import List
+
+from lasr_skills import Detect3D, LookToPoint
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+
+class FindPerson(smach.StateMachine):
+
+    class HandleDetections(smach.State):
+        def __init__(self):
+            smach.State.__init__(
+                self,
+                outcomes=["succeeded", "failed"],
+                input_keys=["detections"],
+                output_keys=["point"],
+            )
+
+        def execute(self, userdata):
+            if len(userdata.detections.detected_objects) == 0:
+                return "failed"
+
+            userdata.point = userdata.detections.detected_objects[0].point
+            return "succeeded"
+
+    def __init__(self, waypoints: List[Pose]):
+        smach.StateMachine.__init__(self, outcomes=["succeeded", "failed"])
+        self.waypoints = waypoints
+
+        with self:
+            waypoint_iterator = smach.Iterator(
+                outcomes=["succeeded", "failed"],
+                it=self.waypoints,
+                it_label="location",
+                input_keys=[],
+                output_keys=[],
+            )
+
+            with waypoint_iterator:
+
+                container_sm = smach.StateMachine(
+                    outcomes=["succeeded", "failed", "continue"],
+                    input_keys=["location"],
+                )
+
+                with container_sm:
+
+                    smach.StateMachine.add(
+                        "GO_TO_LOCATION",
+                        SimpleActionState(
+                            "move_base",
+                            MoveBaseAction,
+                            goal_cb=lambda ud, _: MoveBaseGoal(target_pose=ud.location),
+                        ),
+                        transitions={"succeeded": "DETECT"},
+                    )
+                    smach.StateMachine.add(
+                        "DETECT3D",
+                        Detect3D(filter=["person"]),
+                        transitions={"succeeded": "succeeded", "failed": "failed"},
+                    )
+                    smach.StateMachine.add(
+                        "HANDLE_DETECTIONS",
+                        self.HandleDetections(),
+                        transitions={
+                            "succeeded": "LOOK_AT_PERSON",
+                            "failed": "continue",
+                        },
+                    )
+                    smach.StateMachine.add(
+                        "LOOK_AT_PERSON",
+                        LookToPoint(),
+                        transitions={"succeeded": "succeeded"},
+                    )
+
+
+if __name__ == "__main__":
+    import rospy
+    from smach_ros import IntrospectionServer
+
+    rospy.init_node("find_person")
+    sm = FindPerson([])
+    sis = IntrospectionServer("find_person", sm, "/FIND_PERSON")
+    sis.start()
+    sm.execute()
+    rospy.spin()
+    sis.stop()
