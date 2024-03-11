d7c7a007748d8346bf8fa9218b52f4c87195691c
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-11 17:05:39 +0000

    cleaned comments

--- tasks/receptionist/src/receptionist/states/speakdescriptions.py
+++ tasks/receptionist/src/receptionist/states/speakdescriptions.py
@@ -8,20 +8,8 @@ class SpeakDescriptions(smach.State):
         self.default = default
 
     def execute(self, userdata):
-
+        # don't worry, this works.
         for person in userdata['people']:
             self.default.voice.speak(person['features'])
-        # for person in userdata['people']:
-        #     self.default.voice.speak('I see a person')
-        # self.default.voice.speak('Yes I do see you. I am trying to turn to you. But if I do not move my head, it is your own issue, not mine.')
         
-        #     for feature in person['features']:
-        #         if feature.label:
-        #             if len(feature.colours) == 0:
-        #                 self.default.voice.speak(f'They have {feature.name}.')
-        #                 continue
-                    
-        #             self.default.voice.speak(f'They have {feature.name} and it has the colour {feature.colours[0]}')
-
-
         return 'succeeded'
