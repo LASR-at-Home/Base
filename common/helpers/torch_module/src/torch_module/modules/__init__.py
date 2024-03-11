5765e667174f632eb0dbb635b7b493208a522b66
Benteng Ma <benteng.ma@kcl.ac.uk>
2024-03-11 16:25:16 +0000

    removed loading the pretrained parameters

--- common/helpers/torch_module/src/torch_module/modules/__init__.py
+++ common/helpers/torch_module/src/torch_module/modules/__init__.py
@@ -35,7 +35,7 @@ class UNetWithResnetEncoder(nn.Module):
     def __init__(self, num_classes, in_channels=3, freeze_bn=False, sigmoid=True):
         super(UNetWithResnetEncoder, self).__init__()
         self.sigmoid = sigmoid
-        self.resnet = models.resnet34(pretrained=True)  # Initialize with a ResNet model
+        self.resnet = models.resnet34(pretrained=False)  # Initialize with a ResNet model
         if in_channels != 3:
             self.resnet.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
 
@@ -99,7 +99,7 @@ class UNetWithResnetEncoder(nn.Module):
 class MultiLabelResNet(nn.Module):
     def __init__(self, num_labels, input_channels=3, sigmoid=True, pretrained=False,):
         super(MultiLabelResNet, self).__init__()
-        self.model = models.resnet34(pretrained=pretrained)
+        self.model = models.resnet34(pretrained=False)
         self.sigmoid = sigmoid
 
         if input_channels != 3:
