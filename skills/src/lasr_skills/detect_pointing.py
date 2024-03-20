621de7de9d6284e62e4593d321e0f3d406f6578d
Serge <syraj.alkhalil7@gmail.com>
2024-03-20 15:59:09 +0000

    feat: implement pointing as a state machine

---
+++ skills/src/lasr_skills/detect_pointing.py
@@ -0,0 +1,17 @@
+import rospy
+import smach
+from sensor_msgs.msg import Image
+
+from lasr_vision_msgs.srv import PointingDirection
+
+class PointingDetecor(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'], output_keys=['direction'])
+        self.service = rospy.ServiceProxy('/pointing_detection_service', PointingDirection)
+        self.image_raw = rospy.wait_for_message('/xtion/rgb/image_raw', Image)
+
+    def excute(self, userdata):
+        resp = self.service(self.image_raw)
+        userdata.direction = resp.direction
+
+        return 'succeeded' if resp.direction != 'Err' else 'failed'
