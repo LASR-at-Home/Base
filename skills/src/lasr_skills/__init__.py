e53616d049da869dd5d76911627c5216ec0cd2fb
Zoe <ezoe013@gmail.com>
2024-03-22 11:26:54 +0000

    finished merging?

--- skills/src/lasr_skills/__init__.py
+++ skills/src/lasr_skills/__init__.py
@@ -7,12 +7,9 @@ from .describe_people import DescribePeople
 from .look_to_point import LookToPoint
 from .go_to_location import GoToLocation
 from .go_to_semantic_location import GoToSemanticLocation
-<<<<<<< HEAD
-=======
 from .say import Say
 from .listen import Listen
 from .listen_for import ListenFor
 from .play_motion import PlayMotion
 from .receive_object import ReceiveObject
 from .handover_object import HandoverObject
->>>>>>> 5cb0ba5fd2d082edaee1ee0a86f869d1e0b47456
