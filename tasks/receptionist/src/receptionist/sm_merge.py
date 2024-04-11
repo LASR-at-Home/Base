7f92b165afaa6e9eaa1425f6d72ee6996d585139
Jared Swift <j.w.swift@outlook.com>
2024-04-11 15:51:20 +0100

    refactor: receptionist -> sm_merge (conflicts with python package...)

--- tasks/receptionist/src/receptionist/sm_merge.py
+++ tasks/receptionist/src/receptionist/sm_merge.py
@@ -21,33 +21,33 @@ class Receptionist(smach.StateMachine):
             self.userdata.guest_id = "guest1"
             self.userdata.guest_data = {"host": host_data}
 
-            smach.StateMachine.add(
-                "GO_TO_WAIT_LOCATION",
-                GoToLocation(wait_pose),
-                transitions={
-                    "succeeded": "SAY_WAITING",
-                    "failed": "failed",
-                },
-            )
+            # smach.StateMachine.add(
+            #     "GO_TO_WAIT_LOCATION",
+            #     GoToLocation(wait_pose),
+            #     transitions={
+            #         "succeeded": "SAY_WAITING",
+            #         "failed": "failed",
+            #     },
+            # )
 
-            smach.StateMachine.add(
-                "SAY_WAITING",
-                Say("I am waiting for a guest."),
-                transitions={
-                    "succeeded": "WAIT_FOR_PERSON",
-                    "aborted": "failed",
-                    "preempted": "failed",
-                },
-            )
+            # smach.StateMachine.add(
+            #     "SAY_WAITING",
+            #     Say("I am waiting for a guest."),
+            #     transitions={
+            #         "succeeded": "WAIT_FOR_PERSON",
+            #         "aborted": "failed",
+            #         "preempted": "failed",
+            #     },
+            # )
 
-            smach.StateMachine.add(
-                "WAIT_FOR_PERSON",
-                WaitForPersonInArea(wait_area),
-                transitions={
-                    "succeeded": "succeeded",
-                    "failed": "failed",
-                },
-            )
+            # smach.StateMachine.add(
+            #     "WAIT_FOR_PERSON",
+            #     WaitForPersonInArea(wait_area),
+            #     transitions={
+            #         "succeeded": "succeeded",
+            #         "failed": "failed",
+            #     },
+            # )
 
             smach.StateMachine.add(
                 "GET_NAME_AND_DRINK",
@@ -90,5 +90,5 @@ if __name__ == "__main__":
     wait_area_param = rospy.get_param("/receptionist/wait_area")
     wait_area = Polygon(wait_area_param)
 
-    sm = Receptionist(wait_pose, wait_area)
+    sm = Receptionist(wait_pose, wait_area, {"name": "John", "drink": "beer"})
     outcome = sm.execute()
