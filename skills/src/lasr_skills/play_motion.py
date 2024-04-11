06108ec4940dcc59d80dc3a989986b4b2d49d62e
Jared Swift <j.w.swift@outlook.com>
2024-04-11 14:55:02 +0100

    feat: start/stop and raise/lower head manager + torso before/after navigation.

--- skills/src/lasr_skills/play_motion.py
+++ skills/src/lasr_skills/play_motion.py
@@ -1,4 +1,5 @@
 import smach_ros
+import rospy
 
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 
@@ -11,14 +12,24 @@ class PlayMotion(smach_ros.SimpleActionState):
             super(PlayMotion, self).__init__(
                 "play_motion",
                 PlayMotionAction,
-                goal=PlayMotionGoal(motion_name=motion_name, skip_planning=False),
+                goal=PlayMotionGoal(
+                    motion_name=motion_name,
+                    skip_planning=rospy.get_param(
+                        f"/play_motion/motions/{motion_name}/joints"
+                    )
+                    == ["torso_lift_joint"],
+                ),
             )
         else:
             super(PlayMotion, self).__init__(
                 "play_motion",
                 PlayMotionAction,
                 goal_cb=lambda ud, _: PlayMotionGoal(
-                    motion_name=ud.motion_name, skip_planning=False
+                    motion_name=ud.motion_name,
+                    skip_planning=rospy.get_param(
+                        f"/play_motion/motions/{ud.motion_name}/joints"
+                        == ["torso_lift_joint"]
+                    ),
                 ),
                 input_keys=["motion_name"],
             )
