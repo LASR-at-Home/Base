8b95c891dfddb550bb9104f6425fe7111785c463
m-barker <mattbarker322@gmail.com>
2024-04-15 15:55:59 +0100

    WIP: handling sub commands in parser

--- tasks/gpsr/src/gpsr/regex_command_parser.py
+++ tasks/gpsr/src/gpsr/regex_command_parser.py
@@ -19,6 +19,246 @@ def list_to_regex(list: List[str], key: Union[str, None] = None):
     return f"(?P<{uniq(key)}>{'|'.join(list)})"
 
 
+def generate_command_start(self, cmd_category="", difficulty=0):
+    cmd_list = []
+    # cmd_list = ["goToLoc", "takeObjFromPlcmt", "findPrsInRoom", "findObjInRoom", "meetPrsAtBeac", "countObjOnPlcmt",
+    #             "countPrsInRoom", "tellPrsInfoInLoc", "tellObjPropOnPlcmt", "talkInfoToGestPrsInRoom",
+    #             "answerToGestPrsInRoom", "followNameFromBeacToRoom", "guideNameFromBeacToBeac",
+    #             "guidePrsFromBeacToBeac", "guideClothPrsFromBeacToBeac", "bringMeObjFromPlcmt",
+    #             "tellCatPropOnPlcmt", "greetClothDscInRm", "greetNameInRm", "meetNameAtLocThenFindInRm",
+    #             "countClothPrsInRoom", "countClothPrsInRoom", "tellPrsInfoAtLocToPrsAtLoc", "followPrsAtLoc"]
+
+    # HRI and people perception commands
+    person_cmd_list = [
+        "goToLoc",
+        "findPrsInRoom",
+        "meetPrsAtBeac",
+        "countPrsInRoom",
+        "tellPrsInfoInLoc",
+        "talkInfoToGestPrsInRoom",
+        "answerToGestPrsInRoom",
+        "followNameFromBeacToRoom",
+        "guideNameFromBeacToBeac",
+        "guidePrsFromBeacToBeac",
+        "guideClothPrsFromBeacToBeac",
+        "greetClothDscInRm",
+        "greetNameInRm",
+        "meetNameAtLocThenFindInRm",
+        "countClothPrsInRoom",
+        "countClothPrsInRoom",
+        "tellPrsInfoAtLocToPrsAtLoc",
+        "followPrsAtLoc",
+    ]
+    # Object manipulation and perception commands
+    object_cmd_list = [
+        "goToLoc",
+        "takeObjFromPlcmt",
+        "findObjInRoom",
+        "countObjOnPlcmt",
+        "tellObjPropOnPlcmt",
+        "bringMeObjFromPlcmt",
+        "tellCatPropOnPlcmt",
+    ]
+
+    if cmd_category == "people":
+        cmd_list = person_cmd_list
+    elif cmd_category == "objects":
+        cmd_list = object_cmd_list
+    else:
+        cmd_list = person_cmd_list if random.random() > 0.5 else object_cmd_list
+
+    command = random.choice(cmd_list)
+    # command = "" # To debug commands
+    command_string = ""
+    if command == "goToLoc":
+        command_string = (
+            "{goVerb} {toLocPrep} the {loc_room} then "
+            + self.generate_command_followup("atLoc", cmd_category, difficulty)
+        )
+    elif command == "takeObjFromPlcmt":
+        command_string = (
+            "{takeVerb} {art} {obj_singCat} {fromLocPrep} the {plcmtLoc} and "
+            + self.generate_command_followup("hasObj", cmd_category, difficulty)
+        )
+    elif command == "findPrsInRoom":
+        command_string = (
+            "{findVerb} a {gestPers_posePers} {inLocPrep} the {room} and "
+            + self.generate_command_followup("foundPers", cmd_category, difficulty)
+        )
+    elif command == "findObjInRoom":
+        command_string = (
+            "{findVerb} {art} {obj_singCat} {inLocPrep} the {room} then "
+            + self.generate_command_followup("foundObj", cmd_category, difficulty)
+        )
+    elif command == "meetPrsAtBeac":
+        command_string = (
+            "{meetVerb} {name} {inLocPrep} the {room} and "
+            + self.generate_command_followup("foundPers", cmd_category, difficulty)
+        )
+    elif command == "countObjOnPlcmt":
+        command_string = "{countVerb} {plurCat} there are {onLocPrep} the {plcmtLoc}"
+    elif command == "countPrsInRoom":
+        command_string = (
+            "{countVerb} {gestPersPlur_posePersPlur} are {inLocPrep} the {room}"
+        )
+    elif command == "tellPrsInfoInLoc":
+        command_string = "{tellVerb} me the {persInfo} of the person {inRoom_atLoc}"
+    elif command == "tellObjPropOnPlcmt":
+        command_string = (
+            "{tellVerb} me what is the {objComp} object {onLocPrep} the {plcmtLoc}"
+        )
+    elif command == "talkInfoToGestPrsInRoom":
+        command_string = (
+            "{talkVerb} {talk} {talkPrep} the {gestPers} {inLocPrep} the {room}"
+        )
+    elif command == "answerToGestPrsInRoom":
+        command_string = "{answerVerb} the {question} {ofPrsPrep} the {gestPers} {inLocPrep} the {room}"
+    elif command == "followNameFromBeacToRoom":
+        command_string = (
+            "{followVerb} {name} {fromLocPrep} the {loc} {toLocPrep} the {room}"
+        )
+    elif command == "guideNameFromBeacToBeac":
+        command_string = (
+            "{guideVerb} {name} {fromLocPrep} the {loc} {toLocPrep} the {loc_room}"
+        )
+    elif command == "guidePrsFromBeacToBeac":
+        command_string = "{guideVerb} the {gestPers_posePers} {fromLocPrep} the {loc} {toLocPrep} the {loc_room}"
+    elif command == "guideClothPrsFromBeacToBeac":
+        command_string = "{guideVerb} the person wearing a {colorClothe} {fromLocPrep} the {loc} {toLocPrep} the {loc_room}"
+    elif command == "bringMeObjFromPlcmt":
+        command_string = "{bringVerb} me {art} {obj} {fromLocPrep} the {plcmtLoc}"
+    elif command == "tellCatPropOnPlcmt":
+        command_string = (
+            "{tellVerb} me what is the {objComp} {singCat} {onLocPrep} the {plcmtLoc}"
+        )
+    elif command == "greetClothDscInRm":
+        command_string = (
+            "{greetVerb} the person wearing {art} {colorClothe} {inLocPrep} the {room} and "
+            + self.generate_command_followup("foundPers", cmd_category, difficulty)
+        )
+    elif command == "greetNameInRm":
+        command_string = (
+            "{greetVerb} {name} {inLocPrep} the {room} and "
+            + self.generate_command_followup("foundPers", cmd_category, difficulty)
+        )
+    elif command == "meetNameAtLocThenFindInRm":
+        command_string = "{meetVerb} {name} {atLocPrep} the {loc} then {findVerb} them {inLocPrep} the {room}"
+    elif command == "countClothPrsInRoom":
+        command_string = (
+            "{countVerb} people {inLocPrep} the {room} are wearing {colorClothes}"
+        )
+    elif command == "countClothPrsInRoom":
+        command_string = (
+            "{countVerb} people {inLocPrep} the {room} are wearing {colorClothes}"
+        )
+    elif command == "tellPrsInfoAtLocToPrsAtLoc":
+        command_string = "{tellVerb} the {persInfo} of the person {atLocPrep} the {loc} to the person {atLocPrep} the {loc2}"
+    elif command == "followPrsAtLoc":
+        command_string = "{followVerb} the {gestPers_posePers} {inRoom_atLoc}"
+    else:
+        warnings.warn("Command type not covered: " + command)
+        return "WARNING"
+
+    for ph in re.findall(r"(\{\w+\})", command_string, re.DOTALL):
+        command_string = command_string.replace(ph, self.insert_placeholders(ph))
+
+    # TODO allow multiple articles
+    art_ph = re.findall(r"\{(art)\}\s*([A-Za-z])", command_string, re.DOTALL)
+    if art_ph:
+        command_string = command_string.replace(
+            "art", "an" if art_ph[0][1].lower() in ["a", "e", "i", "o", "u"] else "a"
+        )
+    # TODO eliminate double mentions of location
+    if "loc2" in command_string:
+        command_string = command_string.replace(
+            "loc2",
+            random.choice([x for x in self.location_names if x not in command_string]),
+        )
+    elif "room2" in command_string:
+        command_string = command_string.replace(
+            "room2",
+            random.choice([x for x in self.room_names if x not in command_string]),
+        )
+    elif "plcmtLoc2" in command_string:
+        command_string = command_string.replace(
+            "plcmtLoc2",
+            random.choice(
+                [x for x in self.placement_location_names if x not in command_string]
+            ),
+        )
+    return command_string.replace("{", "").replace("}", "")
+
+
+def generate_command_followup(self, type, cmd_category="", difficulty=0):
+    if type == "atLoc":
+        person_cmd_list = ["findPrs", "meetName"]
+        object_cmd_list = ["findObj"]
+        if cmd_category == "people":
+            cmd_list = person_cmd_list
+        elif cmd_category == "objects":
+            cmd_list = object_cmd_list
+        else:
+            cmd_list = person_cmd_list if random.random() > 0.5 else object_cmd_list
+    elif type == "hasObj":
+        cmd_list = [
+            "placeObjOnPlcmt",
+            "deliverObjToMe",
+            "deliverObjToPrsInRoom",
+            "deliverObjToNameAtBeac",
+        ]
+    elif type == "foundPers":
+        cmd_list = [
+            "talkInfo",
+            "answerQuestion",
+            "followPrs",
+            "followPrsToRoom",
+            "guidePrsToBeacon",
+        ]
+    elif type == "foundObj":
+        cmd_list = ["takeObj"]
+
+    command = random.choice(cmd_list)
+    command_string = ""
+    if command == "findObj":
+        command_string = (
+            "{findVerb} {art} {obj_singCat} and "
+            + self.generate_command_followup("foundObj")
+        )
+    elif command == "findPrs":
+        command_string = (
+            "{findVerb} the {gestPers_posePers} and "
+            + self.generate_command_followup("foundPers")
+        )
+    elif command == "meetName":
+        command_string = "{meetVerb} {name} and " + self.generate_command_followup(
+            "foundPers"
+        )
+    elif command == "placeObjOnPlcmt":
+        command_string = "{placeVerb} it {onLocPrep} the {plcmtLoc2}"
+    elif command == "deliverObjToMe":
+        command_string = "{deliverVerb} it to me"
+    elif command == "deliverObjToPrsInRoom":
+        command_string = "{deliverVerb} it {deliverPrep} the {gestPers_posePers} {inLocPrep} the {room}"
+    elif command == "deliverObjToNameAtBeac":
+        command_string = "{deliverVerb} it {deliverPrep} {name} {inLocPrep} the {room}"
+    elif command == "talkInfo":
+        command_string = "{talkVerb} {talk}}"
+    elif command == "answerQuestion":
+        command_string = "{answerVerb} a {question}"
+    elif command == "followPrs":
+        command_string = "{followVerb} them"
+    elif command == "followPrsToRoom":
+        command_string = "{followVerb} them {toLocPrep} the {loc2_room2}"
+    elif command == "guidePrsToBeacon":
+        command_string = "{guideVerb} them {toLocPrep} the {loc2_room2}"
+    elif command == "takeObj":
+        command_string = "{takeVerb} it and " + self.generate_command_followup("hasObj")
+    else:
+        warnings.warn("Command type not covered: " + command)
+        return "WARNING"
+    return command_string
+
+
 # data from gpsr_commands
 verb_dict = {
     "take": ["take", "get", "grasp", "fetch"],
@@ -228,28 +468,66 @@ def gpsr_regex(configuration: Configuration):
         matcher = re.sub("\(\?P\<", f"(?P<CMD{key}_", matcher)
         commands.append(f"(?P<command_{key}>{matcher})")
 
-    def SUB_COMMAND_TODO_UNIMPLEMENTED():
-        return f'(?P<{uniq("subcommand")}>.*)'
+    def get_possible_sub_commands(type: str) -> str:
+        sub_commands = []
+        if type == "atLoc":
+            sub_commands.append(
+                f"{verb('find')} {art} {Configuration.pick(configuration, 'object', ['obj', 'singCat'])} and {get_possible_sub_commands('foundObj')}"
+            )
+            sub_commands.append(
+                f"{verb('find')} the (?:{gesture_person_list}|{pose_person_list}) and {get_possible_sub_commands('foundPers')}"
+            )
+            sub_commands.append(
+                f"{verb('meet')} {Configuration.pick(configuration, 'name', ['name'])} and {get_possible_sub_commands('foundPers')}"
+            )
+        elif type == "hasObj":
+            sub_commands.append(
+                f"{verb('place')} it {prep('onLocPrep')} the {Configuration.pick(configuration, 'location', ['plcmtLoc'])}"
+            )
+            sub_commands.append(f"{verb('deliver')} it to me")
+            sub_commands.append(
+                f"{verb('deliver')} it {prep('deliverPrep')} the (?:{gesture_person_list}|{pose_person_list}) {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])}"
+            )
+            sub_commands.append(
+                f"{verb('deliver')} it {prep('deliverPrep')} {Configuration.pick(configuration, 'name', ['name'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])}"
+            )
+        elif type == "foundPers":
+            sub_commands.append(f"{verb('talk')} {talk_list}")
+            sub_commands.append(f"{verb('answer')} a {question_list}")
+            sub_commands.append(f"{verb('follow')} them")
+            sub_commands.append(
+                f"{verb('follow')} them {prep('toLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])}"
+            )
+            sub_commands.append(
+                f"{verb('guide')} them {prep('toLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])}"
+            )
+        elif type == "foundObj":
+            sub_commands.append(
+                f"{verb('take')} it and {get_possible_sub_commands('hasObj')}"
+            )
+
+        union = "|".join(sub_commands)
+        return union
 
     command(
         "goToLoc",
-        f"{verb('go')} {prep('toLocPrep')} the {Configuration.pick(configuration, 'location', ['loc', 'room'])} then {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('go')} {prep('toLocPrep')} the {Configuration.pick(configuration, 'location', ['loc', 'room'])} then {get_possible_sub_commands('atLoc')}",
     )
     command(
         "takeObjFromPlcmt",
-        f"{verb('take')} {art} {Configuration.pick(configuration, 'object', ['obj', 'singCat'])} {prep('fromLocPrep')} the {Configuration.pick(configuration, 'location', ['plcmtLoc'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('take')} {art} {Configuration.pick(configuration, 'object', ['obj', 'singCat'])} {prep('fromLocPrep')} the {Configuration.pick(configuration, 'location', ['plcmtLoc'])} and {get_possible_sub_commands('hasObj')}",
     )
     command(
         "findPrsInRoom",
-        f"{verb('find')} a (?:{gesture_person_list}|{pose_person_list}) {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('find')} a (?:{gesture_person_list}|{pose_person_list}) {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {get_possible_sub_commands('foundPers')}",
     )
     command(
         "findObjInRoom",
-        f"{verb('find')} {art} {Configuration.pick(configuration, 'object', ['obj', 'singCat'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('find')} {art} {Configuration.pick(configuration, 'object', ['obj', 'singCat'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {get_possible_sub_commands('foundObj')}",
     )
     command(
         "meetPrsAtBeac",
-        f"{verb('meet')} {Configuration.pick(configuration, 'name', ['name'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('meet')} {Configuration.pick(configuration, 'name', ['name'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {get_possible_sub_commands('foundPers')}",
     )
     command(
         "countObjOnPlcmt",
@@ -301,11 +579,11 @@ def gpsr_regex(configuration: Configuration):
     )
     command(
         "greetClothDscInRm",
-        f"{verb('greet')} the person wearing {art} {color_clothe_list} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('greet')} the person wearing {art} {color_clothe_list} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {get_possible_sub_commands('foundPers')}",
     )
     command(
         "greetNameInRm",
-        f"{verb('greet')} {Configuration.pick(configuration, 'name', ['name'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {SUB_COMMAND_TODO_UNIMPLEMENTED()}",
+        f"{verb('greet')} {Configuration.pick(configuration, 'name', ['name'])} {prep('inLocPrep')} the {Configuration.pick(configuration, 'location', ['room'])} and {get_possible_sub_commands('foundPers')}",
     )
     command(
         "meetNameAtLocThenFindInRm",
@@ -389,4 +667,8 @@ if __name__ == "__main__":
         return gpsr_parse(matches)
 
     # subcommands aren't implemented but are caught:
-    print(execute("go to the sofa then do something here"))
+    print(
+        execute(
+            "locate a cup in the kitchen then fetch it and bring it to the person raising their right arm in the kitchen"
+        )
+    )
