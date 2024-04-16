9d09697a272e4c835ad2964f5397866e917598be
Jared Swift <j.w.swift@outlook.com>
2024-04-16 12:00:22 +0100

    feat: update init.py

--- tasks/receptionist/src/receptionist/states/__init__.py
+++ tasks/receptionist/src/receptionist/states/__init__.py
@@ -11,3 +11,4 @@ from .speakdescriptions import SpeakDescriptions
 from .get_name_and_drink import ParseNameAndDrink
 from .get_attributes import GetGuestAttributes
 from .introduce import Introduce
+from .seat_guest import SeatGuest
