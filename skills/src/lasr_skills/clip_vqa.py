26e4db1eb163bb7534ca6d1d82df43bd8f4af6c2
Jared Swift <j.w.swift@outlook.com>
2024-04-18 11:19:24 +0100

    refactor: cleanup clip vqa skill.

--- skills/src/lasr_skills/clip_vqa.py
+++ skills/src/lasr_skills/clip_vqa.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
 import smach
 from smach import UserData
 from typing import Union
 from lasr_skills import Say
 from lasr_vision_clip.srv import VqaRequest, VqaResponse, Vqa
+import rospy
 
 
 class QueryImage(smach.State):
@@ -28,38 +27,3 @@ class QueryImage(smach.State):
         userdata.answer = response.answer
         userdata.similarity_score = response.similarity
         return "succeeded"
-
-
-if __name__ == "__main__":
-    import rospy
-
-    rospy.init_node("test_query_image")
-    sm = smach.StateMachine(outcomes=["succeeded", "failed"])
-    with sm:
-        smach.StateMachine.add(
-            "QUERY_IMAGE",
-            QueryImage(),
-            transitions={
-                "succeeded": "SAY",
-                "failed": "failed",
-            },
-            remapping={
-                "answers": "answers",
-                "answer": "answer",
-                "similarity_score": "similarity_score",
-            },
-        )
-        smach.StateMachine.add(
-            "SAY",
-            Say(format_str="I think the answer is {}"),
-            transitions={
-                "succeeded": "succeeded",
-                "aborted": "failed",
-                "preempted": "failed",
-            },
-            remapping={
-                "placeholders": "answer",
-            },
-        )
-    sm.userdata.answers = ["a person wearing glasses", "a person not wearing glasses"]
-    sm.execute()
