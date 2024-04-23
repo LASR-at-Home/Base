4217b375f0a5825c94cdcf7dbc62de2a18c91024
fireblonde <nicollehchevska@gmail.com>
2024-04-23 17:47:05 +0100

    chore: add the 3d image skill

--- skills/src/lasr_skills/vision/get_image.py
+++ skills/src/lasr_skills/vision/get_image.py
@@ -1,7 +1,7 @@
 import os
 import smach
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
 
 
 class GetImage(smach.State):
@@ -14,7 +14,7 @@ class GetImage(smach.State):
             self, outcomes=['succeeded', 'failed'], output_keys=['img_msg'])
 
         if topic is None:
-            self.topic = '/xtion/rgb/image_raw' if 'tiago' in os.environ['ROS_MASTER_URI'] else '/camera/image_raw'
+            self.topic = '/xtion/rgb/image_raw' if 'tiago' in os.environ['ROS_MASTER_URI'] else '/usb_cam/image_raw'
         else:
             self.topic = topic
 
@@ -22,6 +22,31 @@ class GetImage(smach.State):
         try:
             userdata.img_msg = rospy.wait_for_message(
                 self.topic, Image)
+        except Exception as e:
+            print(e)
+            return 'failed'
+
+        return 'succeeded'
+
+
+class Get3DImage(smach.State):
+    """
+    State for acquiring a PointCloud2 message.
+    """
+
+    def __init__(self, topic: str = None):
+        smach.State.__init__(
+            self, outcomes=['succeeded', 'failed'], output_keys=['img_msg'])
+
+        if topic is None:
+            self.topic = '/xtion/depth_registered/points'
+        else:
+            self.topic = topic
+
+    def execute(self, userdata):
+        try:
+            userdata.img_msg = rospy.wait_for_message(
+                self.topic, PointCloud2)
         except Exception as e:
             print(e)
             return 'failed'
