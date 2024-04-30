7897c86b0f6d5ae44ad96681882788d05bf46552
Jared Swift <j.w.swift@outlook.com>
2024-04-30 10:32:32 +0100

    feat: wait for person, and detect the direction that they are pointing.

--- tasks/carry_my_luggage/src/carry_my_luggage/state_machine.py
+++ tasks/carry_my_luggage/src/carry_my_luggage/state_machine.py
@@ -1,6 +1,56 @@
 import smach
 
+from lasr_skills import WaitForPerson, DetectPointingDirection, Say
+
+from lasr_vision_msgs.msg import Direction
+
 
 class CarryMyLuggage(smach.StateMachine):
+
+    class HandlePointingDirection(smach.State):
+        def __init__(self):
+            smach.State.__init__(
+                self,
+                outcomes=["succeeded"],
+                input_keys=["pointing_direction"],
+                output_keys=["pointing_direction_str"],
+            )
+
+        def execute(self, userdata):
+            if userdata.pointing_direction == Direction.LEFT:
+                userdata.pointing_direction_str = "left"
+            elif userdata.pointing_direction == Direction.RIGHT:
+                userdata.pointing_direction_str = "right"
+            else:
+                userdata.pointing_direction_str = "forwards"
+            return "succeeded"
+
     def __init__(self):
         smach.StateMachine.__init__(self, outcomes=["succeeded", "failed"])
+
+        with self:
+            smach.StateMachine.add(
+                "WAIT_FOR_PERSON",
+                WaitForPerson(),
+                transitions={
+                    "succeeded": "DETECT_POINTING_DIRECTION",
+                    "failed": "failed",
+                },
+            )
+
+            smach.StateMachine.add(
+                "DETECT_POINTING_DIRECTION",
+                DetectPointingDirection(),
+                transitions={"succeeded": "SAY_DIRECTION", "failed": "failed"},
+            )
+
+            smach.StateMachine.add(
+                "SAY_DIRECTION",
+                Say(format_str="I see you are pointing {}"),
+                transitions={
+                    "succeeded": "succeeded",
+                    "aborted": "failed",
+                    "preempted": "failed",
+                },
+                remapping={"placeholders": "pointing_direction_str"},
+            )
