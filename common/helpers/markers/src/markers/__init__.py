c1dd3a73a714700d7a77847042f4154715a23a54
Matt <mattbarker322@gmail.com>
2024-06-12 10:22:05 +0100

    feat: add publishing of text label of markerrs

--- common/helpers/markers/src/markers/__init__.py
+++ common/helpers/markers/src/markers/__init__.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import PointStamped
 
 from collections import defaultdict
 
-from typing import Union
+from typing import Union, Optional
 
 publisher_counts = defaultdict(int)
 
@@ -16,12 +16,13 @@ def create_marker(
     r: float = 0.0,
     g: float = 1.0,
     b: float = 0.0,
+    name: Optional[str] = None,
 ):
     marker_msg = Marker()
+    marker_msg.type = Marker.SPHERE
     marker_msg.header.frame_id = point_stamped.header.frame_id
     marker_msg.header.stamp = point_stamped.header.stamp
     marker_msg.id = idx
-    marker_msg.type = Marker.SPHERE
     marker_msg.action = Marker.ADD
     marker_msg.pose.position = point_stamped.point
     marker_msg.pose.orientation.w = 1.0
@@ -32,6 +33,10 @@ def create_marker(
     marker_msg.color.r = r
     marker_msg.color.g = g
     marker_msg.color.b = b
+
+    if name is not None:
+        marker_msg.type = Marker.TEXT_VIEW_FACING
+        marker_msg.text = name
     return marker_msg
 
 
@@ -42,11 +47,19 @@ def create_and_publish_marker(
     r: float = 0.0,
     g: float = 1.0,
     b: float = 0.0,
+    name: Optional[str] = None,
 ):
     if idx is None:
         global publisher_counts
         idx = publisher_counts[publisher]
         publisher_counts[publisher] += 1
-
     marker_msg = create_marker(point_stamped, idx, r, g, b)
+    idx = publisher_counts[publisher]
+    publisher_counts[publisher] += 1
     publisher.publish(marker_msg)
+    rospy.sleep(2)  # Needed to prevent markers from being overwritten
+    if name is not None:
+        name_location = point_stamped.point
+        name_location.z += 0.1
+        marker_name_msg = create_marker(point_stamped, idx, r, g, b, name)
+        publisher.publish(marker_name_msg)
