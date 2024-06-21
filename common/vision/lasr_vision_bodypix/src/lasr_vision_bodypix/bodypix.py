460198ec6a4df57d3279a2cc74c33e2e45030c5c
Matt <mattbarker322@gmail.com>
2024-06-21 11:53:12 +0100

    feat: split bodypix service into two: one for mask, one for keypoints

--- common/vision/lasr_vision_bodypix/src/lasr_vision_bodypix/bodypix.py
+++ common/vision/lasr_vision_bodypix/src/lasr_vision_bodypix/bodypix.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+from typing import List
 import rospy
 import cv2_img
 import numpy as np
-
 from PIL import Image
 import re
 import tensorflow as tf
@@ -11,8 +11,13 @@ from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
 
 from sensor_msgs.msg import Image as SensorImage
 
-from lasr_vision_msgs.msg import BodyPixMask, BodyPixPose, BodyPixKeypoint
-from lasr_vision_msgs.srv import BodyPixDetectionRequest, BodyPixDetectionResponse
+from lasr_vision_msgs.msg import BodyPixMask, BodyPixKeypoint
+from lasr_vision_msgs.srv import (
+    BodyPixMaskDetectionRequest,
+    BodyPixMaskDetectionResponse,
+    BodyPixKeypointDetectionRequest,
+    BodyPixKeypointDetectionResponse,
+)
 
 import rospkg
 
@@ -30,7 +35,7 @@ def camel_to_snake(name):
     return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
 
 
-def load_model_cached(dataset: str) -> None:
+def load_model_cached(dataset: str):
     """
     Load a model into cache
     """
@@ -51,46 +56,56 @@ def load_model_cached(dataset: str) -> None:
     return model
 
 
-def detect(
-    request: BodyPixDetectionRequest,
-    debug_publisher: rospy.Publisher = rospy.Publisher(
-        "/bodypix/debug", SensorImage, queue_size=1
-    ),
-) -> BodyPixDetectionResponse:
-    """
-    Run BodyPix inference on given detection request
-    """
-
+def run_inference(dataset: str, confidence: float, img: Image):
     # decode the image
     rospy.loginfo("Decoding")
-    img = cv2_img.msg_to_cv2_img(request.image_raw)
+    img = cv2_img.msg_to_cv2_img(img)
 
     # load model
     rospy.loginfo("Loading model")
-    model = load_model_cached(request.dataset)
+    model = load_model_cached(dataset)
 
     # run inference
     rospy.loginfo("Running inference")
     result = model.predict_single(img)
 
-    mask = result.get_mask(threshold=request.confidence)
+    mask = result.get_mask(threshold=confidence)
     rospy.loginfo("Inference complete")
 
+    return result, mask
+
+
+def detect_masks(
+    request: BodyPixMaskDetectionRequest,
+    debug_publisher: rospy.Publisher = rospy.Publisher(
+        "/bodypix/mask_debug", SensorImage, queue_size=1
+    ),
+) -> BodyPixMaskDetectionResponse:
+    """
+    Run BodyPix inference on given mask detection request
+    """
+
+    result, mask = run_inference(request.dataset, request.confidence, request.image_raw)
     # construct masks response
     masks = []
-    for mask_request in request.masks:
+
+    # This uses this list of parts:
+    # https://github.com/de-code/python-tf-bodypix/blob/develop/tf_bodypix/bodypix_js_utils/part_channels.py#L5
+
+    for part_name in request.parts:
         part_mask = result.get_part_mask(
-            mask=tf.identity(mask), part_names=mask_request.parts
+            mask=tf.identity(mask), part_names=[part_name]
         ).squeeze()
 
-        bodypix_mask = BodyPixMask()
+        if np.max(part_mask) == 0:
+            rospy.logwarn(f"No masks found for part {part_name}")
+            continue
 
+        bodypix_mask = BodyPixMask()
         bodypix_mask.mask = part_mask.flatten().astype(bool).tolist()
         bodypix_mask.shape = list(part_mask.shape)
+        bodypix_mask.part_name = part_name
         masks.append(bodypix_mask)
-
-    # construct poses response and neck coordinates
-    poses = result.get_poses()
 
     # publish to debug topic
     if debug_publisher is not None:
@@ -107,24 +122,54 @@ def detect(
         )
         debug_publisher.publish(cv2_img.cv2_img_to_msg(coloured_mask))
 
-    output_poses = []
-
-    for pose in poses:
-        pose_msg = BodyPixPose()
-        keypoints_msg = []
-
-        for i, keypoint in pose.keypoints.items():
-            if camel_to_snake(keypoint.part) in request.masks[0].parts:
-                keypoint_msg = BodyPixKeypoint()
-                keypoint_msg.xy = [int(keypoint.position.x), int(keypoint.position.y)]
-                keypoint_msg.score = keypoint.score
-                keypoint_msg.part = keypoint.part
-                keypoints_msg.append(keypoint_msg)
-
-        pose_msg.keypoints = keypoints_msg
-        output_poses.append(pose_msg)
-
-    response = BodyPixDetectionResponse()
-    response.poses = output_poses
+    response = BodyPixMaskDetectionResponse()
     response.masks = masks
     return response
+
+
+def detect_keypoints(
+    request: BodyPixKeypointDetectionRequest,
+    debug_publisher: rospy.Publisher = rospy.Publisher(
+        "/bodypix/keypoint_debug", SensorImage, queue_size=1
+    ),
+) -> BodyPixKeypointDetectionResponse:
+
+    result, mask = run_inference(request.dataset, request.confidence, request.image_raw)
+
+    poses = result.get_poses()
+
+    detected_keypoints: List[BodyPixKeypoint] = []
+
+    for pose in poses:
+        for keypoint in pose.keypoints.values():
+            # Check if keypoint is in the mask
+            x = int(keypoint.position.x)
+            y = int(keypoint.position.y)
+            try:
+                if mask[y, x] == 0:
+                    continue
+            # Throws an error if the keypoint is out of bounds
+            # but not clear what type (some TF stuff)
+            except:
+                continue
+            rospy.loginfo(f"Keypoint {keypoint.part} at {x}, {y} is in mask")
+            detected_keypoints.append(
+                BodyPixKeypoint(keypoint_name=keypoint.part, x=x, y=y)
+            )
+
+    # publish to debug topic
+    if debug_publisher is not None:
+        # create coloured mask with poses
+        from tf_bodypix.draw import draw_poses
+
+        coloured_mask = result.get_colored_part_mask(mask).astype(np.uint8)
+        coloured_mask = draw_poses(
+            coloured_mask.copy(),
+            poses,
+            keypoints_color=(255, 100, 100),
+            skeleton_color=(100, 100, 255),
+        )
+
+        debug_publisher.publish(cv2_img.cv2_img_to_msg(coloured_mask))
+
+    return BodyPixKeypointDetectionResponse(keypoints=detected_keypoints)
