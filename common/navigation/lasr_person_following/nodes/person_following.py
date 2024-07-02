fd432800c586765365be1222c7c767e141db2c59
fireblonde <nicollehchevska@gmail.com>
2024-07-02 15:14:26 +0100

    chore: reconfigure

--- common/navigation/lasr_person_following/nodes/person_following.py
+++ common/navigation/lasr_person_following/nodes/person_following.py
@@ -9,17 +9,26 @@ from lasr_person_following.msg import (
     FollowFeedback,
 )
 
+from dynamic_reconfigure.srv import Reconfigure
+from dynamic_reconfigure.msg import Config, DoubleParameter, BoolParameter, IntParameter
+
 from lasr_person_following import PersonFollower
 
 import warnings
 
 
 class PersonFollowingServer:
-
     _server: actionlib.SimpleActionServer
     _follower: PersonFollower
 
     def __init__(self) -> None:
+        self._dynamic_costmap = rospy.ServiceProxy("/move_base/local_costmap/set_parameters", Reconfigure)
+        self._dynamic_velocity = rospy.ServiceProxy("/move_base/PalLocalPlanner/set_parameters", Reconfigure)
+        self._dynamic_recovery = rospy.ServiceProxy("/move_base/set_parameters", Reconfigure)
+
+        self._update_params()
+        rospy.sleep(1)
+        print("Updated params")
 
         self._server = actionlib.SimpleActionServer(
             "follow_person", FollowAction, execute_cb=self._execute_cb, auto_start=False
@@ -31,6 +40,7 @@ class PersonFollowingServer:
         self._server.start()
 
     def _execute_cb(self, _: FollowGoal) -> None:
+        print("Executing follow_person action")
         while not self._follower.begin_tracking(ask=False):
             rospy.logwarn("No people found, retrying...")
             rospy.sleep(rospy.Duration(1.0))
@@ -44,6 +54,23 @@ class PersonFollowingServer:
 
     def _preempt_cb(self) -> None:
         raise NotImplementedError("Preempt not implemented yet")
+
+    def _update_params(self):
+        config = Config()
+        config.ints.append(IntParameter(name="width", value=8))
+        config.ints.append(IntParameter(name="height", value=8))
+        self._dynamic_costmap(config)
+
+        config = Config()
+        config.doubles.append(DoubleParameter(name="max_vel_x", value=0.7))
+
+        self._dynamic_velocity(config)
+
+        config = Config()
+        config.bools.append(BoolParameter(name="recovery_behavior_enabled", value=0))
+        config.bools.append(BoolParameter(name="clearing_rotation_allowed", value=0))
+
+        self._dynamic_recovery(config)
 
 
 if __name__ == "__main__":
