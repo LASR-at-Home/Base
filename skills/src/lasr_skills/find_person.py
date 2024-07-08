db2c3b7f3cde20bdf92b3662953e2c844b44c1a9
Jared Swift <j.w.swift@outlook.com>
2024-07-08 01:01:41 +0100

    feat: merge and parameterise FindPerson skills.

--- skills/src/lasr_skills/find_person.py
+++ skills/src/lasr_skills/find_person.py
@@ -1,41 +1,22 @@
 import smach
+import smach_ros
 import rospy
 
-from lasr_skills import Detect3D, GoToLocation
+from lasr_skills import GoToLocation, AskAndListen, DetectGesture
 import navigation_helpers
 
-from geometry_msgs.msg import Pose, PoseWithCovarianceStamped
-
-from typing import List
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Polygon
+from lasr_vision_msgs.msg import CDRequest, CDResponse
+from lasr_vision_msgs.srv import (
+    CroppedDetectionRequest,
+    CroppedDetectionResponse,
+    CroppedDetection,
+    Recognise,
+)
+from typing import List, Literal
 
 
 class FindPerson(smach.StateMachine):
-    class GetLocation(smach.State):
-        def __init__(self):
-            smach.State.__init__(
-                self,
-                outcomes=["succeeded", "failed"],
-                input_keys=["location_index", "waypoints"],
-                output_keys=["location"],
-            )
-
-        def execute(self, userdata):
-            userdata.location = userdata.waypoints[userdata.location_index]
-            return "succeeded"
-
-    class GetPose(smach.State):
-        def __init__(self):
-            smach.State.__init__(
-                self,
-                outcomes=["succeeded", "failed"],
-                output_keys=["current_pose"],
-            )
-
-        def execute(self, userdata):
-            userdata.current_pose = rospy.wait_for_message(
-                "/amcl_pose", PoseWithCovarianceStamped
-            ).pose.pose
-            return "succeeded"
 
     class ComputePath(smach.State):
         def __init__(self, waypoints: List[Pose]):
@@ -47,39 +28,154 @@ class FindPerson(smach.StateMachine):
             )
             self._waypoints = waypoints
 
-        def execute(self, userdata):
+        def execute(self, userdata) -> str:
+            current_pose: Pose = rospy.wait_for_message(
+                "/robot_pose", PoseWithCovarianceStamped
+            ).pose.pose
             userdata.waypoints = navigation_helpers.min_hamiltonian_path(
-                userdata.current_pose, self._waypoints
+                current_pose, self._waypoints
             )
             return "succeeded"
 
-    class HandleDetections(smach.State):
-        def __init__(self):
-            smach.State.__init__(
+    class HandleDetections(smach.StateMachine):
+
+        class GetResponse(smach.State):
+            def __init__(self):
+                smach.State.__init__(
+                    self,
+                    outcomes=["succeeded", "failed"],
+                    input_keys=["responses"],
+                    output_keys=[
+                        "response",
+                        "responses",
+                        "person_point",
+                        "cropped_image",
+                    ],
+                )
+
+            def execute(self, userdata):
+                if len(userdata.responses[0].detections_3d) == 0:
+                    rospy.logwarn("No response available, returning failed.")
+                    return "failed"
+                userdata.response = userdata.responses[0].detections_3d.pop(0)
+                userdata.cropped_image = userdata.responses[0].cropped_imgs.pop(0)
+                return "succeeded"
+
+        def __init__(
+            self,
+            criteria: Literal["name", "pose", "gesture", "clothes"],
+            criteria_value: str,
+        ):
+            smach.StateMachine.__init__(
                 self,
                 outcomes=["succeeded", "failed"],
-                input_keys=["detections_3d"],
-                output_keys=["person_point"],
+                input_keys=["responses"],
+                output_keys=["responses", "person_point"],
             )
 
-        def execute(self, userdata):
-            if len(userdata.detections_3d.detected_objects) == 0:
-                rospy.logwarn("No person detected, returning failed.")
-                return "failed"
-            userdata.person_point = userdata.detections_3d.detected_objects[0].point
-            return "succeeded"
+            with self:
 
-    def __init__(self, waypoints: List[Pose]):
-        smach.StateMachine.__init__(
-            self, outcomes=["succeeded", "failed"], output_keys=["person_point"]
-        )
+                if criteria == "name":
+
+                    class HandleSpeechResponse(smach.State):
+
+                        def __init__(self):
+                            smach.State.__init__(
+                                self,
+                                outcomes=["succeeded", "failed"],
+                                input_keys=["transcribed_speech"],
+                            )
+
+                        def execute(self, userdata):
+                            # TODO: make this smarter,e.g. levenshtein distance
+                            if "yes" in userdata.transcribed_speech.lower():
+                                return "succeeded"
+                            return "failed"
+
+                    smach.StateMachine.add(
+                        "GET_RESPONSE",
+                        self.GetResponse(),
+                        transitions={"succeeded": "GO_TO_PERSON", "failed": "failed"},
+                    )
+
+                    smach.StateMachine.add(
+                        "GO_TO_PERSON",
+                        GoToLocation(),
+                        transitions={
+                            "succeeded": "ASK_NAME",
+                            "failed": "failed",
+                        },
+                        remapping={"location": "person_point"},
+                    )
+                    smach.StateMachine.add(
+                        "CHECK_NAME",
+                        AskAndListen(
+                            f"I'm looking for {criteria_value}. Are you {criteria_value}?"
+                        ),
+                        transitions={
+                            "succeeded": "HANDLE_SPEECH_RESPONSE",
+                            "failed": "failed",
+                        },
+                    )
+                    smach.StateMachine.add(
+                        "HANDLE_SPEECH_RESPONSE",
+                        HandleSpeechResponse(),
+                        transitions={
+                            "succeeded": "succeeded",
+                            "failed": "GET_RESPONSE",
+                        },
+                    )
+
+                elif criteria == "gesture":
+
+                    smach.StateMachine.add(
+                        "GET_RESPONSE",
+                        self.GetResponse(),
+                        transitions={"succeeded": "DETECT_GESTURE", "failed": "failed"},
+                    )
+
+                    smach.StateMachine.add(
+                        "DETECT_GESTURE",
+                        DetectGesture(criteria_value),
+                        transitions={
+                            "succeeded": "succeeded",
+                            "missing_keypoints": "GET_RESPONSE",
+                            "failed": "GET_RESPONSE",
+                        },
+                    )
+
+                elif criteria == "pose":
+                    raise NotImplementedError("Pose criteria not implemented")
+                elif criteria == "clothes":
+                    raise NotImplementedError("Clothes criteria not implemented")
+
+    def __init__(
+        self,
+        waypoints: List[Pose],
+        polygon: Polygon,
+        criteria: Literal["name", "pose", "gesture", "clothes"],
+        criteria_value: str,
+    ):
+
+        assert criteria in ["name", "pose", "gesture", "clothes"], "Invalid criteria"
+
+        if criteria == "gesture":
+            assert criteria_value in [
+                "raising_left_arm",
+                "raising_right_arm",
+                "pointing_to_the_right",
+                "pointing_to_the_left",
+            ], "Invalid gesture"
+        elif criteria == "pose":
+            assert criteria_value in [
+                "sitting",
+                "standing",
+                "lying_down",
+            ], "Invalid pose"
+
+        smach.StateMachine.__init__(self, outcomes=["succeeded", "failed"])
 
         with self:
-            smach.StateMachine.add(
-                "GET_POSE",
-                self.GetPose(),
-                transitions={"succeeded": "COMPUTE_PATH", "failed": "failed"},
-            )
 
             smach.StateMachine.add(
                 "COMPUTE_PATH",
@@ -118,28 +214,41 @@ class FindPerson(smach.StateMachine):
                             "failed": "failed",
                         },
                     )
+
                     smach.StateMachine.add(
-                        "DETECT3D",
-                        Detect3D(filter=["person"]),
+                        "DETECT",
+                        smach_ros.ServiceState(
+                            "/vision/cropped_detection",
+                            CroppedDetection,
+                            request=CroppedDetectionRequest(
+                                requests=[
+                                    CDRequest(
+                                        method="closest",
+                                        use_mask=True,
+                                        yolo_model="yolov8x-seg.pt",
+                                        yolo_model_confidence=0.5,
+                                        yolo_nms_threshold=0.3,
+                                        return_sensor_reading=False,
+                                        object_names=["person"],
+                                        polygons=[polygon],
+                                    )
+                                ]
+                            ),
+                            output_keys=["responses"],
+                            response_slots=["responses"],
+                        ),
                         transitions={
                             "succeeded": "HANDLE_DETECTIONS",
-                            "failed": "failed",
+                            "aborted": "failed",
+                            "preempted": "failed",
                         },
                     )
+
                     smach.StateMachine.add(
                         "HANDLE_DETECTIONS",
-                        self.HandleDetections(),
+                        self.HandleDetections(criteria, criteria_value),
                         transitions={
                             "succeeded": "succeeded",
                             "failed": "continue",
                         },
                     )
-
-                waypoint_iterator.set_contained_state(
-                    "CONTAINER_STATE", container_sm, loop_outcomes=["continue"]
-                )
-            smach.StateMachine.add(
-                "WAYPOINT_ITERATOR",
-                waypoint_iterator,
-                {"succeeded": "succeeded", "failed": "failed"},
-            )
--- tasks/gpsr/src/gpsr/state_machine_factory.py
+++ tasks/gpsr/src/gpsr/state_machine_factory.py
@@ -3,8 +3,10 @@ import rospy
 import smach
 from smach_ros import ServiceState
 from typing import Dict, List
-from lasr_skills import GoToLocation, FindNamedPerson
+from lasr_skills import GoToLocation, FindPerson
 from gpsr.states import Talk
+
+from geometry_msgs.msg import Pose, Point, Quaternion, Polygon
 
 STATE_COUNT = 0
 
@@ -31,33 +33,62 @@ def build_state_machine(parsed_command: Dict) -> smach.StateMachine:
     with sm:
         for command_verb, command_param in zip(command_verbs, command_params):
             if command_verb == "greet":
+                location_param = f"/gpsr/arena/rooms/{command_param['location']}"
+                sm.add(
+                    f"STATE_{increment_state_count()}",
+                    GoToLocation(location_param=f"{location_param}/pose"),
+                    transitions={
+                        "succeeded": f"STATE_{STATE_COUNT + 1}",
+                        "failed": "failed",
+                    },
+                )
+
+                waypoints: List[Pose] = [
+                    Pose(
+                        position=Point(**wp["position"]),
+                        orientation=Quaternion(**wp["orientation"]),
+                    )
+                    for wp in rospy.get_param(location_param)["waypoints"]
+                ]
+
+                polygon = Polygon(
+                    [
+                        Point(**p)
+                        for p in rospy.get_param(location_param)["polygon"]["points"]
+                    ]
+                )
+
                 if "name" in command_param:
-                    location_param = f"/gpsr/arena/rooms/{command_param['location']}"
-                    sm.add(
-                        f"STATE_{increment_state_count()}",
-                        GoToLocation(location_param=f"{location_param}/pose"),
-                        transitions={
-                            "succeeded": f"STATE_{STATE_COUNT + 1}",
-                            "failed": "failed",
-                        },
-                    )
-                    sm.add(
-                        f"STATE_{increment_state_count()}",
-                        FindNamedPerson(
-                            name=command_param["name"], location_param=location_param
-                        ),
-                        transitions={
-                            "succeeded": f"STATE_{STATE_COUNT + 1}",
-                            "failed": "failed",
-                        },
-                    )
+                    criteria = "name"
+                    criteria_value = command_param["name"]
                 elif "clothes" in command_param:
-                    # TODO:
-                    pass
+                    criteria = "clothes"
+                    criteria_value = command_param["clothes"]
+                elif "gesture" in command_param:
+                    criteria = "gesture"
+                    criteria_value = command_param["gesture"]
+                elif "pose" in command_param:
+                    criteria = "pose"
+                    criteria_value = command_param["pose"]
                 else:
                     raise ValueError(
-                        "Greet command received with no name or clothes in command parameters"
+                        "Greet command received with no name, clothes, gesture, or pose in command parameters"
                     )
+
+                sm.add(
+                    f"STATE_{increment_state_count()}",
+                    FindPerson(
+                        waypoints=waypoints,
+                        polygon=polygon,
+                        criteria=criteria,
+                        criteria_value=criteria_value,
+                    ),
+                    transitions={
+                        "succeeded": f"STATE_{STATE_COUNT + 1}",
+                        "failed": "failed",
+                    },
+                )
+
             elif command_verb == "talk":
                 if "gesture" in command_param:
                     # TODO:
@@ -72,5 +103,6 @@ def build_state_machine(parsed_command: Dict) -> smach.StateMachine:
                     raise ValueError(
                         "Talk command received with no gesture or talk in command parameters"
                     )
+
     rospy.loginfo(f"State machine: {sm}")
     return sm
