7408a40046e177666fa1e3689b7000b917161594
Jared Swift <j.w.swift@outlook.com>
2024-07-09 04:32:57 +0100

    feat: find person in pose.

--- skills/src/lasr_skills/detect_pose.py
+++ skills/src/lasr_skills/detect_pose.py
@@ -1,18 +1,58 @@
 import smach
 
-from typing import Optional
+from typing import Optional, List, Dict
+
+from lasr_skills import QueryImage
 
 
-class DetectPose(smach.State):
+class DetectPose(smach.StateMachine):
 
+    _pose_dict: Dict[str, str] = {
+        "a person standing": "standing",
+        "a person sitting": "sitting",
+        "a person laying down": "lying_down",
+    }
+
+    _possible_answers: List[str] = [
+        "a person standing",
+        "a person sitting",
+        "a person laying down",
+    ]
     _pose_to_detect: Optional[str]
 
-    def __init__(self, pose_to_detect: Optional[str] = None):
-        smach.State.__init__(
+    class HandleResponse(smach.State):
+
+        _pose_dict: Dict[str, str]
+        _pose_to_detect: str
+
+        def __init__(self, pose_dict: Dict[str, str], pose_to_detect: str):
+            smach.State.__init__(
+                self, outcomes=["succeeded", "failed"], input_keys=["answer"]
+            )
+
+            self._pose_dict = pose_dict
+            self._pose_to_detect = pose_to_detect
+
+        def execute(self, userdata):
+            if self._pose_dict[userdata.answer] == self._pose_to_detect:
+                return "succeeded"
+            return "failed"
+
+    def __init__(self, pose_to_detect: str):
+        smach.StateMachine.__init__(
             self, outcomes=["succeeded", "failed"], input_keys=["img_msg"]
         )
 
         self._pose_to_detect = pose_to_detect
 
-    def execute(self, userdata):
-        return "succeeded"
+        with self:
+            smach.StateMachine.add(
+                "QUERY_IMAGE",
+                QueryImage(possible_answers=self._possible_answers),
+                transitions={"succeeded": "HANDLE_RESPONSE", "failed": "failed"},
+            )
+            smach.StateMachine.add(
+                "HANDLE_RESPONSE",
+                self.HandleResponse(self._pose_dict, self._pose_to_detect),
+                transitions={"succeeded": "succeeded", "failed": "failed"},
+            )
