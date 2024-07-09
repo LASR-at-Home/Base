203a536e18dcac924e67eacb85db4a19a161f4d1
Jared Swift <j.w.swift@outlook.com>
2024-07-09 03:02:13 +0100

    fix: gesture detection

--- skills/src/lasr_skills/detect_gesture.py
+++ skills/src/lasr_skills/detect_gesture.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-from typing import Optional, List
 import smach
 import rospy
 import cv2
 import cv2_img
-from lasr_skills.vision import GetCroppedImage, GetImage
 from lasr_vision_msgs.srv import (
     BodyPixKeypointDetection,
     BodyPixKeypointDetectionRequest,
@@ -19,18 +16,16 @@ class DetectGesture(smach.State):
 
     def __init__(
         self,
-        gesture_to_detect: Optional[str] = None,
+        gesture_to_detect: str,
         bodypix_model: str = "resnet50",
         bodypix_confidence: float = 0.1,
         buffer_width: int = 50,
         debug_publisher: str = "/skills/gesture_detection/debug",
     ):
-        """Optionally stores the gesture to detect. If None, it will infer the gesture from the keypoints."""
         smach.State.__init__(
             self,
-            outcomes=["succeeded", "missing_keypoints", "failed"],
+            outcomes=["succeeded", "failed"],
             input_keys=["img_msg"],
-            output_keys=["gesture_detected"],
         )
         self.gesture_to_detect = gesture_to_detect
         self.bodypix_client = rospy.ServiceProxy(
@@ -63,58 +58,41 @@ class DetectGesture(smach.State):
 
         detected_keypoints = res.keypoints
 
+        detected_gesture = "none"
+
         keypoint_info = {
             keypoint.keypoint_name: {"x": keypoint.x, "y": keypoint.y}
             for keypoint in detected_keypoints
         }
 
+        # raising left arm
+        if "leftShoulder" in keypoint_info and "leftWrist" in keypoint_info:
+            if keypoint_info["leftWrist"]["y"] < keypoint_info["leftShoulder"]["y"]:
+                detected_gesture = "raising_left_arm"
+        # pointing to the left
         if "leftShoulder" in keypoint_info and "leftWrist" in keypoint_info:
             if (
-                self.gesture_to_detect == "raising_left_arm"
-                or self.gesture_to_detect is None
+                keypoint_info["leftWrist"]["x"] - self.buffer_width
+                > keypoint_info["leftShoulder"]["x"]
             ):
-                if keypoint_info["leftWrist"]["y"] < keypoint_info["leftShoulder"]["y"]:
-                    self.gesture_to_detect = "raising_left_arm"
+                detected_gesture = "pointing_to_the_left"
+        # raising right arm
+        if "rightShoulder" in keypoint_info and "rightWrist" in keypoint_info:
+            if keypoint_info["rightWrist"]["y"] < keypoint_info["rightShoulder"]["y"]:
+                detected_gesture = "raising_right_arm"
+        # pointing to the right
+        if "rightShoulder" in keypoint_info and "rightWrist" in keypoint_info:
             if (
-                self.gesture_to_detect == "pointing_to_the_left"
-                or self.gesture_to_detect is None
+                keypoint_info["rightShoulder"]["x"] - self.buffer_width
+                > keypoint_info["rightWrist"]["x"]
             ):
-                if (
-                    keypoint_info["leftWrist"]["x"] - self.buffer_width
-                    > keypoint_info["leftShoulder"]["x"]
-                ):
-                    self.gesture_to_detect = "pointing_to_the_left"
+                detected_gesture = "pointing_to_the_right"
 
-        if (
-            "rightShoulder" in keypoint_info
-            and "rightWrist" in keypoint_info
-            and self.gesture_to_detect is None
-        ):
-            print(keypoint_info["rightShoulder"]["x"], keypoint_info["rightWrist"]["x"])
-            if (
-                self.gesture_to_detect == "raising_right_arm"
-                or self.gesture_to_detect is None
-            ):
-                if (
-                    keypoint_info["rightWrist"]["y"]
-                    < keypoint_info["rightShoulder"]["y"]
-                ):
-                    self.gesture_to_detect = "raising_right_arm"
-            if (
-                self.gesture_to_detect == "pointing_to_the_right"
-                or self.gesture_to_detect is None
-            ):
-                if (
-                    keypoint_info["rightShoulder"]["x"] - self.buffer_width
-                    > keypoint_info["rightWrist"]["x"]
-                ):
-                    self.gesture_to_detect = "pointing_to_the_right"
+        if self.gesture_to_detect == "waving":
+            if detected_gesture in ["raising_left_arm", "raising_right_arm"]:
+                detected_gesture = "waving"
 
-        if self.gesture_to_detect is None:
-            self.gesture_to_detect = "none"
-
-        rospy.loginfo(f"Detected gesture: {self.gesture_to_detect}")
-        userdata.gesture_detected = self.gesture_to_detect
+        rospy.loginfo(f"Detected gesture: {detected_gesture}")
 
         cv2_gesture_img = cv2_img.msg_to_cv2_img(userdata.img_msg)
         # Add text to the image
@@ -131,43 +109,4 @@ class DetectGesture(smach.State):
         # Publish the image
         self.debug_publisher.publish(cv2_img.cv2_img_to_msg(cv2_gesture_img))
 
-        return "succeeded"
-
-
-### For example usage:
-class GestureDetectionSM(smach.StateMachine):
-    """
-    State machine for detecting gestures.
-    """
-
-    def __init__(self, gesture_to_detect: Optional[str] = None):
-        smach.StateMachine.__init__(self, outcomes=["succeeded", "failed"])
-        self.gesture_to_detect = gesture_to_detect
-        with self:
-            smach.StateMachine.add(
-                "GET_IMAGE",
-                GetCroppedImage(
-                    "person", "centered", use_mask=True, rgb_topic="/usb_cam/image_raw"
-                ),
-                transitions={"succeeded": "BODY_PIX_DETECTION", "failed": "failed"},
-            )
-
-            smach.StateMachine.add(
-                "BODY_PIX_DETECTION",
-                DetectGesture(gesture_to_detect=self.gesture_to_detect),
-                transitions={
-                    "succeeded": "succeeded",
-                    "failed": "failed",
-                    "missing_keypoints": "failed",
-                },
-            )
-
-
-if __name__ == "__main__":
-    rospy.init_node("gesture_detection_sm")
-    ### Example usage:
-    while not rospy.is_shutdown():
-        sm = GestureDetectionSM()
-        sm.execute()
-
-    rospy.spin()
+        return "succeeded" if detected_gesture == self.gesture_to_detect else "failed"
