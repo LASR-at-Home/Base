57590db8e22f62ce7387bd01e4f449165043a2a2
Siyao <sveali41@gmail.com>
2024-07-15 17:47:10 +0100

    find object

--- tasks/gpsr/src/gpsr/states/go_find_the_object.py
+++ tasks/gpsr/src/gpsr/states/go_find_the_object.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import smach
 import smach_ros
-from lasr_skills import Detect3D, GoToLocation, LookToPoint
+from lasr_skills import GoToLocation, LookToPoint, Say
 from shapely.geometry.polygon import Polygon
 from typing import List, Union
 from geometry_msgs.msg import Pose, Point, Quaternion, CDRequest
-from lasr_skills import Say, LookToPoint
 from lasr_vision_msgs.srv import CroppedDetection, CroppedDetectionRequest
 import rospy
 
@@ -193,7 +192,7 @@ class GoFindTheObject(smach.StateMachine):
                         "aborted": "failed",
                         "preempted": "failed",
                     },
-                    remapping={"polygon": "polygon"}
+                        remapping={"polygon": "polygon"}
                     )
 
                     smach.StateMachine.add(
