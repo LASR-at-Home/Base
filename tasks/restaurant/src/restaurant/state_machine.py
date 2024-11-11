b091cc70df3623a0775f5cedab66096de9ed4c38
Jared Swift <jared.swift@kcl.ac.uk>
2024-11-11 10:57:45 +0000

    feat: initial setup.

---
+++ tasks/restaurant/src/restaurant/state_machine.py
@@ -0,0 +1,35 @@
+import smach
+import smach_ros
+from lasr_skills import Say
+from std_msgs.msg import Empty
+
+
+class Restaurant(smach.StateMachine):
+
+    def __init__(self) -> None:
+        super().__init__(outcomes=["succeeded", "failed"])
+
+        with self:
+            smach.StateMachine.add(
+                "WAIT_START",
+                smach_ros.MonitorState(
+                    "/restaurant/start",
+                    Empty,
+                    lambda _ud, _msg: False,
+                ),
+                transitions={
+                    "valid": "WAIT_START",
+                    "preempted": "WAIT_START",
+                    "invalid": "SAY_START",
+                },
+            )
+
+            smach.StateMachine.add(
+                "SAY_START",
+                Say(text="Start of Restaurant task."),
+                transitions={
+                    "succeeded": "succeeded",
+                    "aborted": "failed",
+                    "preempted": "failed",
+                },
+            )
