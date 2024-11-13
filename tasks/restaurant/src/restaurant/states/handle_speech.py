49d4a59bca7b70c06ce7a26e6d0e58d415a41772
Haiwei L <haiwei8809@gmail.com>
2024-11-13 18:37:44 +0000

    Identify item and number in transcription

--- tasks/restaurant/src/restaurant/states/handle_speech.py
+++ tasks/restaurant/src/restaurant/states/handle_speech.py
@@ -53,7 +53,9 @@ snacks_list = [
     "tictac"
 ]
 
-items_list = list(set(drinks_list) | set(food_list) | set(fruits_list) | set(snacks_list))
+items_list = drinks_list + food_list + fruits_list + snacks_list
+
+numbers_list = ["a", "one", "two", "three", "four", "five"]
 
 def main(guest_transcription):
 
@@ -65,7 +67,25 @@ def main(guest_transcription):
     print(sentence_list)
 
 
+    number_items = []
+    for i in range(len(sentence_list)):
+        if sentence_list[i] in numbers_list:
+            number_items.append(sentence_list[i])
+
+    print(number_items)
+
+    matched_items = []
+    for i in range(len(sentence_list)):
+        for item in items_list:
+            item_words = item.split()
+            if sentence_list[i:i + len(item_words)] == item_words:
+                matched_items.append(item)
+                break 
+    
+    print(matched_items)
+
+
 
 if __name__ == "__main__":
-    guest_transcription = "Hello, are you okay"
+    guest_transcription = "Hello, are you okay. I would like a curry, three big coke and one stroopwafel"
     main(guest_transcription)
