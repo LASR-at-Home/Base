f87018f4fae74920d61c1bf6378c296515ce5503
Ma'ayan Armony <99334379+maayan25@users.noreply.github.com>
2025-03-08 13:40:24 +0000

    Ros2 yolo (#292)
    * Create YOLO package
    
    * port YOLO to ROS2 before testing
    
    * Fix CMakeLists
    
    * Fix import and node duplication issues
    
    * port YOLO launch files
    
    * Add relay test and test with camera input
    
    * Reformat code: Black

---
+++ common/vision/lasr_vision_yolov8/lasr_vision_yolov8/service.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+from typing import Dict
+
+import rclpy
+from ament_index_python import packages
+
+from lasr_vision_yolov8 import yolo as yolo
+from lasr_vision_yolov8.yolo import AccessNode
+
+# from src import lasr_vision_yolov8 as yolo
+# from src import AccessNode
+from sensor_msgs.msg import Image
+from lasr_vision_interfaces.srv import (
+    YoloDetection,
+)
+
+# TODO handle 3D detection service later on
+
+
+class YoloServiceNode:
+    def __init__(self):
+        self.node = AccessNode.get_node()
+        # Determine variables
+        self.node.declare_parameter(
+            "~preload", ["yolov8n.pt"]
+        )  # to have a default value.. maybe there is a cleaner way to do this
+        self.preload = self.node.get_parameter("~preload").value
+        print(f"Preloading models: {self.preload}, type: {type(self.preload)}")
+
+        for model in self.preload:
+            yolo.load_model(model)
+
+        # Prepare publisher
+        self.debug_publishers: Dict[str, rclpy.node.Publisher] = {}
+        self.debug_publisher = self.node.create_publisher(Image, "/yolov8/debug", 1)
+
+        yolo.start_tf_buffer()
+        self.node.create_service(YoloDetection, "/yolov8/detect", self.detect)
+        self.node.get_logger().info("YOLOv8 service started")
+
+    def detect(
+        self, request: YoloDetection.Request(), response: YoloDetection.Response()
+    ) -> YoloDetection.Response():
+        """
+        Hand off detection request to yolo library
+        """
+        if request.dataset in self.debug_publishers:
+            debug_publisher = self.debug_publishers[request.dataset]
+        else:
+            topic_name = re.sub(r"[\W_]+", "", request.dataset)
+            if topic_name == "":
+                topic_name = "/yolov8/debug"
+            else:
+                topic_name = f"/yolov8/debug/{topic_name}"
+            debug_publisher = self.node.create_publisher(Image, topic_name, 1)
+        response = yolo.detect(request, debug_publisher)
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    # Put ourselves in the model folder
+    package_install = packages.get_package_prefix("lasr_vision_yolov8")
+    package_path = os.path.abspath(
+        os.path.join(
+            package_install,
+            os.pardir,
+            os.pardir,
+            "common/vision/lasr_vision_yolov8",
+        )
+    )
+    os.chdir(os.path.abspath(os.path.join(package_path, "models")))
+
+    node = AccessNode.get_node()
+    yolo_service = YoloServiceNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        # rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ common/vision/lasr_vision_yolov8/src/lasr_vision_yolov8/yolo.py
@@ -0,0 +1,113 @@
+import rclpy
+from rclpy.node import Node
+import cv2_img
+from ultralytics import YOLO
+
+from lasr_vision_interfaces.msg import Detection, Detection3D
+from lasr_vision_interfaces.srv import (
+    YoloDetection,
+    # YoloDetection3D,
+)
+
+import tf2_ros as tf
+
+# import tf2_sensor_msgs  # noqa
+
+# TODO address 3D Detection later on (check ROS1 implementation)
+
+# global tf buffer
+tf_buffer = tf.Buffer()  # cache_time=rospy.Duration(10) needed for 3D
+
+
+def start_tf_buffer() -> None:
+    tf.TransformListener(tf_buffer, AccessNode.get_node())
+
+
+# model cache
+loaded_models = {}
+
+
+def load_model(dataset: str) -> None:
+    """
+    Load a model into cache
+    """
+    node = AccessNode.get_node()
+    model = None
+    if dataset in loaded_models:
+        model = loaded_models[dataset]
+    else:
+        model = YOLO(dataset)
+        node.get_logger().info(f"Loaded {dataset} model")
+
+        loaded_models[dataset] = model
+
+    return model
+
+
+def detect(
+    request: YoloDetection.Request(), debug_publisher: rclpy.node.Publisher
+) -> YoloDetection.Response():
+    """
+    Run YOLO inference on given detection request
+    """
+    node = AccessNode.get_node()
+
+    # decode the image
+    node.get_logger().info("Decoding")
+    img = cv2_img.msg_to_pillow_img(request.image_raw)
+
+    # load model
+    node.get_logger().info("Loading model")
+    model = load_model(request.dataset)
+
+    # run inference
+    node.get_logger().info("Running inference")
+    results = model(img, conf=request.confidence, iou=request.nms)
+    result = results[0]
+    node.get_logger().info("Inference complete")
+
+    # construct response
+    detected_objects = []
+    object_count = result.boxes.cls.size(dim=0)
+    has_segment_masks = result.masks is not None
+    for i in range(0, object_count):
+        detection = Detection()
+        detection.name = result.names[int(result.boxes.cls[i])]
+        detection.confidence = float(result.boxes.conf.cpu().numpy()[i])
+        detection.xywh = result.boxes.xywh[i].cpu().numpy().astype(int).tolist()
+
+        # copy segmented mask if available
+        if has_segment_masks:
+            detection.xyseg = result.masks.xy[i].flatten().astype(int).tolist()
+
+        detected_objects.append(detection)
+
+    # publish to debug topic
+    debug_publisher.publish(cv2_img.cv2_img_to_msg(result.plot()))
+
+    response = YoloDetection.Response()
+    response.detected_objects = detected_objects
+    return response
+
+
+class AccessNode(Node):
+    """
+    Class to  create and access the node to avoid duplications
+    """
+
+    _node = None  # Static variable to hold the node instance
+
+    @staticmethod
+    def get_node():
+        """Returns the singleton ROS 2 node instance, creating it if necessary."""
+        if AccessNode._node is None:
+            AccessNode._node = Node("yolo_access_node")
+        return AccessNode._node
+
+    @staticmethod
+    def shutdown():
+        """Shuts down the singleton node properly."""
+        if AccessNode._node is not None:
+            AccessNode._node.destroy_node()
+            AccessNode._node = None
+            AccessNode.shutdown()
