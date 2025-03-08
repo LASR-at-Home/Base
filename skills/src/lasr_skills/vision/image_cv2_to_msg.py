06d46667b14d735b95901835ce1229cec7418767
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2025-03-08 11:14:04 +0000

    ros2 image_cv2_to_msg.py tested

---
+++ skills/src/lasr_skills/vision/image_cv2_to_msg.py
@@ -0,0 +1,22 @@
+import os
+import smach
+import cv2_img
+import rclpy
+# from .get_image import GetImage, ROS2HelperNode
+
+class ImageCv2ToMsg(smach.State):
+    """
+    State for converting cv2 image to sensor Image message
+    """
+
+    def __init__(self):
+        smach.State.__init__(
+            # self, outcomes=["succeeded", "failed"], input_keys=["img", "img_msg"], output_keys=["img_msg"]
+            self, outcomes=["succeeded", "failed"], input_keys=["img"], output_keys=["img_msg"]
+
+    )
+
+    def execute(self, userdata):
+        userdata.img_msg = cv2_img.cv2_img_to_msg(userdata.img)
+        # print(userdata.img_msg)
+        return "succeeded"
