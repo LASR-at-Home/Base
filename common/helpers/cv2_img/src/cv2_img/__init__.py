cef0cee43c230d5bb9a1865535268f074be2efee
Jared Swift <jared.swift@kcl.ac.uk>
2025-03-26 15:03:19 +0000

    Fix msg_to_pillow_img

--- common/helpers/cv2_img/src/cv2_img/__init__.py
+++ common/helpers/cv2_img/src/cv2_img/__init__.py
@@ -52,7 +52,10 @@ def msg_to_pillow_img(msg: SensorImage):
         # BGR => RGB
         img = Image.fromarray(np.array(img)[:, :, ::-1])
     elif msg.encoding == "rgb8":
-        img = Image.frombytes("RGB", size, msg.data, "raw")
+        data = np.frombuffer(msg.data, dtype=np.uint8).reshape(
+            (msg.height, msg.width, 3)
+        )
+        img = Image.fromarray(data)
     else:
         raise Exception(f"Unsupported format {msg.encoding}.")
     return img
