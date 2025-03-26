d48b6e0206cc023bb43f4902860572d9b053ea9a
Jared Swift <jared.swift@kcl.ac.uk>
2025-03-26 15:02:38 +0000

    Cleanup CLIP

--- common/vision/lasr_vision_clip/setup.py
+++ common/vision/lasr_vision_clip/setup.py
@@ -5,7 +5,8 @@ package_name = "lasr_vision_clip"
 setup(
     name=package_name,
     version="0.0.0",
-    packages=find_packages(exclude=["test"]),
+    packages=find_packages(exclude=["test"], where="src"),
+    package_dir={"": "src"},
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
@@ -14,15 +15,11 @@ setup(
     zip_safe=True,
     maintainer="aaliyah",
     maintainer_email="aaliyah.merchant@kcl.ac.uk",
-    description="Object recognition",
     license="MIT",
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "test_clip_utils = lasr_vision_clip.test_clip_utils:main",
-            "test_2_clip_utils = lasr_vision_clip.test_2_clip_utils:main",
-            "clip_utils = clip_utils:main",
-            "learn_face = learn_face:main",
+            "test_vqa = lasr_vision_clip.nodes.test_vqa:main",
             "vqa = lasr_vision_clip.nodes.vqa:main",
         ]
     },
