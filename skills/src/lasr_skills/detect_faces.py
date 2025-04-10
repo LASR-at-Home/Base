b4f9bcdc8c6f6df5a4f3ca121f6d5a1a000eaa88
maab <maab@nmslap008746.client.kcl.ac.uk>
2025-04-10 18:29:24 +0100

    changes to detect_faces

--- skills/src/lasr_skills/detect_faces.py
+++ skills/src/lasr_skills/detect_faces.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.wait_for_message import wait_for_message 
 import smach
 
 from lasr_vision_interfaces.srv import DetectFaces as DetectFacesSrv
@@ -20,25 +21,32 @@ class DetectFaces(smach.State):
             output_keys=["detections"],
         )
         self.node = node
-        self.img_msg = None
         self._image_topic = image_topic
         self._detect_faces = self.node.create_client(DetectFacesSrv, "/detect_faces")
 
-         while not self._detect_faces.wait_for_service(timeout_sec=1.0):
+        while not self._detect_faces.wait_for_service(timeout_sec=1.0):
             self.node.get_logger().info("Waiting for /detect_faces service...")
 
     def execute(self, userdata):
-        self.img_msg = pcl_to_img_msg(userdata.pcl_msg)
-        if self.img_msg is None:
+        img_msg = pcl_to_img_msg(userdata.pcl_msg)
+        if img_msg is None:
             self.node.get_logger().info(f"No image from point cloud, waiting on topic: {self._image_topic}")
             try:
-                self.img_msg = self.node.wait_for_message(self._image_topic, Image, timeout_sec=5.0)
+                img_msg = wait_for_message(
+                    node=self.node,
+                    topic=self._image_topic,
+                    msg_type=Image,
+                    timeout_sec=5.0,
+                )
+                
             except Exception as e:
                 self.node.get_logger().error(f"Failed to get image from topic: {str(e)}")
                 return "failed"
 
 
-        request = DetectFacesSrv.Request(image_raw=self.img_msg)
+        request = DetectFacesSrv.Request()
+        request.image_raw = img_msg
+
         future = self._detect_faces.call_async(request)
         rclpy.spin_until_future_complete(self.node, future)
 
@@ -48,4 +56,5 @@ class DetectFaces(smach.State):
         else:
             self.node.get_logger().error("Detect faces service call failed.")
             return "failed"
-            
+
+     
