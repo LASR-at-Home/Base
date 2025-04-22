c46292e40eadefbc59aa76f21bd34c9d9a97a1dd
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2025-04-22 16:14:25 +0100

    (25) Modify handle_name with _GUEST

--- tasks/receptionist/src/receptionist/states/handle_name.py
+++ tasks/receptionist/src/receptionist/states/handle_name.py
@@ -20,13 +20,13 @@ class HandleName(smach.StateMachine):
 
             with self:
                 smach.StateMachine.add(
-                    f"GET_NAME_{guest_id}",
+                    f"GET_NAME_GUEST_{guest_id}",
                     AskAndListen(
                         "Please say 'Hi Tiago' for me to begin listening. What is your name?"
                     ),
                     transitions={
-                        "succeeded": f"PARSE_NAME_{guest_id}",
-                        "failed": f"PARSE_NAME_{guest_id}",
+                        "succeeded": f"PARSE_NAME_GUEST_{guest_id}",
+                        "failed": f"PARSE_NAME_GUEST_{guest_id}",
                     },
                 )
 
@@ -35,24 +35,24 @@ class HandleName(smach.StateMachine):
                     GetName(guest_id, False),
                     transitions={
                         "succeeded": "succeeded",
-                        "failed": f"REPEAT_GET_NAME_{guest_id}",
+                        "failed": f"REPEAT_GET_NAME_GUEST_{guest_id}",
                     },
                     remapping={"guest_transcription": "transcribed_speech"},
                 )
 
                 smach.StateMachine.add(
-                    f"REPEAT_GET_NAME_{guest_id}",
+                    f"REPEAT_GET_NAME_GUEST_{guest_id}",
                     AskAndListen(
                         "Please speak louder. What is your name?",
                     ),
                     transitions={
-                        "succeeded": f"REPEAT_PARSE_NAME_{guest_id}",
+                        "succeeded": f"REPEAT_PARSE_NAME_GUEST_{guest_id}",
                         "failed": "succeeded",
                     },
                 )
 
                 smach.StateMachine.add(
-                    f"REPEAT_PARSE_NAME_{guest_id}",
+                    f"REPEAT_PARSE_NAME_GUEST_{guest_id}",
                     GetName(guest_id, True),
                     transitions={
                         "succeeded": "succeeded",
