622875e47f13bf32f7c456c18a3ad0883b0453ea
Jared Swift <jared.swift@kcl.ac.uk>
2025-06-14 16:32:35 +0100

    GGUF

--- common/language/lasr_llm/nodes/llm.py
+++ common/language/lasr_llm/nodes/llm.py
@@ -4,37 +4,23 @@ from typing import Dict, Any
 
 import rospy
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-from transformers.pipelines.text_generation import TextGenerationPipeline
+from llama_cpp import Llama
+
 
 from lasr_llm_msgs.srv import Llm, LlmRequest, LlmResponse
 
 
 class LLMService:
 
-    _generation_args: Dict[str, Any] = {
-        "max_new_tokens": 500,
-        "return_full_text": False,
-    }
-
-    _model: AutoModelForCausalLM
-    _tokenizer: AutoTokenizer
-    _pipeline: TextGenerationPipeline
+    _model: Llama
 
     def __init__(self):
-        self._model = AutoModelForCausalLM.from_pretrained(
-            "microsoft/Phi-3-mini-4k-instruct",
-            device_map="cpu",
-            torch_dtype="auto",
-            trust_remote_code=True,
-        )
-        self._tokenizer = AutoTokenizer.from_pretrained(
-            "microsoft/Phi-3-mini-4k-instruct"
-        )
-        self._pipeline = pipeline(
-            "text-generation",
-            model=self._model,
-            tokenizer=self._tokenizer,
+        self._model = Llama.from_pretrained(
+            repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
+            verbose=False,
+            filename="*fp16.gguf",
+            n_ctx=4096,
+            device="cpu",
         )
 
         self._service = rospy.Service("/lasr_llm/llm", Llm, self._llm)
@@ -43,17 +29,16 @@ class LLMService:
     def _llm(self, request: LlmRequest) -> LlmResponse:
         response = LlmResponse()
 
-        llm_input = [
-            {"role": "system", "content": request.system_prompt},
-            {"role": "user", "content": request.prompt},
-        ]
-        full_prompt = self._tokenizer.apply_chat_template(
-            llm_input, tokenize=False, add_generation_prompt=True
+        prompt = f"{request.system_prompt} The user says: {request.prompt}"
+        rospy.loginfo(f"Prompting LLM with prompt:\n {prompt}")
+        llm_output = self._model(
+            f"<|user|>\n{prompt}<|end|>\n<|assistant|>",
+            max_tokens=256,
+            stop=["<|end|>"],
+            echo=False,
         )
-        rospy.loginfo(f"Prompting LLM with prompt:\n {full_prompt}")
-        llm_output = self._pipeline(llm_input, **self._generation_args)
-        rospy.loginfo(f"LLM Output:\n {llm_input}")
-        response.output = llm_output[0]["generated_text"]
+        rospy.loginfo(f"LLM Output:\n {llm_output}")
+        response.output = llm_output["choices"][0]["text"])
 
         return response
 
