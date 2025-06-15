51800eda7bbec8ba3a0194ecdc17e8fffe127366
Jared Swift <jared.swift@kcl.ac.uk>
2025-06-15 22:07:10 +0100

    Format.

--- common/manipulation/lasr_manipulation_3d_completion/setup.py
+++ common/manipulation/lasr_manipulation_3d_completion/setup.py
@@ -5,8 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=["lasr_manipulation_3d_completion"], package_dir={"": "src"}
-    
 )
 
 setup(**setup_args)
-
