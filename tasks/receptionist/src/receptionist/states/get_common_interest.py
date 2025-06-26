4f3226fbe9afd9e5534f523d92712654bbc7bf4c
Matt <mattbarker322@gmail.com>
2025-06-26 16:38:25 +0100

    feat: add sentence embedding similaity into interest commonality

--- tasks/receptionist/src/receptionist/states/get_common_interest.py
+++ tasks/receptionist/src/receptionist/states/get_common_interest.py
@@ -2,13 +2,13 @@ import rospy
 import smach
 
 from lasr_skills import Say
-from lasr_llm_msgs.srv import Llm, LlmRequest
+from lasr_llm_msgs.srv import SentenceEmbedding
 
 
 class GetInterest(smach.State):
     """Prompts an LLM to find a common interest between the guests and the host."""
 
-    _llm: rospy.ServiceProxy
+    _sentence_embed_srv: rospy.ServiceProxy
 
     def __init__(self):
         smach.State.__init__(
@@ -17,32 +17,33 @@ class GetInterest(smach.State):
             input_keys=["guest_data"],
             output_keys=["interest_message"],
         )
-        self._llm = rospy.ServiceProxy("/lasr_llm/llm", Llm)
-        self._llm.wait_for_service()
+        self._sentence_embed_srv = rospy.ServiceProxy(
+            "/lasr_sentence_embedding/sentence_embedding", SentenceEmbedding
+        )
+        self._sentence_embed_srv.wait_for_service()
 
     def execute(self, userdata):
-        failure_message = "I'm afraid I couldn't find a common interest between you all, you'll have to make do with small talk."
         try:
-            request = LlmRequest()
-            request.system_prompt = f"You are a robot acting as a party host. You are tasked with finding a common interest between two people, out of a group of three people. You will receive input such as 'Name: James, Interest: football. Name: Chloe, Interest: baking. Name: John, Interest: tennis'. You should output exactly three words, the names of the two people who share a common interest, and the common interest itself. In the example, this might be 'James, John, sports'. If you cannot find any common interest, output 'none'."
-            request.prompt = f"Name: {userdata.guest_data['guest1']['name']}, Interest: {userdata.guest_data['guest1']['interest']}. Name: {userdata.guest_data['guest2']['name']}, Interest: {userdata.guest_data['guest2']['interest']}. Name: {userdata.guest_data['host']['name']}, Interest: {userdata.guest_data['host']['interest']}."
-            response = self._llm(request)
-            commonality = response.output.lower()
-            # Remove punctuation and split the response
-            commonality = commonality.replace(".", "").replace(",", "").split()
-            if len(commonality) != 3 or commonality[0] == "none":
-                rospy.logerr("Failed to get common interest from LLM.")
-                userdata.interest_message = failure_message
-                return "failed"
-            name_1, name_2, common_interest = commonality
-            if name_1 == name_2:
-                userdata.interest_message = failure_message
-                return "failed"
-            userdata.interest_message = f"To get the conversation going, you might like to know that {name_1} and {name_2} both share an interest in {common_interest}. Do with that what you will."
+            guest_ids = ["host", "guest1", "guest2"]
+            interests = [
+                userdata.guest_data["host"]["interest"],
+                userdata.guest_data["guest1"]["interest"],
+                userdata.guest_data["guest2"]["interest"],
+            ]
+            response = self._sentence_embed_srv(interests)
+            most_similar_1, most_similar_2 = response.most_similar
+
+            most_similar_1_name = guest_ids[interests.index(most_similar_1)]
+            most_similar_2_name = guest_ids[interests.index(most_similar_2)]
+
+            userdata.interest_message = (
+                "To break the ice, I thought you'd like to know that "
+                f"{most_similar_1_name} and {most_similar_2_name} have a common interest in "
+                f"'{most_similar_1}'."
+            )
         except:
-            rospy.logerr("Failed to call LLM service.")
-            userdata.interest_message = "I'm afraid I couldn't find a common interest between you all, you'll have to make do with small talk."
             return "failed"
+
         return "succeeded"
 
 
