dcc0733c5c26cc949e452cdcad88f62165241988
Matt Barker <105945282+m-barker@users.noreply.github.com>
2025-06-30 18:21:19 +0100

    Better lang sam (#334)
    * feat: 3D langsam with skill
    
    * feat: tf server
    
    * feat: working transform point server
    
    * feat: working langsam detect 3d

--- skills/src/lasr_skills/__init__.py
+++ skills/src/lasr_skills/__init__.py
@@ -29,6 +29,7 @@ from .find_person import FindPerson
 from .detect_all_in_polygon import DetectAllInPolygon
 from .eye_tracker import StartEyeTracker, StopEyeTracker
 from .detect_nearest import GetNearestObject
+from .lang_sam_detect_3d import DetectLangSam3D
 
 # from .xml_question_answer import XmlQuestionAnswer
 from .find_person_and_tell import FindPersonAndTell
