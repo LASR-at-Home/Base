6891ab244f195e4cb51a2b8a8aeb288f7662fc39
Matt <mattbarker322@gmail.com>
2025-07-05 18:43:47 +0100

    WIP: go to waving person

--- tasks/restaurant/src/restaurant/state_machine.py
+++ tasks/restaurant/src/restaurant/state_machine.py
@@ -2,8 +2,30 @@ import smach
 import smach_ros
 from geometry_msgs.msg import Pose
 from lasr_skills import AskAndListen, GoToLocation, Rotate, Say, Wait
-from restaurant.states import Survey, HandleOrder, GetPoses
+from restaurant.states import Survey, HandleOrder, GetPoses, ComputeApproach
 from std_msgs.msg import Empty
+
+
+class ChooseWavingPerson(smach.State):
+    """
+    State to choose a waving person from the list of detected people.
+    The last person in the list is chosen
+    """
+
+    def __init__(self):
+        smach.State.__init__(
+            self,
+            outcomes=["succeeded", "failed"],
+            input_keys=["hands_up_detections"],
+            output_keys=["waving_person_detection", "hands_up_detections"],
+        )
+
+    def execute(self, userdata):
+        if userdata.hands_up_detections:
+            userdata.chosen_person = userdata.hands_up_detections.pop()
+            return "succeeded"
+        else:
+            return "failed"
 
 
 class Restaurant(smach.StateMachine):
@@ -51,7 +73,7 @@ class Restaurant(smach.StateMachine):
                 "SURVEY",
                 Survey((-71.0, 71.0), 10),
                 transitions={
-                    "customer_found": "GET_APPROACH_POSE",
+                    "customer_found": "CHOOSE_WAVING_PERSON",
                     "customer_not_found": "SURVEY",
                 },
                 remapping={
@@ -59,12 +81,42 @@ class Restaurant(smach.StateMachine):
                 },
             )
 
-            # smach.StateMachine.add(
-            #     "GO_TO_CUSTOMER",
-            #     GoToLocation(),
-            #     remapping={"location": "customer_approach_pose"},
-            #     transitions={"succeeded": "TAKE_ORDER", "failed": "failed"},
-            # )
+            smach.StateMachine.add(
+                "CHOOSE_WAVING_PERSON",
+                ChooseWavingPerson(),
+                transitions={
+                    "succeeded": "COMPUTE_APPROACH_POSE",
+                    "failed": "SURVEY",
+                },
+                remapping={
+                    "hands_up_detections": "hands_up_detections",
+                    "waving_person_detection": "waving_person_detection",
+                },
+            )
+            smach.StateMachine.add(
+                "COMPUTE_APPROACH_POSE",
+                ComputeApproach(),
+                transitions={
+                    "succeeded": "GO_TO_CUSTOMER",
+                    "failed": "SURVEY",
+                },
+                remapping={
+                    "customer_approach_pose": "customer_approach_pose",
+                },
+            )
+
+            smach.StateMachine.add(
+                "GO_TO_CUSTOMER",
+                GoToLocation(),
+                transitions={
+                    "succeeded": "LOOK_TO_CUSTOMER",
+                    "failed": "GO_TO_CUSTOMER",
+                },
+                remapping={
+                    "location": "customer_approach_pose",
+                },
+            )
+
 
             # smach.StateMachine.add(
             #     "TAKE_ORDER",
