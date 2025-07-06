674ab0581e313743b2d8ef39c42d22c919090aee
Matt <mattbarker322@gmail.com>
2025-07-06 13:11:11 +0100

    feat: add subprocess calls for navigation and map change in restaurant node

--- tasks/restaurant/nodes/main.py
+++ tasks/restaurant/nodes/main.py
@@ -2,8 +2,18 @@
 
 import rospy
 from restaurant.state_machine import Restaurant
+import subprocess
 
 if __name__ == "__main__":
     rospy.init_node("restaurant")
     restaurant = Restaurant()
     restaurant.execute()
+    subprocess.call(["rosservice", "call", "/pal_navigation_sm", "input: 'LOC'"])
+    subprocess.call(
+        [
+            "rosservice",
+            "call",
+            "/pal_map_manager/change_map",
+            "input: '2025-06-17-lab'",
+        ]
+    )
