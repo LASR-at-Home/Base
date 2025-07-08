8e8ba1373b5f2667ffb0a4b294bd3fd5e18b1fd5
Jared Swift <jared.swift@kcl.ac.uk>
2025-07-08 01:11:32 +0100

    No static planning scene, for now.

--- tasks/storing_groceries/src/storing_groceries/states/setup_planning_scene.py
+++ tasks/storing_groceries/src/storing_groceries/states/setup_planning_scene.py
@@ -25,113 +25,113 @@ class SetupPlanningScene(smach.StateMachine):
                     request=EmptyRequest(),
                 ),
                 transitions={
-                    "succeeded": "ADD_TABLE_SURFACE",
+                    "succeeded": "succeeded",
                     "preempted": "failed",
                     "aborted": "failed",
                 },
             )
 
-            smach.StateMachine.add(
-                "ADD_TABLE_SURFACE",
-                smach_ros.ServiceState(
-                    "/lasr_manipulation_planning_scene/add_support_surface",
-                    AddSupportSurface,
-                    request=AddSupportSurfaceRequest(
-                        "table",
-                        PoseStamped(
-                            header=Header(frame_id="map"),
-                            pose=Pose(
-                                position=Point(
-                                    **rospy.get_param(
-                                        "/storing_groceries/table/surface/pose/position"
-                                    ),
-                                ),
-                                orientation=Quaternion(
-                                    **rospy.get_param(
-                                        "/storing_groceries/table/surface/pose/orientation"
-                                    )
-                                ),
-                            ),
-                        ),
-                        Vector3(
-                            **rospy.get_param(
-                                "/storing_groceries/table/surface/dimensions"
-                            )
-                        ),
-                    ),
-                    output_keys=["success"],
-                    response_slots=["success"],
-                ),
-                transitions={
-                    "succeeded": "ADD_SHELF_SURFACES",
-                    "preempted": "failed",
-                    "aborted": "failed",
-                },
-            )
+    #         smach.StateMachine.add(
+    #             "ADD_TABLE_SURFACE",
+    #             smach_ros.ServiceState(
+    #                 "/lasr_manipulation_planning_scene/add_support_surface",
+    #                 AddSupportSurface,
+    #                 request=AddSupportSurfaceRequest(
+    #                     "table",
+    #                     PoseStamped(
+    #                         header=Header(frame_id="map"),
+    #                         pose=Pose(
+    #                             position=Point(
+    #                                 **rospy.get_param(
+    #                                     "/storing_groceries/table/surface/pose/position"
+    #                                 ),
+    #                             ),
+    #                             orientation=Quaternion(
+    #                                 **rospy.get_param(
+    #                                     "/storing_groceries/table/surface/pose/orientation"
+    #                                 )
+    #                             ),
+    #                         ),
+    #                     ),
+    #                     Vector3(
+    #                         **rospy.get_param(
+    #                             "/storing_groceries/table/surface/dimensions"
+    #                         )
+    #                     ),
+    #                 ),
+    #                 output_keys=["success"],
+    #                 response_slots=["success"],
+    #             ),
+    #             transitions={
+    #                 "succeeded": "ADD_SHELF_SURFACES",
+    #                 "preempted": "failed",
+    #                 "aborted": "failed",
+    #             },
+    #         )
 
-            shelf_iterator = smach.Iterator(
-                outcomes=["succeeded", "failed"],
-                it=rospy.get_param("/storing_groceries/cabinet/shelves"),
-                it_label="shelf_name",
-                exhausted_outcome="succeeded",
-                input_keys=[],
-                output_keys=[],
-            )
+    #         shelf_iterator = smach.Iterator(
+    #             outcomes=["succeeded", "failed"],
+    #             it=rospy.get_param("/storing_groceries/cabinet/shelves"),
+    #             it_label="shelf_name",
+    #             exhausted_outcome="succeeded",
+    #             input_keys=[],
+    #             output_keys=[],
+    #         )
 
-            with shelf_iterator:
-                container_sm = smach.StateMachine(
-                    outcomes=["continue", "succeeded", "failed"],
-                    input_keys=["shelf_name"],
-                )
+    #         with shelf_iterator:
+    #             container_sm = smach.StateMachine(
+    #                 outcomes=["continue", "succeeded", "failed"],
+    #                 input_keys=["shelf_name"],
+    #             )
 
-                with container_sm:
-                    smach.StateMachine.add(
-                        "ADD_SUPPORT_SURFACE",
-                        smach_ros.ServiceState(
-                            "/lasr_manipulation_planning_scene/add_support_surface",
-                            AddSupportSurface,
-                            request_cb=self._shelf_request_cb,
-                            output_keys=["success"],
-                            response_slots=["success"],
-                            input_keys=["shelf_name"],
-                        ),
-                        transitions={
-                            "succeeded": "continue",
-                            "preempted": "failed",
-                            "aborted": "failed",
-                        },
-                    )
-                smach.Iterator.set_contained_state(
-                    "CONTAINER_SM", container_sm, loop_outcomes=["continue"]
-                )
+    #             with container_sm:
+    #                 smach.StateMachine.add(
+    #                     "ADD_SUPPORT_SURFACE",
+    #                     smach_ros.ServiceState(
+    #                         "/lasr_manipulation_planning_scene/add_support_surface",
+    #                         AddSupportSurface,
+    #                         request_cb=self._shelf_request_cb,
+    #                         output_keys=["success"],
+    #                         response_slots=["success"],
+    #                         input_keys=["shelf_name"],
+    #                     ),
+    #                     transitions={
+    #                         "succeeded": "continue",
+    #                         "preempted": "failed",
+    #                         "aborted": "failed",
+    #                     },
+    #                 )
+    #             smach.Iterator.set_contained_state(
+    #                 "CONTAINER_SM", container_sm, loop_outcomes=["continue"]
+    #             )
 
-            smach.StateMachine.add(
-                "ADD_SHELF_SURFACES",
-                shelf_iterator,
-                transitions={"succeeded": "succeeded", "failed": "failed"},
-            )
+    #         smach.StateMachine.add(
+    #             "ADD_SHELF_SURFACES",
+    #             shelf_iterator,
+    #             transitions={"succeeded": "succeeded", "failed": "failed"},
+    #         )
 
-    def _shelf_request_cb(self, userdata, _):
-        return AddSupportSurfaceRequest(
-            userdata.shelf_name,
-            PoseStamped(
-                header=Header(frame_id="map"),
-                pose=Pose(
-                    position=Point(
-                        **rospy.get_param(
-                            f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/pose/position"
-                        ),
-                    ),
-                    orientation=Quaternion(
-                        **rospy.get_param(
-                            f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/pose/orientation"
-                        )
-                    ),
-                ),
-            ),
-            Vector3(
-                **rospy.get_param(
-                    f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/dimensions"
-                )
-            ),
-        )
+    # def _shelf_request_cb(self, userdata, _):
+    #     return AddSupportSurfaceRequest(
+    #         userdata.shelf_name,
+    #         PoseStamped(
+    #             header=Header(frame_id="map"),
+    #             pose=Pose(
+    #                 position=Point(
+    #                     **rospy.get_param(
+    #                         f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/pose/position"
+    #                     ),
+    #                 ),
+    #                 orientation=Quaternion(
+    #                     **rospy.get_param(
+    #                         f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/pose/orientation"
+    #                     )
+    #                 ),
+    #             ),
+    #         ),
+    #         Vector3(
+    #             **rospy.get_param(
+    #                 f"/storing_groceries/cabinet/shelves/{userdata.shelf_name}/surface/dimensions"
+    #             )
+    #         ),
+    #     )
