d39caa1a0358084419434a95084329ec199ef4db
Jared Swift <jared.swift@kcl.ac.uk>
2025-07-09 14:50:43 +0100

    Start properly scanning shelves

--- tasks/storing_groceries/src/storing_groceries/states/scan_shelves.py
+++ tasks/storing_groceries/src/storing_groceries/states/scan_shelves.py
@@ -36,127 +36,29 @@ class ScanShelves(smach.StateMachine):
 
         with self:
 
-            smach.StateMachine.add(
-                "LOOK_AT_SHELF",
-                LookToPoint(
-                    pointstamped=PointStamped(
-                        point=Point(
-                            **rospy.get_param(
-                                "/storing_groceries/cabinet/shelves/bottom/look_point"
-                            )
-                        ),
-                        header=Header(frame_id="map"),
-                    )
-                ),
-                transitions={
-                    "succeeded": "DETECT_OBJECTS",
-                    "aborted": "failed",
-                    "timed_out": "failed",
-                },
-            )
-
-            smach.StateMachine.add(
-                "DETECT_OBJECTS",
-                DetectAllInPolygonSensorData(
-                    ShapelyPolygon(rospy.get_param("/storing_groceries/table/polygon")),
-                    object_filter=[
-                        k for k in rospy.get_param("/storing_groceries/objects")
-                    ],
-                    min_coverage=0.9,
-                    min_confidence=0.1,
-                    z_axis=0.8,
-                    model="lasr.pt",
-                ),
-                transitions={"succeeded": "CLASSIFY_SHELF", "failed": "failed"},
-            )
-
-            smach.StateMachine.add(
-                "CLASSIFY_SHELF",
-                smach.CBState(
-                    self._classify_shelf,
-                    output_keys=["shelf_category"],
-                    input_keys=["detected_objects"],
-                    outcomes=["succeeded", "failed"],
-                ),
-                transitions={"succeeded": "SAY_CLASSIFICATION", "failed": "failed"},
-            )
-
-            smach.StateMachine.add(
-                "SAY_CLASSIFICATION",
-                Say(format_str="This shelf contains {}"),
-                remapping={"placeholders": "shelf_category"},
-                transitions={
-                    "succeeded": "succeeded",
-                    "preempted": "failed",
-                    "aborted": "failed",
-                },
-            )
-
-            smach.StateMachine.add(
-                "MATCH_CLASSIFICATION",
-                smach.CBState(
-                    self._match_classification,
-                    output_keys=["say_str"],
-                    input_keys=["shelf_category", "selected_object"],
-                    outcomes=["succeeded"],
-                ),
-                transitions={"succeeded": "SAY_CLASSIFICATION_STR"},
-            )
-
-            smach.StateMachine.add(
-                "SAY_CLASSIFICATION_STR",
-                Say(),
-                remapping={"text": "say_str"},
-                transitions={
-                    "succeeded": "succeeded",
-                    "preempted": "failed",
-                    "aborted": "failed",
-                },
-            )
-
-            # self.userdata.shelf_data = {}
-
-            # shelf_iterator = smach.Iterator(
-            #     outcomes=["succeeded", "failed"],
-            #     it=rospy.get_param("/storing_groceries/cabinet/shelves"),
-            #     it_label="shelf_id",
-            #     exhausted_outcome="succeeded",
-            #     input_keys=[],
-            #     output_keys=[],
-            # )
-
-            # with shelf_iterator:
-            #     container_sm = smach.StateMachine(
-            #         outcomes=["continue", "succeeded", "failed"],
-            #         input_keys=["shelf_id"],
-            #     )
-
-            #     with container_sm:
-
             # smach.StateMachine.add(
-            #     "ADJUST_TORSO",
-            #     smach_ros.SimpleActionState(
-            #         "/torso_controller/follow_joint_trajectory",
-            #         FollowJointTrajectoryAction,
-            #         goal_cb=self._adjust_torso_goal_cb,
-            #         ouput_keys=[],
-            #         input_keys=["shelf_id"],
+            #     "LOOK_AT_SHELF",
+            #     LookToPoint(
+            #         pointstamped=PointStamped(
+            #             point=Point(
+            #                 **rospy.get_param(
+            #                     "/storing_groceries/cabinet/shelves/bottom/look_point"
+            #                 )
+            #             ),
+            #             header=Header(frame_id="map"),
+            #         )
             #     ),
             #     transitions={
             #         "succeeded": "DETECT_OBJECTS",
-            #         "preempted": "failed",
             #         "aborted": "failed",
+            #         "timed_out": "failed",
             #     },
             # )
 
             # smach.StateMachine.add(
             #     "DETECT_OBJECTS",
             #     DetectAllInPolygonSensorData(
-            #         ShapelyPolygon(
-            #             rospy.get_param(
-            #                 "/storing_groceries/cabinet/shelves/bottom/polygon"
-            #             )
-            #         ),
+            #         ShapelyPolygon(rospy.get_param("/storing_groceries/table/polygon")),
             #         object_filter=[
             #             k for k in rospy.get_param("/storing_groceries/objects")
             #         ],
@@ -165,21 +67,119 @@ class ScanShelves(smach.StateMachine):
             #         z_axis=0.8,
             #         model="lasr.pt",
             #     ),
-            #     transitions={"succeeded": "SELECT_OBJECT", "failed": "failed"},
+            #     transitions={"succeeded": "CLASSIFY_SHELF", "failed": "failed"},
             # )
 
-    # def _adjust_torso_goal_cb(self, userdata, goal):
-    #     goal = FollowJointTrajectoryActionGoal()
-    #     goal.trajectory.joint_names = ["torso_lift_joint"]
-    #     point = JointTrajectoryPoint()
-    #     point.positions = [
-    #         rospy.get_param(
-    #             f"/storing_groceries/cabinet/shelves/{userdata.shelf_id}/torso_lift_joint"
-    #         )
-    #     ]
-    #     point.time_from_start = rospy.Duration(1)
-    #     goal.trajectory.points.append(point)
-    #     return goal
+            # smach.StateMachine.add(
+            #     "CLASSIFY_SHELF",
+            #     smach.CBState(
+            #         self._classify_shelf,
+            #         output_keys=["shelf_category"],
+            #         input_keys=["detected_objects"],
+            #         outcomes=["succeeded", "failed"],
+            #     ),
+            #     transitions={"succeeded": "SAY_CLASSIFICATION", "failed": "failed"},
+            # )
+
+            # smach.StateMachine.add(
+            #     "SAY_CLASSIFICATION",
+            #     Say(format_str="This shelf contains {}"),
+            #     remapping={"placeholders": "shelf_category"},
+            #     transitions={
+            #         "succeeded": "succeeded",
+            #         "preempted": "failed",
+            #         "aborted": "failed",
+            #     },
+            # )
+
+            # smach.StateMachine.add(
+            #     "MATCH_CLASSIFICATION",
+            #     smach.CBState(
+            #         self._match_classification,
+            #         output_keys=["say_str"],
+            #         input_keys=["shelf_category", "selected_object"],
+            #         outcomes=["succeeded"],
+            #     ),
+            #     transitions={"succeeded": "SAY_CLASSIFICATION_STR"},
+            # )
+
+            # smach.StateMachine.add(
+            #     "SAY_CLASSIFICATION_STR",
+            #     Say(),
+            #     remapping={"text": "say_str"},
+            #     transitions={
+            #         "succeeded": "succeeded",
+            #         "preempted": "failed",
+            #         "aborted": "failed",
+            #     },
+            # )
+
+            self.userdata.shelf_data = {}
+
+            shelf_iterator = smach.Iterator(
+                outcomes=["succeeded", "failed"],
+                it=rospy.get_param("/storing_groceries/cabinet/shelves"),
+                it_label="shelf_id",
+                exhausted_outcome="succeeded",
+                input_keys=[],
+                output_keys=[],
+            )
+
+            with shelf_iterator:
+                container_sm = smach.StateMachine(
+                    outcomes=["continue", "succeeded", "failed"],
+                    input_keys=["shelf_id"],
+                )
+
+                with container_sm:
+
+                    smach.StateMachine.add(
+                        "ADJUST_TORSO",
+                        smach_ros.SimpleActionState(
+                            "/torso_controller/follow_joint_trajectory",
+                            FollowJointTrajectoryAction,
+                            goal_cb=self._adjust_torso_goal_cb,
+                            ouput_keys=[],
+                            input_keys=["shelf_id"],
+                        ),
+                        transitions={
+                            "succeeded": "DETECT_OBJECTS",
+                            "preempted": "failed",
+                            "aborted": "failed",
+                        },
+                    )
+
+                    smach.StateMachine.add(
+                        "DETECT_OBJECTS",
+                        DetectAllInPolygonSensorData(
+                            # ShapelyPolygon(
+                            #     rospy.get_param(
+                            #         "/storing_groceries/cabinet/shelves/bottom/polygon"
+                            #     )
+                            # ),
+                            # object_filter=[
+                            #     k for k in rospy.get_param("/storing_groceries/objects")
+                            # ],
+                            # min_coverage=0.9,
+                            # min_confidence=0.1,
+                            # z_axis=0.8,
+                            # model="lasr.pt",
+                        ),
+                        transitions={"succeeded": "SELECT_OBJECT", "failed": "failed"},
+                    )
+
+    def _adjust_torso_goal_cb(self, userdata, goal):
+        goal = FollowJointTrajectoryActionGoal()
+        goal.trajectory.joint_names = ["torso_lift_joint"]
+        point = JointTrajectoryPoint()
+        point.positions = [
+            rospy.get_param(
+                f"/storing_groceries/cabinet/shelves/{userdata.shelf_id}/torso_lift_joint"
+            )
+        ]
+        point.time_from_start = rospy.Duration(1)
+        goal.trajectory.points.append(point)
+        return goal
 
     def _classify_shelf(self, userdata):
 
