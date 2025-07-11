0f88bc56b239e88759b3afde2cb91f92660b2442
Jared Swift <jared.swift@kcl.ac.uk>
2025-07-11 22:05:47 +0100

    Keep track of shelf data.

--- tasks/storing_groceries/src/storing_groceries/states/scan_shelves.py
+++ tasks/storing_groceries/src/storing_groceries/states/scan_shelves.py
@@ -16,16 +16,6 @@ from collections import defaultdict
 class ScanShelves(smach.StateMachine):
 
     def __init__(self):
-        """
-        Scan each shelf to determine the category of the objects from:
-        - cleaning_supplies
-        - drinks
-        - food
-        - decorations
-        - fruits
-        - snacks
-        - dishes
-        """
         super().__init__(
             outcomes=["succeeded", "failed"],
             output_keys=["shelf_data"],
@@ -34,84 +24,6 @@ class ScanShelves(smach.StateMachine):
 
         with self:
 
-            # smach.StateMachine.add(
-            #     "LOOK_AT_SHELF",
-            #     LookToPoint(
-            #         pointstamped=PointStamped(
-            #             point=Point(
-            #                 **rospy.get_param(
-            #                     "/storing_groceries/cabinet/shelves/bottom/look_point"
-            #                 )
-            #             ),
-            #             header=Header(frame_id="map"),
-            #         )
-            #     ),
-            #     transitions={
-            #         "succeeded": "DETECT_OBJECTS",
-            #         "aborted": "failed",
-            #         "timed_out": "failed",
-            #     },
-            # )
-
-            # smach.StateMachine.add(
-            #     "DETECT_OBJECTS",
-            #     DetectAllInPolygonSensorData(
-            #         ShapelyPolygon(rospy.get_param("/storing_groceries/table/polygon")),
-            #         object_filter=[
-            #             k for k in rospy.get_param("/storing_groceries/objects")
-            #         ],
-            #         min_coverage=0.9,
-            #         min_confidence=0.1,
-            #         z_axis=0.8,
-            #         model="lasr.pt",
-            #     ),
-            #     transitions={"succeeded": "CLASSIFY_SHELF", "failed": "failed"},
-            # )
-
-            # smach.StateMachine.add(
-            #     "CLASSIFY_SHELF",
-            #     smach.CBState(
-            #         self._classify_shelf,
-            #         output_keys=["shelf_category"],
-            #         input_keys=["detected_objects"],
-            #         outcomes=["succeeded", "failed"],
-            #     ),
-            #     transitions={"succeeded": "SAY_CLASSIFICATION", "failed": "failed"},
-            # )
-
-            # smach.StateMachine.add(
-            #     "SAY_CLASSIFICATION",
-            #     Say(format_str="This shelf contains {}"),
-            #     remapping={"placeholders": "shelf_category"},
-            #     transitions={
-            #         "succeeded": "succeeded",
-            #         "preempted": "failed",
-            #         "aborted": "failed",
-            #     },
-            # )
-
-            # smach.StateMachine.add(
-            #     "MATCH_CLASSIFICATION",
-            #     smach.CBState(
-            #         self._match_classification,
-            #         output_keys=["say_str"],
-            #         input_keys=["shelf_category", "selected_object"],
-            #         outcomes=["succeeded"],
-            #     ),
-            #     transitions={"succeeded": "SAY_CLASSIFICATION_STR"},
-            # )
-
-            # smach.StateMachine.add(
-            #     "SAY_CLASSIFICATION_STR",
-            #     Say(),
-            #     remapping={"text": "say_str"},
-            #     transitions={
-            #         "succeeded": "succeeded",
-            #         "preempted": "failed",
-            #         "aborted": "failed",
-            #     },
-            # )
-
             self.userdata.shelf_data = {}
 
             shelf_iterator = smach.Iterator(
@@ -119,14 +31,14 @@ class ScanShelves(smach.StateMachine):
                 it=rospy.get_param("/storing_groceries/cabinet/shelves"),
                 it_label="shelf_id",
                 exhausted_outcome="succeeded",
-                input_keys=[],
-                output_keys=[],
+                input_keys=["shelf_data"],
+                output_keys=["shelf_data"],
             )
 
             with shelf_iterator:
                 container_sm = smach.StateMachine(
                     outcomes=["continue", "succeeded", "failed"],
-                    input_keys=["shelf_id"],
+                    input_keys=["shelf_id", "shelf_data"],
                 )
 
                 with container_sm:
@@ -195,24 +107,13 @@ class ScanShelves(smach.StateMachine):
                         "CLASSIFY_SHELF",
                         smach.CBState(
                             self._classify_shelf,
-                            output_keys=["shelf_category"],
-                            input_keys=["detected_objects"],
+                            output_keys=["shelf_data"],
+                            input_keys=["shelf_data", "detected_objects"],
                             outcomes=["succeeded", "failed"],
                         ),
                         transitions={
-                            "succeeded": "SAY_CLASSIFICATION",
-                            "failed": "failed",
-                        },
-                    )
-
-                    smach.StateMachine.add(
-                        "SAY_CLASSIFICATION",
-                        Say(format_str="This shelf contains {}"),
-                        remapping={"placeholders": "shelf_category"},
-                        transitions={
                             "succeeded": "continue",
-                            "preempted": "failed",
-                            "aborted": "failed",
+                            "failed": "failed",
                         },
                     )
 
@@ -264,9 +165,12 @@ class ScanShelves(smach.StateMachine):
     def _classify_shelf(self, userdata):
 
         category_counts = defaultdict(int)
+        userdata.shelf_data["objects"] = [
+            obj.name for obj, _ in userdata.detected_objects
+        ]
 
         if not userdata.detected_objects:
-            userdata.shelf_category = "empty"
+            userdata.shelf_data["category"] = "empty"
             return "succeeded"
 
         for obj, _ in userdata.detected_objects:
@@ -276,7 +180,7 @@ class ScanShelves(smach.StateMachine):
             category_counts[category] += 1
 
         shelf_category = max(category_counts, key=lambda key: category_counts[key])
-        userdata.shelf_category = shelf_category
+        userdata.shelf_data["category"] = shelf_category
 
         return "succeeded"
 
@@ -289,9 +193,3 @@ class ScanShelves(smach.StateMachine):
         else:
             userdata.say_str = f"{userdata.selected_object[0].name} is a {object_category}. This shelf contains {userdata.shelf_category}, so I will place the {userdata.selected_object[0].name} here."
         return "succeeded"
-
-
-if __name__ == "__main__":
-    rospy.init_node("test_scan_shelves")
-    scan_shelves = ScanShelves()
-    scan_shelves.execute()
