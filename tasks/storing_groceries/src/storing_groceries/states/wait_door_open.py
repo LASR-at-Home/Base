fe88ee22ea8da350b2f90934318c276c89eec26e
Euniceverse <153387754+Euniceverse@users.noreply.github.com>
2025-07-11 22:55:47 +0100

    25 storing groceries state machine (#352)
    * Copy receptionist as storing groceries
    
    * Add check_door_state.py
    
    * Working version for doors
    
    * Detect with time
    
    * Delete unnecessary files
    
    * Apply WaitDoorOpen state and SelectObject
    
    * Add Choose Object feature
    
    * Detect object open or not
    
    * Add classify_category()
    
    * Update llm_inference
    
    * Update structure
    
    * Update choose object with not_graspable
    
    * feat: working LLM service for receptionist
    
    * Remove unnecessary files
    
    * Create llm for storing_groceries
    
    * Add states and configure for task
    
    * Add detection with yolo
    
    * Fix error in llm when using cpu
    
    * Add fine details for test
    
    * fix simple error in llm
    
    * commit
    
    * Update on Door openning
    
    * update LangSamState
    
    * Progress on AnalyseDepthState
    
    * Prepare to change to LangSam3d
    
    * temp commit
    
    * Merge
    
    * Temp commit
    
    * Format.
    
    * Fix permissions.
    
    * More permissions.
    
    * Cleanup redundant files.
    
    * Use xtion/depth_registered/points.
    
    * Default to map frame.
    
    * Move motions.
    
    * Fix permissions.
    
    * Remove catkin virtualenv.
    
    ---------
    
    Co-authored-by: Jared Swift <jared.swift@kcl.ac.uk>

---
+++ tasks/storing_groceries/src/storing_groceries/states/wait_door_open.py
@@ -0,0 +1,40 @@
+import smach
+
+from lasr_skills import Say, CheckDoorStatus
+from storing_groceries.states import *
+
+# TODO: Add recovery states and prevent infinit loop
+
+
+class WaitDoorOpen(smach.StateMachine):
+    def __init__(self):
+        super().__init__(
+            outcomes=["succeeded", "aborted", "preempted"],
+            input_keys=[],
+        )
+
+        with self:
+
+            smach.StateMachine.add(
+                "CHECK_DOOR_STATUS",
+                CheckDoorStatus(
+                    expected_closed_depth=1.2,  # adjust for cabinet (~0.5) or room door (~1.2)
+                    change_thresh=0.4,
+                    open_thresh=0.6,
+                ),
+                transitions={
+                    "open": "DOOR_OPEN",
+                    "closed": "CHECK_DOOR_STATUS",
+                    "error": "CHECK_DOOR_STATUS",
+                },
+            )
+
+            smach.StateMachine.add(
+                "DOOR_OPEN",
+                Say(text="Door is open, Starting storing groceries"),
+                transitions={
+                    "succeeded": "succeeded",
+                    "aborted": "aborted",
+                    "preempted": "preempted",
+                },
+            )
