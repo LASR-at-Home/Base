0bb317b363b39b22739cb1485a02fd5f48a64df2
Siyao <sveali41@gmail.com>
2025-07-18 22:03:53 +0100

    let's win robocup

---
+++ tasks/give_me_a_hand/src/give_me_a_hand/states/communicate_operators.py
@@ -0,0 +1,34 @@
+# So this is a state machine 
+
+from lasr_skills import AskAndListen
+
+
+# Which has 
+
+
+
+
+# ASK - "Where should I put the object"
+
+# e.g - "Where should I put the object?" "put the object by the microwave"
+
+#
+
+# AskAndListen - output transcription 
+
+
+
+# Pass transcription into HandleRequestLLM 
+
+
+# WHich should output POSE. 
+
+
+
+#Pass Pose to GoToLocation 
+
+
+
+# DONE
+
+
