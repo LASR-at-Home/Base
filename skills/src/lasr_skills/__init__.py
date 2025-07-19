eac7bff537d9a16ae43000952ce5f74e5d478465
Siyao <sveali41@gmail.com>
2025-07-19 01:50:25 +0100

    place

--- skills/src/lasr_skills/__init__.py
+++ skills/src/lasr_skills/__init__.py
@@ -43,6 +43,6 @@ from .detect_dict import DetectDict
 from .point_at_person import PointAtPerson
 from .detect_gesture_3d import DetectGesture3D, DetectHandUp3D
 from .detect_door_opening import DetectDoorOpening
-from .follow_person import FollowPerson
+# from .follow_person import FollowPerson
 from .listen_for_wakeword import ListenForWakeword
 from .detect_door_opening import DetectDoorOpening
--- tasks/give_me_a_hand/src/give_me_a_hand/states/grasp_and_put.py
+++ tasks/give_me_a_hand/src/give_me_a_hand/states/grasp_and_put.py
@@ -5,12 +5,15 @@ import smach_ros
 
 from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
 from std_msgs.msg import Header  
-from lasr_skills import ReceiveObject, GoToLocation, PlayMotion
+from lasr_skills import ReceiveObject, PlayMotion, GoToLocation
 from lasr_manipulation_msgs.msg import PlaceAction, PlaceGoal
+import yaml
+import rosparam
+from tasks.lift.src.lift import sm
 
 
 class HandoverAndDeliver(smach.StateMachine):
-    def __init__(self, location_pose: Pose):
+    def __init__(self):
         super(HandoverAndDeliver, self).__init__(
             outcomes=["succeeded", "failed", "escape"],
             input_keys=[
@@ -31,16 +34,38 @@ class HandoverAndDeliver(smach.StateMachine):
                 },
             )
 
-            # smach.StateMachine.add(
-            #     "DELIVER_OBJECT",
-            #     GoToLocation(location_pose),
-            #     transitions={
-            #         "succeeded": "PLAY_PREPLACE_MOTION",
-            #         "failed": "PLAY_PREPLACE_MOTION",
-            #     },
-            #     remapping={"goal_pose": "goal_pose"},
-            # )
+            # go to location
+            smach.StateMachine.add(
+                "DELIVER_OBJECT",
+                GoToLocation(),
+                transitions={
+                    "succeeded": "PLAY_PREPLACE_MOTION",
+                    "failed": "PLAY_PREPLACE_MOTION",
+                },
+                remapping={"goal_pose": "goal_pose"},
+            )
 
+            # detection obj
+            smach.StateMachine.add(
+                "LOAD_PLACE_POSE",
+                LoadPlacePoseByName(),
+                transitions={
+                    "succeeded": "PLAY_PREPLACE_MOTION",
+                    "failed": "failed",
+                },
+                
+            )
+
+            smach.StateMachine.add(
+                "GO_TO_LOCATION",
+                GoToLocation(location_param = "give_me_a_hand/place_location"),
+                transitions={
+                    "succeeded": "DETECT",
+                    "failed": "failed",
+                },
+                remapping={"location": "pose"},
+                )
+            
             smach.StateMachine.add(
                 "PLAY_PREPLACE_MOTION",
                 PlayMotion(motion_name="home_to_preplace"),
@@ -90,41 +115,57 @@ class HandoverAndDeliver(smach.StateMachine):
 
     def _place_cb(self, userdata, goal):
         goal = PlaceGoal()
-        goal.object_id = (
-            userdata.selected_object.name
-            if hasattr(userdata.selected_object, "name")
-            else userdata.selected_object
-        )
+        goal.object_id = userdata.obj_id
         goal.candidate_poses = [userdata.place_pose] 
         goal.surface_id = userdata.surface_id
         return goal
+    
+
+class LoadPlacePoseByName(smach.State):
+    def __init__(self, yaml_file_path):
+        smach.State.__init__(
+            self,
+            outcomes=["succeeded", "failed"],
+            input_keys=["place_location_name"],
+            output_keys=["place_pose"],
+        )
+        with open(yaml_file_path, 'r') as f:
+            self.pose_dict = yaml.safe_load(f)
+
+    def execute(self, userdata):
+        name = userdata.place_location_name
+        rospy.loginfo(f"[LoadPlacePoseByName] Looking for location: {name}")
+
+        if name not in self.pose_dict:
+            rospy.logerr(f"[LoadPlacePoseByName] '{name}' not found in YAML.")
+            return "failed"
+        
+        userdata.pose = rospy.get_param(
+            f"give_me_a_have/{name}/pose", None
+        )
+
+        userdata.place_pose = rospy.get_param(
+            f"give_me_a_have/{name}/place_pose", None
+        )
+
+        userdata.surface_id = "shelf"
+        userdata.obj_id = "object_1"
+
+        return "succeeded"
+
 
 
 if __name__ == "__main__":
     rospy.init_node("handover_and_deliver_test")
 
     # Define goal location
-    goal_pose_stamped = PoseStamped(
-        header=Header(frame_id="map"),
-        pose=Pose(
-            position=Point(x=0.7, y=-0.15, z=0.95),
-            orientation=Quaternion(x=0.600, y=0.100, z=0.048, w=0.749),
-        ),
-    )
-
-    sm = HandoverAndDeliver(location_pose=goal_pose_stamped.pose)
-
-    sm.userdata.selected_object = type("Obj", (), {"name": "object_1"})()
-    sm.userdata.object_name = sm.userdata.selected_object.name
-
-    sm.userdata.place_pose = PoseStamped(
-        header=Header(frame_id="base_footprint"),
-        pose=Pose(
-            position=Point(x=0.7, y=-0.15, z=0.95),
-            orientation=Quaternion(w=1.0),
-        ),
-    )
-    sm.userdata.goal_pose = goal_pose_stamped  
-    sm.userdata.surface_id = "shelf"
+    sm.userdata.place_location_name = "shelf"
+    sm = HandoverAndDeliver()
     outcome = sm.execute()
-    rospy.loginfo(f"State machine finished with outcome: {outcome}")
+
+
+
+
+
+
+
