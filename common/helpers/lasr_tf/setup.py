4df15fe634c3edcf54e7c3e938aae08b6807fd44
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2025-11-30 20:02:33 +0000

    Ported the lasr_tf package (#358)
    * Ported the lasr_tf package
    
    * Reformated files

---
+++ common/helpers/lasr_tf/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = "lasr_tf"
+
+setup(
+    name="lasr_tf",
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="fadi",
+    maintainer_email="fadimostefai@gmail.com",
+    description="The lasr_tf package",
+    license="MIT",
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    entry_points={
+        "console_scripts": ["tf_server = lasr_tf.tf_server:main"],
+    },
+)
