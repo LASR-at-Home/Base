6e204fcb416588fc3b740c42f1b1d3964107ba04
larace27 <151533915+larace27@users.noreply.github.com>
2026-04-30 11:38:02 +0100

    Port lasr_wakewords to ROS 2 (#407)
    Co-authored-by: Lara Ergenç (k23090770) <k23090770@nmslab-004177.nms.kcl.ac.uk>
    Co-authored-by: Lara Ergenç (k23090770) <k23090770@nmslab-008349.nms.kcl.ac.uk>

---
+++ common/speech/lasr_wakewords/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = "lasr_wakewords"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/models", glob("models/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Siyao Li",
+    maintainer_email="sveali41@gmail.com",
+    description="Wakeword detection service built on openWakeWord",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "wakeword_service = lasr_wakewords.wakeword_service:main",
+        ]
+    },
+)
