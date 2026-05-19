a8548d9c87c1a46d845780300a100ec1c6bcf445
rajhirym <rym.rajhi@icloud.com>
2026-05-19 16:57:35 +0100

    vlm service

--- common/foundation_models/lasr_vlm/setup.py
+++ common/foundation_models/lasr_vlm/setup.py
@@ -18,6 +18,9 @@ class InstallCommand(setuptools.command.install.install):
 
 
 setup(
+    cmdclass={
+        'install': InstallCommand
+    },
     name=package_name,
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
@@ -37,6 +40,6 @@ setup(
         ],
     },
     entry_points={
-        "console_scripts": [],
+        "console_scripts": ["vlm_service = lasr_vlm.nodes.vlm_service:main"],
     },
 )
