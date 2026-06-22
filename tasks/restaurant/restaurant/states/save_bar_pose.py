7d3fe8927a6afa5726ca3ffae75e850726fc73af
raspberryenvoie <raspberryenvoie@protonmail.com>
2026-06-22 18:31:55 +0100

    Test

--- tasks/restaurant/restaurant/states/save_bar_pose.py
+++ tasks/restaurant/restaurant/states/save_bar_pose.py
@@ -1,6 +1,8 @@
 import message_filters
 import yasmin
+import yasmin_ros
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from rclpy.qos import QoSProfile, DurabilityPolicy, ReliabilityPolicy, HistoryPolicy
 
 
 class SaveBarPose(yasmin.State):
@@ -8,14 +10,24 @@ class SaveBarPose(yasmin.State):
         super().__init__(outcomes=["succeeded", "failed"])
         self.node = node
         self.topic = topic
-        pose_info = message_filters.Subscriber(
-            self.node, PoseWithCovarianceStamped, self.topic
-        )
 
-        self.cache = message_filters.Cache(pose_info)
+        self.robot_pose = None
+        self.robot_pose_sub = yasmin_ros.logger_node.create_subscription(
+            PoseWithCovarianceStamped,
+            "/amcl_pose",
+            self.robot_point_cb,
+            QoSProfile(
+                depth=1,
+                reliability=ReliabilityPolicy.RELIABLE,
+                durability=DurabilityPolicy.TRANSIENT_LOCAL,
+                history=HistoryPolicy.KEEP_LAST,
+            ),
+        )
+    def robot_point_cb(self, msg: PoseWithCovarianceStamped):
+        self.robot_pose = msg
 
     def execute(self, blackboard):
-        msg = self.cache.getLast()
+        msg = self.robot_pose
         if msg is not None:
             blackboard["bar_pose"] = msg
             yasmin.YASMIN_LOG_INFO(f"Type is {type(msg)}.")
