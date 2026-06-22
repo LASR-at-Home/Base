9feb00d510e0a382a8bf2f076883f830fafeee89
Hayeong Lee <hayeong.lee@kcl.ac.uk>
2026-06-22 19:11:02 +0100

    Fix save_bar_pose.py

--- tasks/restaurant/restaurant/states/save_bar_pose.py
+++ tasks/restaurant/restaurant/states/save_bar_pose.py
@@ -1,18 +1,20 @@
-import message_filters
 import yasmin
 import yasmin_ros
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from rclpy.qos import QoSProfile, DurabilityPolicy, ReliabilityPolicy, HistoryPolicy
 
+import time
 
 class SaveBarPose(yasmin.State):
-    def __init__(self, node, topic: str = "/amcl_pose"):
+    def __init__(self, topic: str = "/amcl_pose"):
         super().__init__(outcomes=["succeeded", "failed"])
-        self.node = node
         self.topic = topic
+        self.node = yasmin_ros.logger_node
+
+        self.add_output_key("bar_pose")
 
         self.robot_pose = None
-        self.robot_pose_sub = yasmin_ros.logger_node.create_subscription(
+        self.robot_pose_sub = self.node.create_subscription(
             PoseWithCovarianceStamped,
             "/amcl_pose",
             self.robot_point_cb,
@@ -23,18 +25,23 @@ class SaveBarPose(yasmin.State):
                 history=HistoryPolicy.KEEP_LAST,
             ),
         )
+        time.sleep(1)
+        
     def robot_point_cb(self, msg: PoseWithCovarianceStamped):
         self.robot_pose = msg
 
     def execute(self, blackboard):
         msg = self.robot_pose
-        if msg is not None:
+        try:
             blackboard["bar_pose"] = msg
             yasmin.YASMIN_LOG_INFO(f"Type is {type(msg)}.")
             yasmin.YASMIN_LOG_INFO("Saved bar pose to blackboard.")
+            if msg is None:
+                yasmin.YASMIN_LOG_INFO(f"Message has value {msg}.")
+                return "failed"
             return "succeeded"
-        else:
+        except Exception as e:
             yasmin.YASMIN_LOG_ERROR(
-                "Failed to save bar pose. No pose message received."
+                f"Failed to save bar pose. No pose message received. {e}"
             )
             return "failed"
