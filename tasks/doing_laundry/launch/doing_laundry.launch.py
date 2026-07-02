11db82d4b9c1e091e38bea867a100405e28d1ee8
Aabumouti03 <anoudabumouti@gmail.com>
2026-07-02 01:21:35 +0100

    add launch file and edit config and set up

---
+++ tasks/doing_laundry/launch/doing_laundry.launch.py
@@ -0,0 +1,50 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    load_motions = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("skills"),
+                "launch",
+                "load_motions.launch.py",
+            )
+        )
+    )
+
+    yolo_service = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("lasr_vision_yolo"),
+                "launch",
+                "service_launch.xml",
+            )
+        )
+    )
+
+    state_machine = TimerAction(
+        period=10.0,
+        actions=[
+            Node(
+                package="doing_laundry",
+                executable="state_machine",
+                name="doing_laundry",
+                parameters=[
+                    os.path.join(
+                        get_package_share_directory("doing_laundry"),
+                        "config",
+                        "config.yaml",
+                    )
+                ],
+                output="screen",
+            )
+        ],
+    )
+
+    return LaunchDescription([load_motions, yolo_service, state_machine])
--- tasks/doing_laundry/setup.py
+++ tasks/doing_laundry/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob
 package_name = "doing_laundry"
 
 setup(
@@ -9,6 +10,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
