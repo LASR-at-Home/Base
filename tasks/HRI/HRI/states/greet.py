9253f4189fbb2264459f6a7d5686e69e8aa62446
Ma'ayan Armony <maayan.armony@gmail.com>
2026-07-03 06:59:34 +0100

    Add 1 retry for getting name and drink

--- tasks/HRI/HRI/states/greet.py
+++ tasks/HRI/HRI/states/greet.py
@@ -130,6 +130,10 @@ class GreetGuest(yasmin.StateMachine):
                     "GET_NAME_DRINK": "failed",
                     "GET_FACE_ATTRIBUTES": "failed",
                 },
+                "failed_speech": {
+                    "GET_NAME_DRINK": "failed",
+                    "GET_FACE_ATTRIBUTES": "succeeded",
+                },
                 "failed_vision": {
                     "GET_NAME_DRINK": "succeeded",
                     "GET_FACE_ATTRIBUTES": "failed",
@@ -180,8 +184,30 @@ class GreetGuest(yasmin.StateMachine):
                 "succeeded": transition,
                 "failed": "failed",
                 "failed_vision": "failed",
+                "failed_speech": "REPEAT_ASK_GUEST",
                 "failed_face": "failed",
                 "failed_attributes": "failed",
+            },
+        )
+
+        self.add_state(
+            "REPEAT_ASK_GUEST",
+            AskAndListen(
+                tts_phrase="I am sorry, I did not understand. Please say 'Hi Tiago' for me to begin listening. What is your name and drink?",
+            ),
+            transitions={
+                "succeeded": "GET_NAME_DRINK",
+                "failed": "SAY_WELCOME",
+            },
+            remappings={"transcribed_speech": "guest_transcription"},
+        )
+
+        self.add_state(
+            "GET_NAME_DRINK",
+            GetNameAndDrink(guest_id=guest_id, last_resort=last_resort),
+            transitions={
+                "succeeded": "SAY_WELCOME",
+                "failed": "SAY_WELCOME",
             },
         )
 
