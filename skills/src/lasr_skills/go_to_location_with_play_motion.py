5542e59fce829fae242d9c8ed2b16a59a02168c0
Fadi <130671609+Fadi-Mostefai@users.noreply.github.com>
2026-07-28 15:45:42 +0100

    Safety Inspection Code (#462)
    * Safety SM made
    
    * Testing Safety SM
    
    * Changes from testing
    
    * Ros2 interfaces (#458)
    
    * WIP.
    
    * feat: robot interfaces.
    
    * Revert "WIP."
    
    This reverts commit c92a2bcaebf2a7a545fc74b9e98e9ab9f84d98d5.
    
    ---------
    
    Co-authored-by: Jared Swift <jared.swift@kcl.ac.uk>
    
    ---------
    
    Co-authored-by: Jared Swift <jared.swift@kcl.ac.uk>

--- skills/src/lasr_skills/go_to_location_with_play_motion.py
+++ skills/src/lasr_skills/go_to_location_with_play_motion.py
@@ -9,10 +9,12 @@ class SafeGoToLocation(yasmin.StateMachine):
 
         self.add_input_key("location")
         self.add_input_key("motion_name")
-
-        location_param = location_param.upper()
-
-        state_name = f"GO_TO_{location_param}"
+        
+        if location_param:
+            location_param = location_param.upper()
+            state_name = f"GO_TO_{location_param}"
+        else:
+            state_name = "SAFE_GO_TO_POINT"
 
         self.add_state(
             "PRE_NAV",
@@ -23,6 +25,9 @@ class SafeGoToLocation(yasmin.StateMachine):
                 "canceled": state_name,
             },
         )
+        
+        if location_param:
+            location_param = location_param.lower()
 
         self.add_state(
             state_name,
